aea237d1d69ca0a3e636591a9d2c999dbff36783
s.lebedev <s.lebedev@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2014-11-14 15:49:24 +0000

    Update macro to enable to run with FairRaunOnline

--- macro/rich/prototype/beamtime/run_analysis.C
+++ macro/rich/prototype/beamtime/run_analysis.C
@@ -8,7 +8,54 @@ void run_analysis()
    //string hldFileName = "/Users/slebedev/Development/cbm/trunk/cbmroot/macro/fles/secondtest_pulser16ch+sync.hld";
    string hldFileName = "../../../fles/secondtest_pulser16ch+sync.hld";
 
-   CbmRichTrbUnpack* richUnpack = new CbmRichTrbUnpack();
-   richUnpack->Run(hldFileName);
+   // --- Specify number of events to be produced.
+   // --- -1 means run until the end of the input file.
+   Int_t nEvents = -1;
+
+   // --- Specify output file name (this is just an example)
+   TString outFile = "/Users/slebedev/Development/cbm/trunk/cbmroot/macro/fles/output_test_file.root";
+
+   // --- Set log output levels
+   FairLogger::GetLogger()->SetLogScreenLevel("INFO");
+   FairLogger::GetLogger()->SetLogVerbosityLevel("LOW");
+
+   // --- Set debug level
+   gDebug = 0;
+
+
+   CbmRichTrbUnpack* source = new CbmRichTrbUnpack(hldFileName);
+
+   // --- Event header
+ //  FairEventHeader* event = new CbmTbEvent();
+ //  event->SetRunId(260);
+
+   // --- Run
+   FairRunOnline *run = new FairRunOnline(source);
+   run->SetOutputFile(outFile);
+ //  run->SetEventHeader(event);
+
+   CbmRichTrbRawQa* qaRaw = new CbmRichTrbRawQa();
+   run->AddTask(qaRaw);
+
+
+
+   run->Init();
+
+
+   // --- Start run
+   TStopwatch timer;
+   timer.Start();
+   run->Run(nEvents, 0); // run until end of input file
+   timer.Stop();
+
+   // --- End-of-run info
+   Double_t rtime = timer.RealTime();
+   Double_t ctime = timer.CpuTime();
+   std::cout << std::endl << std::endl;
+   std::cout << "Macro finished successfully." << std::endl;
+   std::cout << "Output file is " << outFile << std::endl;
+   std::cout << "Real time " << rtime << " s, CPU time " << ctime << " s" << std::endl;
+   std::cout << " Test passed" << std::endl;
+   std::cout << " All ok " << std::endl;
 }
 
