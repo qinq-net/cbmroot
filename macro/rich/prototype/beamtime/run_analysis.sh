b231c6493a682f972273bcce80778a7a08e34f9d
s.lebedev <s.lebedev@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2014-11-22 10:47:27 +0000

    Add comments on how to run analysis

--- macro/rich/prototype/beamtime/run_analysis.sh
+++ macro/rich/prototype/beamtime/run_analysis.sh
@@ -39,7 +39,10 @@ analyze_run_directory() {
    analyze_run_files ${1} ${2} ${3} ${filesArray[@]}
 }
 
-#filesArray=( te14324200946.hld )
+#example how to run analysis for single (or multiple files)
+#filesArray=( file1.hld file2.hld file3.hld file4.hld)
+#analyze_run_files outputRootFileWithHist.root outputDirWithImages/ titleOfAnalysis ${filesArray[@]}
+
 analyze_run_files wls_off_ring_a1.root wls_off_ring_a1/ wls_off_ring_a1 wls_off_ring_a1.hld
 analyze_run_files wls_off_ring_b1.root wls_off_ring_b1/ wls_off_ring_b1 wls_off_ring_b1.hld
 analyze_run_files wls_off_ring_b2.root wls_off_ring_b2/ wls_off_ring_b2 wls_off_ring_b2.hld
@@ -52,9 +55,10 @@ analyze_run_files wls_off_ring_h1.root wls_off_ring_h1/ wls_off_ring_h1 wls_off_
 analyze_run_files wls_off_ring_h2.root wls_off_ring_h2/ wls_off_ring_h2 wls_off_ring_h2.hld
 
 
+#example how to run analysis for all .hld files in directory
+#analyze_run_directory outputRootFileWithHist.root outputDirWithImages/ titleOfAnalysis directoryWithHldFiles/
 
-
-#analyze_run_directory all_test_file.root recoRR/ testRun .
+#analyze_run_directory all_test_file.root recoRR/ testRun dirWithHld
 
 
 export SCRIPT=no
