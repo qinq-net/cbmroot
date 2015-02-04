3e3c2a050cf3bd250ba7adad87e3772ae78a3a33
e.lebedeva <e.lebedeva@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2015-02-04 11:41:23 +0000

    run hadd in parallel

--- macro/analysis/dielectron/draw_litqa.C
+++ macro/analysis/dielectron/draw_litqa.C
@@ -5,19 +5,21 @@ void draw_litqa()
 
 
    std::string dir = "/Users/slebedev/Development/cbm/data/lmvm/dec14/8gev/stsv13d/richv14a/trd4/tofv13/0.7field/nomvd/rho0/";
-   std::string outputDir = dir + "results_litqa/";
    std::string fileName = dir + "litqa.auau.8gev.centr.all.root";
 
-   gSystem->mkdir(outputDir.c_str(), true);
+   std::string outputDirTracking = dir + "results_litqa_tracking/";
+   std::string outputDirClustering = dir + "results_litqa_clustering/";
+   gSystem->mkdir(outputDirTracking.c_str(), true);
+   gSystem->mkdir(outputDirClustering.c_str(), true);
 
    CbmSimulationReport* trackingQaReport = new CbmLitTrackingQaReport();
-   trackingQaReport->Create(fileName, outputDir);
+   trackingQaReport->Create(fileName, outputDirTracking);
+
+   CbmSimulationReport* clusteringQaReport = new CbmLitClusteringQaReport();
+   clusteringQaReport->Create(fileName, outputDirClustering);
 
 //   CbmSimulationReport* fitQaReport = new CbmLitFitQaReport();
 //   fitQaReport->Create(fileName, outputDir);
-
-//   CbmSimulationReport* clusteringQaReport = new CbmLitClusteringQaReport();
-//   clusteringQaReport->Create(fileName, outputDir);
 
 //   CbmLitRadLengthQaReport* radLengthQaReport = new CbmLitRadLengthQaReport();
 //   radLengthQaReport->Create(fileName, outputDir);
