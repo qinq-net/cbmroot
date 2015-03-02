ad19f48ea481a07557da895b27eaa46a5aa3da26
s.lebedev <s.lebedev@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2015-03-02 09:36:36 +0000

    Add new histogram for study report

--- macro/rich/prototype/beamtime/draw_study_report.C
+++ macro/rich/prototype/beamtime/draw_study_report.C
@@ -6,34 +6,31 @@ void draw_study_report()
 
 
 	std::vector<std::string> fileNames, studyNames;
-	std::string outputDir = "report_wls_off/;
+	std::string outputDir = "results/report_wls_on_offset100/";
 
-	fileNames.push_back("wls_off_ring_a1.root");
-	fileNames.push_back("wls_off_ring_b1.root");
-	fileNames.push_back("wls_off_ring_b2.root");
-	fileNames.push_back("wls_off_ring_b3.root");
-	fileNames.push_back("wls_off_ring_d1.root");
-	fileNames.push_back("wls_off_ring_dg1.root");
-	fileNames.push_back("wls_off_ring_g1.root");
-	fileNames.push_back("wls_off_ring_gh1.root");
-	fileNames.push_back("wls_off_ring_h1.root");
-	fileNames.push_back("wls_off_ring_h2.root");
+	fileNames.push_back("results/ringB1.root");
+	fileNames.push_back("results/ringH1.root");
+	fileNames.push_back("results/ringG1.root");
+	fileNames.push_back("results/ringH2.root");
+	fileNames.push_back("results/ringB2.root");
+	fileNames.push_back("results/ringA1.root");
+	fileNames.push_back("results/ringB3.root");
 
-	studyNames.push_back("wls_off_ring_a1");
-	studyNames.push_back("wls_off_ring_b1");
-	studyNames.push_back("wls_off_ring_b2");
-	studyNames.push_back("wls_off_ring_b3");
-	studyNames.push_back("wls_off_ring_d1");
-	studyNames.push_back("wls_off_ring_dg1");
-	studyNames.push_back("wls_off_ring_g1");
-	studyNames.push_back("wls_off_ring_gh1");
-	studyNames.push_back("wls_off_ring_h1");
-	studyNames.push_back("wls_off_ring_h2");
+
+	studyNames.push_back("ringB1");
+	studyNames.push_back("ringH1");
+	studyNames.push_back("ringG1");
+	studyNames.push_back("ringH2");
+	studyNames.push_back("ringB2");
+	studyNames.push_back("ringA1");
+	studyNames.push_back("ringB3");
+
 
 	if (outputDir != ""){
 		gSystem->mkdir(outputDir.c_str(), true); // create directory if it does not exist
 	}
 
-	CbmStudyReport* reportQa = new CbmRichTrbRecoQaStudyReport();
+	CbmRichTrbRecoQaStudyReport* reportQa = new CbmRichTrbRecoQaStudyReport();
+	reportQa->SetFitHist(true);
 	reportQa->Create(fileNames, studyNames, outputDir);
 }
