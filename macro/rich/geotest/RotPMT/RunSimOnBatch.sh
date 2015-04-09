cefeecb19071110d6fce834b5ee67293edaadc46
t.mahmoud <t.mahmoud@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2015-04-09 08:14:39 +0000

    new dimentions

--- macro/rich/geotest/RotPMT/RunSimOnBatch.sh
+++ macro/rich/geotest/RotPMT/RunSimOnBatch.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#$ -wd /tmp 
 
 rotx=$1
 roty=$2
@@ -11,10 +12,11 @@ MomMin=$7
 MomMax=$8
 
 
+# specify input and output directories
 echo "rotx = $rotx, roty = $roty"
-
+outdir=/hera/cbm/users/tariq/GeoOptRootFiles/
 cbmroot_config_path=/hera/cbm/users/tariq/cbmroot/buildcbm/config.sh
-macro_dir=/hera/cbm/users/tariq/cbmroot/macro/rich/geotest/RotPMT/
+macro_dir=/hera/cbm/users/tariq/cbmroot/macro/rich/geotest/RotPMT
 # Needed to run macro via script
 export SCRIPT=yes
 export N_EVS=$nEvs
@@ -36,15 +38,14 @@ mkdir -p ${outdir}/log
 
 # create special and enter special workdir for job
 username=$(whoami)
+#workdir=/tmp/$username/$rotmir.$rotx.$roty.$GeoCase.$PtNotP.
 workdir=/tmp/$username/$JOB_ID.$SGE_TASK_ID
 mkdir -p $workdir
 cd $workdir
 
-#root -b -l -q "Run_Sim_GeoOpt_Batch.C()"
-#root -b -l -q "Run_Reco_GeoOpt_Batch.C()"
-
 root -b -l -q "${macro_dir}/Run_Sim_GeoOpt_Batch.C()"
-root -b -l -q "${macro_dir}/Run_Rec_GeoOpt_Batch.C()"
+root -b -l -q "${macro_dir}/Run_Reco_GeoOpt_Batch.C()"
+root -b -l -q "${macro_dir}/Run_Ana_GeoOpt_Batch.C()"
 
 cp -v ${SGE_STDOUT_PATH} ${outdir}/log/${JOB_ID}.${SGE_TASK_ID}.log
 
