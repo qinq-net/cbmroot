ff99e10e54b101b806c6f073fa7242ef1ab3256a
v.friese <v.friese@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2015-05-05 10:55:59 +0000

    New version of realistic STS digitization (H. Malygina). Refs #130 @10h.

--- sts/digitize/CbmStsSensorTypeDssdReal.cxx
+++ sts/digitize/CbmStsSensorTypeDssdReal.cxx
@@ -8,7 +8,7 @@
 #include <iomanip>
 #include <fstream>
 #include <cstring>
-//#include <vector>
+#include <time.h>
 
 #include "TMath.h"
 #include "TRandom3.h"
@@ -35,49 +35,24 @@ using std::vector;
 // --- Energy for creation of an electron-hole pair in silicon [GeV]  ------
 const double kPairEnergy = 3.57142e-9;
 
-// --- Flags to switch on/off different physical processes in sensor -------
-static Bool_t fDiffusion = 1;
-static Bool_t fNonUniformity = 1;
-static Bool_t fCrossTalk = 1;
-static Bool_t fField = 1;
-
 // -----   Constructor   ---------------------------------------------------
-    CbmStsSensorTypeDssdReal::CbmStsSensorTypeDssdReal()
+CbmStsSensorTypeDssdReal::CbmStsSensorTypeDssdReal()
 : CbmStsSensorTypeDssd(), 
-    fVdepletion (), fVbias (), fTemperature (),
-    fCcoup (), fCinter (), fkTq (), fCTcoef (), fPhysics(CbmStsPhysics::Instance())
+    fPhysics(CbmStsPhysics::Instance()),
+	    fNonUniformity(1),
+	    fDiffusion(1),
+	    fCrossTalk(1),
+	    fLorentzShift(1)
 {
 }
 // -------------------------------------------------------------------------
 
-/*
-// -----   Print parameters   ----------------------------------------------
-void CbmStsSensorTypeDssdReal::Print(Option_t* opt) const {
-
-LOG(INFO) << "Properties of sensor type " << GetName() << ": "
-<< FairLogger::endl
-<< "\t  Dimensions: (" << fixed << setprecision(4)
-<< fDx << ", " << fDy << ", " << fDz << ") cm"
-<< FairLogger::endl
-<< "\t  Front side: pitch = "
-<< setprecision(0) << fPitch[0]*1.e4 << " mum, "
-<< fNofStrips[0] << " strips, stereo angle "
-<< setprecision(1) << fStereo[0] << " degrees"
-<< FairLogger::endl
-<< "\t  Back side:  pitch = "
-<< setprecision(0) << fPitch[1]*1.e4 << " mum, "
-<< fNofStrips[1] << " strips, stereo angle "
-<< setprecision(1) << fStereo[1] << " degrees"
-<< FairLogger::endl;
-
-}
-// -------------------------------------------------------------------------
-*/
 
 
 // -----   Process an MC Point  --------------------------------------------
 Int_t CbmStsSensorTypeDssdReal::ProcessPoint(CbmStsSensorPoint* point,
 	const CbmStsSensor* sensor) const {
+
 
     // --- Catch if parameters are not set
     if ( ! fIsSet ) {
@@ -120,9 +95,6 @@ Int_t CbmStsSensorTypeDssdReal::ProcessPoint(CbmStsSensorPoint* point,
 
 
 // -----   Produce charge on the strips   ----------------------------------
-//Int_t CbmStsSensorTypeDssdReal::ProduceCharge(CbmStsSensorPoint* point,
-//	Int_t side,
-//	const CbmStsSensor* sensor, Double_t * ELossLayerArray)
 Int_t CbmStsSensorTypeDssdReal::ProduceCharge(CbmStsSensorPoint* point,
 	Int_t side,
 	const CbmStsSensor* sensor, vector<Double_t> &ELossLayerArray)
@@ -143,8 +115,8 @@ const {
 
     // Total produced charge
     Double_t qtot = point->GetELoss() / kPairEnergy;
-    LOG(DEBUG4) << "Total produced charge (from Geant) =  " << 
-	qtot <<  FairLogger::endl;
+   if (side == 0) LOG(DEBUG) << "Total produced charge (from Geant) =  " << 
+	point -> GetELoss() * 1.e6 << " keV" << FairLogger::endl;
 
     // Stereo angle and strip pitch
     Double_t tanphi = fTanStereo[side];
@@ -174,13 +146,14 @@ const {
     // Calculate the length of trajectiry inside sensor 
     // (without taking into account the band in magnetic field)
     Double_t trajLength = sqrt ((locX2 - locX1) * (locX2 - locX1) + 
-	    fDz * fDz + (locY2 - locY1) * (locY2 - locY1));
+	    (locZ2 - locZ1) * (locZ2 - locZ1) + 
+	    (locY2 - locY1) * (locY2 - locY1));
 
 
     // shift in (or out) coordinate due to magnetic field      
     Double_t tanLor = 0.;
     Double_t field = point -> GetBy();
-    if (!fField)   field = 0;
+    if (!fLorentzShift)   field = 0.;
     if (side == 0) {// n-side
 	tanLor = 1650e-4 * field;// electrons
 	if (locZ1 < 0)	locX1 += fDz * tanLor;
@@ -238,8 +211,10 @@ const {
     Double_t * stripChargeCT;
     Double_t totalLossCharge = 0.;
 
-    // thickness of trajectory layer in cm, should be > 2 mum and < 10 mum
-    Double_t delta = 3e-4;
+    // thickness of trajectory layer in cm, should be > 2 mum and < 10 mum. 
+    // The trajectory is divided on whole number of steps with step length about 3 mum 
+    Double_t delta = trajLength / Int_t(trajLength / 3.e-4);
+    LOG(DEBUG4) << "delta = " << delta << " cm" << FairLogger::endl;
 
     // --- Get mass and charge
     Double_t mass   = 0.;
@@ -270,34 +245,44 @@ const {
     // --- Get stopping power
     Double_t dedx = fPhysics->StoppingPower(eKin, mass, charge, isElectron);
 
-    LOG(DEBUG3) << GetName() << ": PID " << point->GetPid() << ", mass "
-    		        << mass << " GeV, charge " << charge << ", Ekin " << eKin
-    		        << " GeV, <dE/dx> = " << 1000. * dedx << " MeV "
-    		        << FairLogger::endl;
+    LOG(DEBUG3) << GetName() << ": PID " << point -> GetPid() << ", mass "
+	<< mass << " GeV, charge " << charge << ", Ekin " << eKin
+	<< " GeV, <dE/dx> = " << 1000. * dedx << " MeV/cm "
+	<< FairLogger::endl;
 
 
     Double_t chargeLayerUni = qtot / (Int_t (trajLength / delta));
+    //calculate some values for thermal diffusion
+    Double_t kTq, Vdepl, Vbias;
+    kTq = sqrt (2. * sensor -> GetConditions().GetTemperatur() * 1.38e-4 / 1.6);
+    Vdepl = sensor -> GetConditions().GetVfd();
+    Vbias = sensor -> GetConditions().GetVbias();
 
-
-    TRandom3 * ELossRandom = new TRandom3();
+    Int_t layer_counter = 0;
     for (Int_t iLayer = 0; iLayer < Int_t(trajLength / delta); iLayer ++) {
 	if (side == 0){
 	    // energy loss calculation in eV; m, Ekin - in GeV, dEdx - in GeV/cm
-	    if (fNonUniformity) ELossLayerArray.push_back(EnergyLossLayer(ELossRandom, delta, mass, eKin, dedx));
+	    if (fNonUniformity) { 
+		Double_t lossInLayer = fPhysics -> EnergyLoss(delta, mass, eKin, dedx); 
+		ELossLayerArray.push_back(lossInLayer);
+	    }
 	    else ELossLayerArray.push_back(chargeLayerUni);
 	}
 
+	layer_counter ++;
 	totalLossCharge += ELossLayerArray[iLayer];// in eV 
 	// thermal diffusion calculation, result in eV
 	ThermalSharing(delta, stripCharge, ELossLayerArray[iLayer], 
-		locX1, locX2, locY1, locY2, locZ1, i1, i2, trajLength, iLayer, side); 
+	    locX1, locX2, locY1, locY2, locZ1, locZ2, i1, i2, trajLength, iLayer, side, kTq, Vdepl, Vbias); 
     }
-    LOG(DEBUG4) << "Total produced charge after energy loss = " << totalLossCharge * 1e-3 << " keV" << FairLogger::endl;
+if (side == 0) LOG(DEBUG4) << "Total produced charge after energy loss = " << totalLossCharge * 1e-3 << " keV" << FairLogger::endl;
+    LOG(DEBUG4) << "number of layers = " << layer_counter << ", side =  " << side << FairLogger::endl;
 
     if (fCrossTalk) {
+	Double_t CTcoef =  sensor -> GetConditions().GetCrossTalk();
 	stripChargeCT = new Double_t[nStrips];
 	memset(stripChargeCT, 0, nStrips * sizeof(Double_t) );
-	CrossTalk (stripCharge, stripChargeCT, nStrips, tanphi);
+	CrossTalk (stripCharge, stripChargeCT, nStrips, tanphi, CTcoef);
     }
     else stripChargeCT = stripCharge;
 
@@ -310,60 +295,26 @@ const {
 	    // normalize registered charge to total charge from simulation - qtot
 	    Double_t chargeNorm = stripChargeCT[iStrip] * qtot / totalLossCharge;// in e
 	    // --- Register charge to module
-	    RegisterCharge(sensor, side, iStrip, chargeNorm,
+	    RegisterCharge(sensor, side, iStrip, chargeNorm, 
 		    (point -> GetTime()));
 	    nSignals++;
 	}
     } // Loop over fired strips
 
+       //deleting pointers
+    delete stripCharge;
+    delete stripChargeCT;
+
+    
     return nSignals;
-}
-// -------------------------------------------------------------------------
-
-
-// -----   EnergyLoss calculation for 1 layer   -------------------------------
-Double_t CbmStsSensorTypeDssdReal::EnergyLossLayer (TRandom3 * ELossRandom, 
-	Double_t delta, Double_t m, Double_t Ekin, Double_t dEdx) const {
-
-    Double_t gamma = (Ekin + m) / m, beta2 = 1. - 1. / (gamma * gamma);
-    Double_t SiCharge = 14.;
-
-    // Model's parameters
-    Double_t r = 0.4, C = dEdx * 1e9;//eV
-    Double_t I = 16. * pow (SiCharge, 0.9), Emax = 1e6;//eV
-    Double_t f2 = 2. / SiCharge, f1 = 1. - f2;
-    Double_t E2 = 10. * SiCharge * SiCharge, E1 = pow (I / pow (E2, f2), 1. / f1);//eV
-
-    //Model's variables' definition
-    Double_t sigma1 = C * f1 / E1 * ( log (2.0 * m * beta2 * gamma * gamma / E1) - beta2) /
-	( log (2.0 * m * beta2 * gamma * gamma / I) - beta2) * (1.0 - r);
-    Double_t sigma2 = C * f2 / E2 * ( log (2.0 * m * beta2 * gamma * gamma / E2) - beta2) /
-	( log (2.0 * m * beta2 * gamma * gamma / I) - beta2) * (1.0 - r);
-    Double_t sigma3 = C * Emax / (I * (Emax + I) * log( (Emax + I) / I)) * r;
-
-
-    //   LOG(INFO) << "before " << FairLogger::endl;
-    Int_t n1 = ELossRandom -> Poisson(sigma1 * delta);
-    //   LOG(INFO) << "n1 = " << n1 << FairLogger::endl;
-    Int_t n2 = ELossRandom -> Poisson(sigma2 * delta);
-    Int_t n3 = ELossRandom -> Poisson(sigma3 * delta);
-
-    Double_t EnergyLossExit = n1 * E1 + n2 * E2, EnergyLossIon = 0.;
-
-    for (Int_t j = 1; j <= n3; j++){
-	Double_t u = ELossRandom -> Uniform(1);
-	EnergyLossIon += I / (1 - u * Emax / (Emax + I));
-    }
-
-    return EnergyLossIon + EnergyLossExit;//eV
 }
 // -------------------------------------------------------------------------
 
 
 // -----   Thermal diffusion    -----------------------------
 void CbmStsSensorTypeDssdReal::ThermalSharing (Double_t delta, Double_t * stripCharge, Double_t chargeLayer, 
-	Double_t locX1, Double_t locX2, Double_t locY1, Double_t locY2, Double_t locZ1,
-	Int_t i1, Int_t i2, Double_t trajLength, Int_t iLayer, Int_t side) const {
+	Double_t locX1, Double_t locX2, Double_t locY1, Double_t locY2, Double_t locZ1, Double_t locZ2,
+	Int_t i1, Int_t i2, Double_t trajLength, Int_t iLayer, Int_t side, Double_t kTq, Double_t Vdepletion, Double_t Vbias) const {
 
     // Stereo angle and strip pitch
     Double_t tanphi = fTanStereo[side];
@@ -372,20 +323,20 @@ void CbmStsSensorTypeDssdReal::ThermalSharing (Double_t delta, Double_t * stripC
 
     Double_t xLayer = locX1 + (iLayer + 0.5) * delta * (locX2 - locX1) / trajLength;
     Double_t yLayer = locY1 + (iLayer + 0.5) * delta * (locY2 - locY1) / trajLength;
-    Double_t zLayer = (iLayer + 0.5) * delta * fDz / trajLength;
+    Double_t zLayer = locZ1 + (iLayer + 0.5) * delta * (locZ2 - locZ1) / trajLength;
+    //Double_t zLayer = (iLayer + 0.5) * delta * fDz / trajLength;
 
 
     Double_t tau, sigmaLayer;
     if (fDiffusion) {
 	if ( (side == 0 && locZ1 > 0) || (side == 1 && locZ1 < 0) ) zLayer = fDz - zLayer;  
-	tau = fDz * fDz / (2 * fVdepletion) * log ((fVbias + fVdepletion) / (fVbias - fVdepletion) * 
-		(1 - 2 * zLayer / fDz * fVdepletion / (fVbias + fVdepletion))); 
-	sigmaLayer = fkTq * sqrt(tau); 
+	tau = fDz * fDz / (2. * Vdepletion) * log ((Vbias + Vdepletion) / (Vbias - Vdepletion) * 
+		(1. - 2. * zLayer / fDz * Vdepletion / (Vbias + Vdepletion))); 
+	sigmaLayer = kTq * sqrt(tau); 
     }
 
     Double_t * stripChargeSh = new Double_t[i2 - i1 + 3];
     memset(stripChargeSh, 0, (i2 - i1 + 3) * sizeof(Double_t) );
-    //for (Int_t i = 0; i < (i2 - i1 + 3); i++) stripChargeSh[i] = 0.;
     if ( (xLayer >= 0) && (xLayer <= fDx) ){//layer inside sensor
 
 	for (Int_t iStrip = i1; iStrip <= i2; iStrip++){
@@ -404,29 +355,14 @@ void CbmStsSensorTypeDssdReal::ThermalSharing (Double_t delta, Double_t * stripC
 		    Double_t edgeLeft = iro * pitch + tanphi * (fDy - yLayer);
 		    Double_t edgeRight = (iro + 1) * pitch + tanphi * (fDy - yLayer);
 
-		    /*	    if ( (xLayer - 20 * 1e-4) < iStrip * pitch + tanphi * (fDy - yLayer)) {
-			    chargePrev = chargeLayer * 0.5 * (1 + TMath::Erf( (iStrip * pitch + 
-			    tanphi * (fDy - yLayer) - xLayer) / (sqrt(2) * sigmaLayer) ));
-			    if (edgeLeft >= pitch / 2 && chargePrev > 0) {stripChargeSh[iStripShifted - 1] += chargePrev;
-			    LOG(DEBUG4) << "chargePrev = " << chargePrev << FairLogger::endl;
-			    }
-			    }
-			    if ( (xLayer + 20 * 1e-4) > (iStrip + 1) * pitch + tanphi * (fDy - yLayer) ) {
-			    chargeNext = chargeLayer * 0.5 * TMath::Erfc( ((iStrip + 1) * pitch + 
-			    tanphi * (fDy - yLayer) - xLayer) / (sqrt(2) * sigmaLayer) );
-			    if (edgeRight <= fDx - pitch / 2 && chargeNext > 0) {stripChargeSh[iStripShifted + 1] += chargeNext;
-			    LOG(DEBUG4) << "chargeNext = " << chargeNext <<  FairLogger::endl;
-			    }
-			    } */
-
-		    if ( (xLayer - 20 * 1e-4) < edgeLeft) {
-			chargePrev = chargeLayer * 0.5 * (1 + TMath::Erf(( edgeLeft - xLayer) / (sqrt(2) * sigmaLayer) ));
-			if (edgeLeft >= pitch / 2 && chargePrev > 0) {stripChargeSh[iStripShifted - 1] += chargePrev;
+		   if ( (xLayer - 20. * 1e-4) < edgeLeft) {
+			chargePrev = chargeLayer * 0.5 * (1. + TMath::Erf(( edgeLeft - xLayer) / (sqrt(2.) * sigmaLayer) ));
+			if (edgeLeft >= pitch / 2. && chargePrev > 0) {stripChargeSh[iStripShifted - 1] += chargePrev;
 			}
 		    }
-		    if ( (xLayer + 20 * 1e-4) > edgeRight ) {
-			chargeNext = chargeLayer * 0.5 * TMath::Erfc( (edgeRight - xLayer) / (sqrt(2) * sigmaLayer));
-			if (edgeRight <= fDx - pitch / 2 && chargeNext > 0) {stripChargeSh[iStripShifted + 1] += chargeNext;
+		    if ( (xLayer + 20. * 1e-4) > edgeRight ) {
+			chargeNext = chargeLayer * 0.5 * TMath::Erfc( (edgeRight - xLayer) / (sqrt(2.) * sigmaLayer));
+			if (edgeRight <= fDx - pitch / 2. && chargeNext > 0) {stripChargeSh[iStripShifted + 1] += chargeNext;
 			}
 		    } 
 		    stripChargeSh[iStripShifted] += chargeLayer - chargePrev - chargeNext;
@@ -443,114 +379,33 @@ void CbmStsSensorTypeDssdReal::ThermalSharing (Double_t delta, Double_t * stripC
 	    stripCharge[iStrip] += stripChargeSh[iStripShifted];
 	}
     }
+    delete stripChargeSh;
 }
 // --------------------------------------------------------------------
 
 
 // -----   Cross-talk   -----------------------------------------------
 void CbmStsSensorTypeDssdReal::CrossTalk(Double_t * stripCharge, Double_t * stripChargeCT, 
-	Int_t nStrips, Double_t tanphi) const {
+	Int_t nStrips, Double_t tanphi, Double_t CTcoef) const {
 
     for (Int_t iStrip = 1; iStrip < nStrips - 1; iStrip ++) // loop over all strips except first and last one
-	stripChargeCT[iStrip] = stripCharge[iStrip] * (1 - 2 * fCTcoef) + 
-	    (stripCharge[iStrip - 1] + stripCharge[iStrip + 1]) * fCTcoef;
+	stripChargeCT[iStrip] = stripCharge[iStrip] * (1 - 2 * CTcoef) + 
+				(stripCharge[iStrip - 1] + stripCharge[iStrip + 1]) * CTcoef;
 
     if (tanphi != 0){// there are cross-connections
-	stripChargeCT[0] = stripCharge[0] * (1 - 2 * fCTcoef) + 
-	    (stripCharge[nStrips - 1] + stripCharge[1]) * fCTcoef;//first strip
-	stripChargeCT[nStrips - 1] = stripCharge[nStrips - 1] * (1 - 2 * fCTcoef) + 
-	    (stripCharge[0] + stripCharge[nStrips - 2]) * fCTcoef; // last strip
+	stripChargeCT[0] = stripCharge[0] * (1 - 2 * CTcoef) + 
+			   (stripCharge[nStrips - 1] + stripCharge[1]) * CTcoef;//first strip
+	stripChargeCT[nStrips - 1] = stripCharge[nStrips - 1] * (1 - 2 * CTcoef) + 
+			    (stripCharge[0] + stripCharge[nStrips - 2]) * CTcoef; // last strip
     }
     if (tanphi == 0){// no cross-connections
-	stripChargeCT[0]           = stripCharge[0] * (1 - fCTcoef) 
-	    + stripCharge[1] * fCTcoef;//first strip
-	stripChargeCT[nStrips - 1] = stripCharge[nStrips - 1] * (1 - fCTcoef) 
-	    + stripCharge[nStrips - 2] * fCTcoef; // last strip
+	stripChargeCT[0]           = stripCharge[0] * (1 - CTcoef) 
+				   + stripCharge[1] * CTcoef;//first strip
+	stripChargeCT[nStrips - 1] = stripCharge[nStrips - 1] * (1 - CTcoef) 
+		        	    + stripCharge[nStrips - 2] * CTcoef; // last strip
     }
 }
 // -------------------------------------------------------------------------
-
-
-// -----   Stopping power   -----------------------------------------------
-Double_t CbmStsSensorTypeDssdReal::StoppingPower(Bool_t fElectron, 
-	Double_t mx, Double_t Ex, Int_t zx) const {
-
-    Double_t mp = 0.938, E, dEdx;//[GeV]
-    ifstream inFile;
-    TString dataDir = gSystem -> Getenv("VMCWORKDIR");
-    dataDir += "/sts/digitize";
-
-    //calculate dEdx
-    if (fElectron){ 
-    	E = Ex;
-	if (E <= 10){//read from electron table, we have table only till E = 10 GeV
-	    LOG(DEBUG2) << "it is ELECTRON" << FairLogger::endl;
-	    inFile.open(dataDir + "/dEdx_Si_e.txt");
-	    if(inFile.is_open()){
-		LOG(DEBUG2) << "opened file" << FairLogger::endl;
-		Double_t Etemp = 0., dEdxtemp = 0.;
-		while (Etemp <= E){
-		    inFile >> Etemp;//E, Mev
-		    inFile >> dEdxtemp;//dEdx, MeV/...
-		    Etemp *= 1e-3;
-		}
-		LOG(DEBUG4) << "dE/dx =  " << dEdxtemp << " MeV/..." <<  Etemp << FairLogger::endl;
-		dEdx = dEdxtemp * 1e-3 * 2.33;
-		inFile.close();
-	    }else{
-		LOG(ERROR) << "File with dE/dx for electrons WAS NOT opened." << FairLogger::endl;}
-
-	} else {
-	    dEdx = 100e-3 * 2.33;// convert to MeV/cm, rho(Si) = 2.33 g/cm^2 
-	}
-    } else {//read from proton table
-	E = Ex * mp / mx;
-	if (E >= 10) {//for energy >= 10 GeV assume dEdx to be const
-	    dEdx = zx * zx * 2e-3 * 2.33;// convert to MeV/cm, rho(Si) = 2.33 g/cm^2 
-	} else {
-	    inFile.open(dataDir + "/dEdx_Si_p.txt");
-	    if(inFile.is_open()){
-		Double_t Etemp = 0., dEdxtemp = 0.;
-		//int counter = 0;
-		while (Etemp <= E){
-		    inFile >> Etemp;//E, Mev
-		    inFile >> dEdxtemp;//dEdx, MeV/...
-		    Etemp *= 1e-3;
-		    //	counter++;
-		}
-		LOG(DEBUG4) << "dE/dx =  " << dEdxtemp << " MeV/..." <<  Etemp << "  " << E << FairLogger::endl;
-		dEdx = dEdxtemp * 1e-3 * zx * zx * 2.33; // convert to MeV/cm, rho(Si) = 2.33 g/cm^2
-		inFile.close();
-	    }else{
-		LOG(ERROR) << "File with dE/dx for protons WAS NOT opened." << FairLogger::endl;
-	    }
-	}
-    }
-    return dEdx;
-}
-// -------------------------------------------------------------------------
-
-
-// -----   Set the parameters   --------------------------------------------
-void CbmStsSensorTypeDssdReal::SetParameters(Double_t dx, Double_t dy,
-	Double_t dz, Int_t nStripsF,
-	Int_t nStripsB, Double_t stereoF,
-	Double_t stereoB,
-	Double_t Vdep, Double_t Vbias, Double_t temperature,
-	Double_t Ccoup, Double_t Cinter) {
-
-    // --- Set members
-    fVdepletion   = Vdep;
-    fVbias        = Vbias;
-    fTemperature  = temperature;
-    fCcoup        = Ccoup;
-    fCinter       = Cinter;
-    fkTq = sqrt (2. * temperature * 1.38e-4 / 1.6);
-    fCTcoef = Cinter / (Cinter + Ccoup);
-    CbmStsSensorTypeDssd::SetParameters(dx, dy, dz, nStripsF, nStripsB, stereoF, stereoB);   
-}
-// -------------------------------------------------------------------------
-
 
 
 ClassImp(CbmStsSensorTypeDssdReal)
