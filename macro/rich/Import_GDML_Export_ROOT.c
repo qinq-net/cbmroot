537afb1c97a9f81e57fb37f71d3c707bee467d32
s.lebedev <s.lebedev@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2015-05-07 09:35:56 +0000

    Macro updates in rich directory

--- macro/rich/Import_GDML_Export_ROOT.c
+++ macro/rich/Import_GDML_Export_ROOT.c
@@ -1,30 +1,27 @@
-void Import_GDML_Export_ROOT(/*TString FileName*/)
+void Import_GDML_Export_ROOT()
 {
-  TString FileName = "/geometry/rich/rich_v14d";
-  TString InFile=FileName+".gdml";
-  TString OutFile=FileName+".root";
-  TString vmcDir = TString(gSystem->Getenv("VMCWORKDIR"));
-  
-  TGeoManager *gdml = new TGeoManager("gdml", "FAIRGeom");
-  
-  TGDMLParse parser;
-  // Define your input GDML file HERE
-  TGeoVolume* gdmlTop = parser.GDMLReadFile (vmcDir + InFile);
-  TGeoVolume* rootTop = new TGeoVolumeAssembly("TOP");
-  
-  gGeoManager->SetTopVolume(rootTop);
-  
-  // Define your position HERE
-  TGeoRotation* rot = new TGeoRotation ("rot", 0., 0., 0.);
-  TGeoCombiTrans* posrot = new TGeoCombiTrans (0., 0., 270., rot);
-  
-  rootTop->AddNode (gdmlTop, 1, posrot);
-  
-  gGeoManager->CloseGeometry();
-  
-  // Define you output ROOT file HERE
-  TFile* outfile = new TFile(vmcDir + OutFile, "RECREATE");
-  rootTop->Write();
-  outfile->Close();
+	TString vmcDir = TString(gSystem->Getenv("VMCWORKDIR"));
+
+	TGeoManager *gdml = new TGeoManager("gdml", "FAIRGeom");
+
+	TGDMLParse parser;
+	// Define your input GDML file HERE
+	TGeoVolume* gdmlTop = parser.GDMLReadFile (vmcDir + "/geometry/rich/2015_plus1deg.gdml");
+	TGeoVolume* rootTop = new TGeoVolumeAssembly("TOP");
+
+	gGeoManager->SetTopVolume(rootTop);
+
+	// Define your position HERE
+	TGeoRotation* rot = new TGeoRotation ("rot", 0., 0., 0.);
+	TGeoCombiTrans* posrot = new TGeoCombiTrans (0., 0., 270., rot);
+
+	rootTop->AddNode (gdmlTop, 1, posrot);
+
+	gGeoManager->CloseGeometry();
+
+	// Define you output ROOT file HERE
+	TFile* outfile = new TFile(vmcDir + "/geometry/rich/2015_plus1deg.root", "RECREATE");
+	rootTop->Write();
+	outfile->Close();
 }
 
