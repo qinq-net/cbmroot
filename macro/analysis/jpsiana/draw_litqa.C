0c3e5129abdca117e3e7e06efd4c10c1bc36ee35
s.lebedev <s.lebedev@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2015-06-19 15:17:10 +0000

    Add litqa drawing macro for jpsi nalysis

---
+++ macro/analysis/jpsiana/draw_litqa.C
@@ -0,0 +1,34 @@
+void draw_litqa()
+{
+   gROOT->LoadMacro("$VMCWORKDIR/macro/littrack/loadlibs.C");
+   loadlibs();
+
+   std::string dir = "/Users/slebedev/Development/cbm/data/jpsi/";
+   std::string fileName = dir + "litqa.auau.25gev.centr.all.root";
+
+   std::string script = std::string(TString(gSystem->Getenv("SCRIPT")).Data());
+    if (script == "yes"){
+      dir = std::string(TString(gSystem->Getenv("LMVM_MAIN_DIR")).Data());
+      fileName = dir + std::string(TString(gSystem->Getenv("LMVM_LITQA_FILE_NAME")).Data());
+   }
+
+   std::string outputDirTracking = dir + "results_litqa_tracking/";
+   std::string outputDirClustering = dir + "results_litqa_clustering/";
+   gSystem->mkdir(outputDirTracking.c_str(), true);
+   gSystem->mkdir(outputDirClustering.c_str(), true);
+
+   CbmSimulationReport* trackingQaReport = new CbmLitTrackingQaReport();
+   trackingQaReport->Create(fileName, outputDirTracking);
+
+//   CbmSimulationReport* clusteringQaReport = new CbmLitClusteringQaReport();
+//   clusteringQaReport->Create(fileName, outputDirClustering);
+
+//   CbmSimulationReport* fitQaReport = new CbmLitFitQaReport();
+//   fitQaReport->Create(fileName, outputDir);
+
+//   CbmLitRadLengthQaReport* radLengthQaReport = new CbmLitRadLengthQaReport();
+//   radLengthQaReport->Create(fileName, outputDir);
+
+//   CbmSimulationReport* tofQaReport = new CbmLitTofQaReport();
+//   tofQaReport->Create(fileName, outputDir);
+}
