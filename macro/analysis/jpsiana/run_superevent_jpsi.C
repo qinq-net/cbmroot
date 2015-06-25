189b579d072cd55d4bdf9d3a260aa55b3ee1c7f4
s.lebedev <s.lebedev@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2015-06-25 11:33:57 +0000

    macro for running superevent

---
+++ macro/analysis/jpsiana/run_superevent_jpsi.C
@@ -0,0 +1,14 @@
+
+void run_superevent_jpsi()
+{
+	TStopwatch timer;
+	timer.Start();
+	gROOT->LoadMacro("$VMCWORKDIR/macro/littrack/loadlibs.C");
+	loadlibs();
+
+	CbmAnaJpsiSuperEvent* se = new CbmAnaJpsiSuperEvent();
+	se->AddFile("/Users/slebedev/Development/cbm/data/jpsi/analysis.0001.root");
+
+	se->Run();
+}
+
