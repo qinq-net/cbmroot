a8233c1995bc8b8bf3421a9eb10006dc421de9aa
d.emschermann <d.emschermann@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2015-08-04 07:05:20 +0000

    switch to MVD v18b as default and move TRD and TOF 200 cm upstream

--- geometry/setup/sis18_mcbm_setup.C
+++ geometry/setup/sis18_mcbm_setup.C
@@ -49,10 +49,10 @@ void sis18_mcbm_setup()
   fieldSymType =  3;
 
   // -----  Tags  -----------------------------------------------------------
-  mvdTag       = "v18a";
+  mvdTag       = "v18b";     // "v18a";
   stsTag       = "v18e";     // "v18d";
-  trdTag       = "v18a_1e";  // "v18b_1e";
-  tofTag       = "v13_5a";   // "v13_6a";
+  trdTag       = "v18b_1e";  // "v18a_1e";
+  tofTag       = "v13_6a";   // "v13_5a"; 
   
   // -----  DigiFiles  ------------------------------------------------------
   stsDigi      = "mcbm/sts_" + stsTag + "_std.digi.par";
