d6ed111c126da12532128870f7752bb0cfb37561
d.emschermann <d.emschermann@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2015-08-04 09:53:30 +0000

    update event displays

--- macro/run/eventDisplay.C
+++ macro/run/eventDisplay.C
@@ -10,32 +10,35 @@ void eventDisplay(const char* setup = "sis100_electron")
 
   fRun->SetInputFile(InputFile.Data());
   fRun->SetOutputFile(dataDir + setup + "_test.root");
-//  fRun->SetOutputFile("data/test.root");
 
   FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
   FairParRootFileIo* parInput1 = new FairParRootFileIo();
   parInput1->open(ParFile.Data());
   rtdb->setFirstInput(parInput1);
 
-  FairEventManager *fMan= new FairEventManager();
-  FairMCTracks *Track =  new FairMCTracks ("Monte-Carlo Tracks");
-  FairMCPointDraw *RichPoint =   new FairMCPointDraw ("RichPoint",kOrange,  kFullSquare);
-  FairMCPointDraw *TofPoint =    new FairMCPointDraw ("TofPoint",kBlue,  kFullSquare);
-  FairMCPointDraw *TrdPoint= new FairMCPointDraw ("TrdPoint",kTeal,  kFullSquare);
-  FairMCPointDraw *EcalPoint =    new FairMCPointDraw ("EcalPoint",kYellow,  kFullSquare);
-  FairMCPointDraw *RefPlanePoint = new FairMCPointDraw ("RefPlanePoint",kPink,  kFullSquare);
-  FairMCPointDraw *StsPoint = new FairMCPointDraw ("StsPoint",kCyan,  kFullSquare);
+  FairEventManager *fMan  = new FairEventManager();
+  FairMCTracks     *Track = new FairMCTracks ("Monte-Carlo Tracks");
+
+  FairMCPointDraw  *MvdPoint      = new FairMCPointDraw ("MvdPoint",      kBlack,  kFullSquare);
+  FairMCPointDraw  *StsPoint      = new FairMCPointDraw ("StsPoint",      kBlue,   kFullSquare);
+  FairMCPointDraw  *RichPoint     = new FairMCPointDraw ("RichPoint",     kOrange, kFullSquare);
+  FairMCPointDraw  *RefPlanePoint = new FairMCPointDraw ("RefPlanePoint", kPink,   kFullSquare);
+  FairMCPointDraw  *TrdPoint      = new FairMCPointDraw ("TrdPoint",      kCyan,   kFullSquare);
+  FairMCPointDraw  *TofPoint      = new FairMCPointDraw ("TofPoint",      kGreen,  kFullSquare);
+  FairMCPointDraw  *EcalPoint     = new FairMCPointDraw ("EcalPoint",     kYellow, kFullSquare);
                                                                
   fMan->AddTask(Track);
   
-  fMan->AddTask(RichPoint);
-  fMan->AddTask(EcalPoint);   
-  fMan->AddTask(TofPoint);   
-  fMan->AddTask(TrdPoint);
-  fMan->AddTask(RefPlanePoint);
+  fMan->AddTask(MvdPoint);
   fMan->AddTask(StsPoint);
+  fMan->AddTask(RichPoint);
+  fMan->AddTask(RefPlanePoint);
+  fMan->AddTask(TrdPoint);
+  fMan->AddTask(TofPoint);   
+  fMan->AddTask(EcalPoint);   
  
     
   //  fMan->Init(1,4,10000);
-  fMan->Init(1,5,10000);  // make STS sensors visible by default
+  fMan->Init(1,5,10000);  // make STS visible by default
+  //  fMan->Init(1,6,10000);  // make MVD visible by default
 }
