037b12eec3d83b428ba17389a5ba99be6ed6a316
f.uhlig <f.uhlig@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2015-09-08 09:28:24 +0000

    Move functions needed for CTest runtime dependency generation from the macros in rootalias.C. This file is loaded automatically when starting a root session. The location of this file is defined in the new .rootrc file. The .rootrc file must be copied also to the directory from which root is executed during the automatic tests.

---
+++ macro/include/rootalias.C
@@ -0,0 +1,24 @@
+// Functions needed for CTest runtime dependency
+// The only way to introduce such a runtime dependency is via a file
+// which is created at the end of the macro if everything worked as
+// expected
+void Generate_CTest_Dependency_File(TString filename)
+{
+  TString touchCommand = "touch " + filename;
+  gSystem->Exec(touchCommand);
+}
+
+TString Remove_CTest_Dependency_File(TString outDir, TString macroName, const char* setup)
+{
+  TString _setup(setup);
+  TString testDir = outDir;
+  TString testFile = macroName + "_" + _setup + "_ok";
+
+  if (gSystem->FindFile(testDir, testFile)) {
+    TString rmCommand = "rm " + testFile;
+    gSystem->Exec(rmCommand);
+  }
+  TString depFile = outDir + "/" + macroName + "_" + _setup + "_ok";
+  depFile.ReplaceAll("//", "/");
+  return depFile;
+}
