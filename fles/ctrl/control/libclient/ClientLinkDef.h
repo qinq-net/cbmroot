cc8347377188a8170b5fbf7586bd71540b5c8bfe
p.-a.loizeau <p.-a.loizeau@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2015-09-24 13:18:45 +0000

    Disable the generation of streamer in ROOT dictionary for the ControlClient class. refs #367 @2h

--- fles/ctrl/control/libclient/ClientLinkDef.h
+++ fles/ctrl/control/libclient/ClientLinkDef.h
@@ -6,6 +6,6 @@
 #pragma link C++ class CbmNet::ListSeqItem+;
 #pragma link C++ class CbmNet::ListPar+;
 #pragma link C++ class CbmNet::ListParItem+;
-#pragma link C++ class CbmNet::ControlClient+;
+#pragma link C++ class CbmNet::ControlClient-;
 
 #endif
