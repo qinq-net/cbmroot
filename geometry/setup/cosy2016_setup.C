b6e93d9351c26a30a1873b1b11bf3c6a5afd4174
d.emschermann <d.emschermann@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2015-10-02 10:16:27 +0000

    add COSY 2016 setup

---
+++ geometry/setup/cosy2016_setup.C
@@ -0,0 +1,96 @@
+//
+//   comments
+//
+
+// 2015-06-18 - DE - mCBM @ SIS18 setup
+//
+
+/*
+static TString caveGeom;
+static TString pipeGeom;
+static TString magnetGeom;
+static TString mvdGeom;
+static TString stsGeom;
+static TString richGeom;
+static TString muchGeom;
+static TString shieldGeom;
+static TString trdGeom;
+static TString tofGeom;
+static TString ecalGeom;
+static TString platformGeom;
+static TString psdGeom;
+static Double_t psdZpos;
+static Double_t psdXpos;
+
+static TString mvdTag;
+static TString stsTag;
+static TString trdTag;
+static TString tofTag;
+
+static TString stsDigi;
+static TString trdDigi;
+static TString tofDigi;
+
+static TString mvdMatBudget;
+static TString stsMatBudget;
+
+static TString  fieldMap;
+static Double_t fieldZ;
+static Double_t fieldScale;
+static Int_t    fieldSymType;
+
+static TString defaultInputFile;
+*/
+
+void cosy2016_setup()
+{
+  // -----   Magnetic field   -----------------------------------------------
+  fieldMap     = "field_v12b";   // name of field map
+  fieldZ       = 40.;            // field centre z position
+  fieldScale   =  0.; // 1.;     // field scaling factor
+  fieldSymType =  3;
+
+  // -----  Tags  -----------------------------------------------------------
+  mvdTag       = "v18b";     // "v18a";
+  stsTag       = "v16a";     // "v18d";
+  trdTag       = "v18b_1e";  // "v18a_1e";
+  tofTag       = "v13_6a";   // "v13_5a"; 
+  
+  // -----  DigiFiles  ------------------------------------------------------
+  stsDigi      = "mcbm/sts_" + stsTag + "_std.digi.par";
+  trdDigi      = "mcbm/trd_" + trdTag + ".digi.par";
+  tofDigi      = "mcbm/tof_" + tofTag + ".digi.par";
+
+  // -----  Material budget files -------------------------------------------
+  mvdMatBudget = "mcbm/mvd_" + mvdTag + "_matbudget.root";
+  stsMatBudget = "mcbm/sts_" + stsTag + "_matbudget.root";
+
+  // -----  Geometries  -----------------------------------------------------
+  caveGeom     = "mcbm/cave_mcbm.geo";
+  pipeGeom     = "mcbm/pipe_v18b.root";
+  magnetGeom   = ""; // magnet/magnet_v15a.geo.root";
+  mvdGeom      = "mcbm/mvd_" + mvdTag + ".geo.root";
+  stsGeom      = "mcbm/sts_" + stsTag + ".geo.root";
+  richGeom     = ""; //no reco// "mcbm/rich_prototype_v18b.geo";
+  muchGeom     = ""; //no reco// "mcbm/much_v18a.geo";
+  shieldGeom   = "";
+  platformGeom = ""; // "passive/platform_v13a.geo";
+  trdGeom      = ""; // "mcbm/trd_" + trdTag + ".geo.root";
+  tofGeom      = ""; // "mcbm/tof_" + tofTag + ".geo.root";
+  ecalGeom     = "";
+  psdGeom      = ""; // "psd_geo_xy.txt";
+  psdZpos = 800.;
+  psdXpos = 11.;
+
+  // ------ Input file ------------------------------------------------------
+
+  defaultInputFile = "/input/urqmd.agag.1.65gev.centr.00001.root";
+  //  defaultInputFile = "/input/urqmd.agag.1.65gev.mbias.00001.root";
+  //  defaultInputFile = "/input/urqmd.nini.1.93gev.centr.00001.root";
+  //  defaultInputFile = "/input/urqmd.nini.1.93gev.mbias.00001.root";
+  //  defaultInputFile = "/input/urqmd.pau.4.5gev.mbias.00001.root";
+
+  cout << "Reading geometry setup and magnetic field settings from "
+       << gSystem->UnixPathName(gInterpreter->GetCurrentMacroName())
+       << endl;
+}
