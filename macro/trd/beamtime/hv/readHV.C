5c24d02a45b36acac0edb9f7cbd25d97739a0f3a
c.bergmann <c.bergmann@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2015-10-29 07:38:29 +0000

    ifstream cast matches now file format

--- macro/trd/beamtime/hv/readHV.C
+++ macro/trd/beamtime/hv/readHV.C
@@ -1,34 +1,65 @@
 #include "Riostream.h"
 #include "TString.h"
 #include <map>
+#include "TH1.h"
+#include "TH2.h"
 void readHV(TString inFile="exampleHV.txt")
 {
+  Bool_t debug = false;
   ifstream in;
   in.open(inFile);
-  Int_t time_high(-1), time_low(-1); 
-  Int_t chID(-1);
+  Int_t time_high(-1), time_low(-1), chID(-1), lineLength(0);
   Float_t voltage(0.0), current(0.0);
   
   TString line("");
+  TString sTime(""), sChID(""), sVoltage(""), sCurrent("");
   char cline[500];
   if (!in){
     cout << inFile << " not found!" << endl;
   } else {
     cout << inFile << " found!" << endl;
+    Int_t nLines = 0;
     while(in){
+      //sTime = "";
       in.getline(cline,500);
       line = cline;
-      cout << line << endl;
-      TString sTime(line(0,13)),sChID(line(48,3)),sVoltage(line(69,12)),sCurrent(line(131,12));
+      lineLength = (Int_t)line.Length();
+      line.ReplaceAll("= Opaque: Float: ",""); 
+      if (line.EndsWith("A")){
+	line.ReplaceAll("WIENER-CRATE-MIB::outputMeasurementCurrent.u","");
+	line.ReplaceAll(" A","");
+	if (debug)
+	  cout << "A: ";
+	sCurrent = line(4,lineLength-4);
+      } else if (line.EndsWith("V")){
+
+	line.ReplaceAll("WIENER-CRATE-MIB::outputMeasurementTerminalVoltage.u","");
+	line.ReplaceAll(" V","");
+	if (debug)
+	  cout << "V: ";
+	sVoltage = line(4,lineLength-4);
+	sCurrent = "";
+      } else {
+	if (debug)
+	  cout << "T: ";
+	sTime = line;
+      }
+      if (debug)
+	cout << lineLength << ":   " << line << endl;
+
+      sChID = line(0,3);
       chID = sChID.Atoi();
       voltage = sVoltage.Atof();
       current = sCurrent.Atof();
-      cout << sTime  << endl;
-      cout << chID << endl;
-      cout << voltage << endl;
-      cout << current << endl;
-
+      if (debug){
+	cout << "T=" << sTime  << endl;
+	cout << "C=" << chID << endl;
+	cout << "V=" << voltage << endl;
+	cout << "A=" << current << endl;
       }
+      nLines++;
+    }
+    cout << "found " << nLines << " lines in file " << inFile << endl;
   }
 
 }
