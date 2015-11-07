b66656d8257382dd9d38aed98844fc5c03d1a350
t.mahmoud <t.mahmoud@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2015-11-07 15:42:49 +0000

    new code

--- macro/rich/geotest/RotPMT/Run_Sim_GeoOpt_Batch.C
+++ macro/rich/geotest/RotPMT/Run_Sim_GeoOpt_Batch.C
@@ -376,7 +376,7 @@ TString  GetDimentionText(int DimCase, int EnlargedPMTWidth, int EnlargedPMTHigh
 
 ////////////////////////////////////////////////////////
 TString GetRICH_GeoFile( char *RotMirText, TString PMTRotText, TString PMTTransText, int GeoCase, TString PMTDimsText,TString ExtraText){
-  //return "rich/minus10deg_ext_mirror_update_Mod.gdml";
+  return "rich/minus10deg_ext_mirror_update_Mod.gdml";
   // return "rich/minus10deg_ext_mirror.gdml";
   //GeoCase=-2 ==> old geometry with rich_v08a.geo (RICH starts at 1600, Mirror tilt -1)
   //GeoCase=-1 ==> old geometry with rich_v14a.gdml (RICH starts at 1800, Mirror tilt -1)
