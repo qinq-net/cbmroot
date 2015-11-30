af5c893369f5c7dffe14213fdb4aaf4914b9f80f
d.emschermann <d.emschermann@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2015-11-30 16:08:09 +0000

    allow selection of the root version to compile

--- autoinstall_framework.sh
+++ autoinstall_framework.sh
@@ -19,6 +19,10 @@
 # ./autoinstall_framework.sh 1 1 1
 #-------------------------------------
 
+# choose your root verion
+#export ROOTVER=6
+export ROOTVER=5
+
 # put your desired variants here:
 #export FSOFTDEV=jul15p2  # nov15p1
 export FSOFTDEV=jul15p3  # nov15p1
@@ -28,7 +32,7 @@ export FROOTDEV=v-15.11
 export FSOFTPRO=jul15p3
 export FROOTPRO=v-15.07a 
 
-# former verions
+# former versions
 # fairsoft: mar15p2
 # fairroot: v-15.03a
 
@@ -105,15 +109,20 @@ if [ $SETUP_FAIRSOFT -ge 1 ]; then
   echo "Setting up Fairsoft ..."
 
   cd ..
-  git clone https://github.com/FairRootGroup/FairSoft fairsoft_$FSOFTVER
-  cd fairsoft_$FSOFTVER
+  git clone https://github.com/FairRootGroup/FairSoft fairsoft_${FSOFTVER}_root{$ROOTVER}
+  cd fairsoft_${FSOFTVER}_root{$ROOTVER}
   git tag -l
   git checkout -b $FSOFTVER $FSOFTVER
+
   #emacs -nw automatic.conf
   #./configure.sh automatic.conf
-  sed s/compiler=/compiler=gcc/ automatic.conf > automatic_gcc.conf
-  # sed s/build_root6=no/build_root6=yes/ automatic.conf > automatic_root6.conf
-  # sed s/compiler=/compiler=gcc/ automatic_root6.conf > automatic_gcc.conf
+
+  if [ $ROOTVER -eq 6 ]; then
+    sed s/build_root6=no/build_root6=yes/ automatic.conf > automatic_root.conf
+  else 
+    cp automatic.conf automatic_root.conf
+  fi
+  sed s/compiler=/compiler=gcc/ automatic_root.conf > automatic_gcc.conf
   ./configure.sh automatic_gcc.conf
   
   cd $CBMSRCDIR
@@ -131,19 +140,19 @@ if [ $SETUP_FAIRROOT -ge 1 ]; then
   # set SIMPATH
   cd ..
   echo "SIMPATH	before: $SIMPATH"
-  cd fairsoft_$FSOFTVER/installation/
+  cd fairsoft_${FSOFTVER}_root{$ROOTVER}/installation/
   export SIMPATH=`pwd`
   echo "SIMPATH	now   : $SIMPATH"
   cd $CBMSRCDIR
 
   cd ..
-  git clone https://github.com/FairRootGroup/FairRoot.git fairroot_src_$FROOTVER-fairsoft_$FSOFTVER
-  cd fairroot_src_$FROOTVER-fairsoft_$FSOFTVER
+  git clone https://github.com/FairRootGroup/FairRoot.git fairroot_src_$FROOTVER-fairsoft_${FSOFTVER}_root{$ROOTVER}
+  cd fairroot_src_$FROOTVER-fairsoft_${FSOFTVER}_root{$ROOTVER}
   git tag -l
   git checkout -b $FROOTVER $FROOTVER
   mkdir build
   cd build
-  cmake .. -DCMAKE_INSTALL_PREFIX=../../fairroot_$FROOTVER-fairsoft_$FSOFTVER
+  cmake .. -DCMAKE_INSTALL_PREFIX=../../fairroot_$FROOTVER-fairsoft_${FSOFTVER}_root{$ROOTVER}
   nice make install -j$NUMOFCPU
   
   cd $CBMSRCDIR
@@ -161,14 +170,14 @@ if [ $SETUP_CBMROOT -ge 1 ]; then
   # set SIMPATH
   cd ..
   echo "SIMPATH	before: $SIMPATH"
-  cd fairsoft_$FSOFTVER/installation/
+  cd fairsoft_${FSOFTVER}_root{$ROOTVER}/installation/
   export SIMPATH=`pwd`
   echo "SIMPATH	now   : $SIMPATH"
   cd $CBMSRCDIR
 
   # set FAIRROOTPATH
   cd ..
-  cd fairroot_$FROOTVER-fairsoft_$FSOFTVER
+  cd fairroot_$FROOTVER-fairsoft_${FSOFTVER}_root{$ROOTVER}
   export FAIRROOTPATH=`pwd`
   echo "FAIRROOTPATH: $FAIRROOTPATH"
   cd $CBMSRCDIR
