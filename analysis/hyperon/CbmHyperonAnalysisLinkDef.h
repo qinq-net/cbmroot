b7f7ca95bd114972c28e6973dbd8256ae1d6ae97
f.uhlig <f.uhlig@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2015-12-17 14:55:47 +0000

    The code from the hyperon directory was removed from libCbmAnalysis. Instaed build the dedicate library libCbmHyperonAnalysis. In the moment the code doesn't compile, so the building of libCbmHyperonAnalysis is not done. #Refs 534 @15m.

---
+++ analysis/hyperon/CbmHyperonAnalysisLinkDef.h
@@ -0,0 +1,24 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class CbmAnaHypYPtGenerator+;
+#pragma link C++ class CbmAnaHypTools+;
+#pragma link C++ class CbmAnaHypSecFinder+;
+#pragma link C++ class CbmAnaHypSecFinderQa+;
+#pragma link C++ class CbmAnaHypPair+;
+#pragma link C++ class CbmAnaHypLambdaCandidate+;
+#pragma link C++ class CbmAnaHypTrack+;
+#pragma link C++ class CbmAnaHypRecEff+;
+#pragma link C++ class CbmAnaHypRecLambda+;
+#pragma link C++ class CbmAnaHypSelectLambda+;
+#pragma link C++ class CbmAnaHypRecCascade+;
+#pragma link C++ class CbmAnaHypMix+;
+
+#endif
+
+
+
+
