23dadf1f6da32d64c7f5d515e682ef6680d8531f
n.herrmann <n.herrmann@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2016-01-14 15:41:08 +0000

    restore missing shell scripts

---
+++ macro/tof/beamtime/nov15/init_calib.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+# shell script to initialize clusterizer calibrations
+cRun=$1
+#cRun='CbmTofSps_01Dec0128'
+#cRun='CbmTofSps_29Nov2103'
+#cRun='CbmTofSps_29Nov1257'
+#cRun='CbmTofSps_29Nov1202'
+#cRun='CbmTofSps_29Nov0952'
+#cRun='CbmTofSps_28Nov0728'
+#cRun='CbmTofSps_28Nov0001'
+#cRun='CbmTofSps_28Nov0041'
+#cRun='CbmTofSps_27Nov2115'
+#cRun='CbmTofSps_27Nov1728'
+nIter=1
+echo 'Initialize clusterizer calibration for run '$cRun, execute $nIter iterations
+#iDut=3; iMRef=9; iBRef=7; 
+#iDut=9; iMRef=4; iBRef=3;  
+#iDut=9; iMRef=7; iBRef=3;  
+#iDut=9; iMRef=3; iBRef=5;  
+###iDut=3; iMRef=9; iBRef=5;  
+#iDut=9; iMRef=9; iBRef=5;
+#iDut=3; iMRef=4; iBRef=5;
+#iDut=921; iMRef=920; iBRef=300;
+iDut=400; iMRef=300; iBRef=500;
+  
+#iDut=6; iMRef=1; iBRef=8;  
+#iDut=6; iMRef=1; iBRef=5;  
+#iDut=1; iMRef=6; iBRef=5;  
+
+((iSet=$iDut*1000+$iMRef))
+#iSet=0
+#lastOpt=''
+nEvi=500000
+mkdir ${cRun}
+cp rootlogon.C ${cRun}
+cp .rootrc ${cRun}
+cd ${cRun}
+
+iStep=0
+iStepLast=0
+while [[ $nIter > 0 ]]; do 
+echo start from scratch with $nIter iterations 
+if((1)); then
+echo really start from scratch with $nIter iterations 
+
+for inOpt in ''$nEvi',0,0,0,50' ''$nEvi',03,0,0,50' ''$nEvi',13,0,0,50' ''$nEvi',23,0,0,50' ''$nEvi',23,0,0,50' ''$nEvi',33,0,0,50' ''$nEvi',33,0,0,50'  ''$nEvi',33,0,0,50' ''$nEvi',43,0,0,50' ''$nEvi',43,0,0,50' ''$nEvi',43,0,0,50' ''$nEvi',53,0,0,50' ''$nEvi',53,0,0,50' ''$nEvi',63,0,0,50' ''$nEvi',63,0,0,50'
+do
+
+((iStepLast = ${iStep}))
+((iStep += 1))
+mkdir Init${iStep}
+cp rootlogon.C Init${iStep}
+cp .rootrc Init${iStep}
+cd Init${iStep}
+
+if [[ ${lastOpt:+1} ]] ; then
+# echo last round was done with $lastOpt, extract 2. and 3. word
+i1=`expr index $inOpt , `
+i2=($i1+3)
+#echo `expr index $inOpt , ` = $i1
+cMode=${inOpt:$i1:2}
+cSel=${inOpt:$i2:1}
+echo next iteration: cMode=$cMode, cSel=$cSel 
+if [[ ${cSel} = "-" ]];then 
+    cSel=${inOpt:$i2:2}
+    echo cSel=$cSel 
+    cSel="0"
+fi
+#copy calibration file 
+#cp -v tofTestBeamClust_${cRun}.hst.root ${cRun}_${cMode}_${cSel}tofTestBeamClust.hst.root
+cp -v ../Init${iStepLast}/tofTestBeamClust_${cRun}_set${iSet}.hst.root ${cRun}_set${iSet}_${cMode}_${cSel}tofTestBeamClust.hst.root
+fi 
+
+# generate new calibration file
+root -b -q '../../ana_digi_ini.C('$inOpt',"'${cRun}'",'${iSet}','${iBRef}') '
+lastOpt=$inOpt
+
+#./screenshot.sh
+cp *pdf ../
+cd .. 
+done
+
+cp -v  ./Init${iStep}/${cRun}_set${iSet}_${cMode}_${cSel}tofTestBeamClust.hst.root ${cRun}_set${iSet}_03_0tofTestBeamClust.hst.root 
+cp -v  ./Init$i{Step}/${cRun}_set${iSet}_${cMode}_${cSel}tofTestBeamClust.hst.root ${cRun}_set${iSet}_03_00tofTestBeamClust.hst.root
+ 
+fi
+
+echo execute main loop at $nIter. iteration 
+
+for inOpt in ''$nEvi',03,0,0,50' ''$nEvi',03,0,0,50' ''$nEvi',03,1,'${iMRef}',0' ''$nEvi',03,0,'${iDut}',0' ''$nEvi',03,1,'${iMRef}',0' ''$nEvi',03,0,'${iDut}',0' ''$nEvi',13,0,0,50' ''$nEvi',13,0,0,50' ''$nEvi',13,1,'${iMRef}',0'  ''$nEvi',13,0,'${iDut}',0' ''$nEvi',13,1,'${iMRef}',0'  ''$nEvi',13,0,'${iDut}',0' ''$nEvi',23,0,0,50' ''$nEvi',23,0,0,50' ''$nEvi',23,1,'${iMRef}',0'  ''$nEvi',23,0,'${iDut}',0' ''$nEvi',23,1,'${iMRef}',0'  ''$nEvi',23,0,'${iDut}',0' ''$nEvi',01,0,'-${iDut}',0' ''$nEvi',01,1,'-${iMRef}',0' ''$nEvi',33,0,0,50' ''$nEvi',33,0,0,50' ''$nEvi',33,1,'${iMRef}',0'  ''$nEvi',33,0,'${iDut}',0' ''$nEvi',04,0,'-${iDut}',0' ''$nEvi',33,0,'${iDut}',0' ''$nEvi',04,1,'-${iMRef}',0' ''$nEvi',33,1,'${iMRef}',0'  ''$nEvi',43,0,0,50' ''$nEvi',43,0,0,50' ''$nEvi',43,1,'${iMRef}',0' ''$nEvi',43,0,'${iDut}',0' ''$nEvi',43,1,'${iMRef}',0' ''$nEvi',43,0,'${iDut}',0' ''$nEvi',43,1,'${iMRef}',0' ''$nEvi',43,0,0,50' ''$nEvi',43,1,'${iMRef}',0' ''$nEvi',43,0,'${iDut}',0' ''$nEvi',53,0,0,50' ''$nEvi',53,0,0,50' ''$nEvi',53,1,'${iMRef}',0' ''$nEvi',53,0,'${iDut}',0' ''$nEvi',53,1,'${iMRef}',0' ''$nEvi',53,0,'${iDut}',0' ''$nEvi',52,0,0,0' ''$nEvi',52,0,0,50' '500000,14,0,-'${iDut}',0' '500000,14,1,-'${iMRef}',0' ''$nEvi',53,0,'${iDut}',0' ''$nEvi',53,1,'${iMRef}',0' ''$nEvi',63,0,'${iDut}',0' ''$nEvi',63,1,'${iMRef}',0' ''$nEvi',73,0,'${iDut}',0' ''$nEvi',73,1,'${iMRef}',0' ''$nEvi',73,0,'${iDut}',0' '2000000,24,0,-'${iDut}',0' ''$nEvi',73,1,'${iMRef}',0' '2000000,24,1,-'${iMRef}',0' ''$nEvi',83,0,'${iDut}',0' ''$nEvi',83,1,'${iMRef}',0' ''$nEvi',93,0,'${iDut}',0' ''$nEvi',93,1,'${iMRef}',0'
+do   
+
+((iStepLast = ${iStep}))
+((iStep += 1))
+mkdir Init${iStep}
+cp rootlogon.C Init${iStep}
+cp .rootrc Init${iStep}
+cd Init${iStep}
+
+if [[ ${lastOpt:+1} ]] ; then
+# echo last round was done with $lastOpt, extract 2. and 3. word
+i1=`expr index $inOpt , `
+i2=($i1+3)
+#echo `expr index $inOpt , ` = $i1
+cMode=${inOpt:$i1:2}
+cSel=${inOpt:$i2:1}
+echo next iteration: cMode=$cMode, cSel=$cSel 
+if [[ ${cSel} = "-" ]];then 
+    cSel=${inOpt:$i2:2}
+    echo cSel=$cSel 
+    cSel="0"
+fi
+#copy calibration file 
+cp -v ../Init${iStepLast}/tofTestBeamClust_${cRun}_set${iSet}.hst.root ${cRun}_set${iSet}_${cMode}_${cSel}tofTestBeamClust.hst.root
+fi 
+
+# generate new calibration file
+root -b -q '../../ana_digi_cal.C('$inOpt',"'${cRun}'",'${iSet}','${iBRef}') '
+
+lastOpt=$inOpt
+
+cp -v tofTestBeamClust_${cRun}_set${iSet}.hst.root ../${cRun}_set${iSet}_${cMode}_${cSel}${mode}tofTestBeamClust.hst.root
+cp *pdf ../
+#./screenshot.sh
+cd .. 
+rm ../${cRun}_set${iSet}_${cMode}_${cSel}${mode}tofTestBeamClust.hst.root
+ln -s ./${cRun}/${cRun}_set${iSet}_${cMode}_${cSel}${mode}tofTestBeamClust.hst.root ../${cRun}_set${iSet}_${cMode}_${cSel}${mode}tofTestBeamClust.hst.root
+
+done
+(( nIter -= 1))
+done 
+#
---
+++ macro/tof/beamtime/nov15/iter_calib.sh
@@ -0,0 +1,150 @@
+# shell script to iterate clusterizer calibrations 
+cRun=$1
+#cRun='CbmTofSps_01Dec0128'
+#cRun='CbmTofSps_29Nov2103'
+#cRun='CbmTofSps_28Nov0041'
+#cRun='CbmTofSps_27Nov2115'
+#iDut=921; iRef=920; iSel2=-300
+#iDut=921; iRef=300; iSel2=920
+iDut=400; iRef=300; iSel2=921
+#iDut=3; iRef=9; iSel2=9
+##iDut=9; iRef=9; iSel2=3
+#iDut=9; iRef=4; iSel2=-3
+#iDut=9; iRef=7; iSel2=-3
+#iDut=6; iRef=1;   iSel2=-8
+###iDut=6; iRef=1; iSel2=8;  
+c0='000000'
+
+((iSet=$iDut*1000+$iRef))
+
+if((${iSel2}<0));then
+ ((iBRef=-iSel2))
+ else
+ iBRef=500
+ fi
+
+echo iDut=$iDut, iRef=$iRef, iSet=$iSet, iSel2=$iSel2, iBRef=$iBRef
+
+#mkdir ${cRun}
+#cp rootlogon.C ${cRun}
+#cp .rootrc ${cRun}
+cd ${cRun}
+
+iStep=0
+iStepLast=0
+# history for 1805: 1 6 8 2 10 2 10
+#for mode in 1 6 8 2 10 4 5 4
+#for mode in 2 10 2 10 
+#for mode in 4
+#for mode in 5
+#for mode in 1
+#for mode in 6 8 2 10 4 5 4
+for mode in 1 6 8 
+do
+
+case $mode in 
+1)
+nIter=4
+alist=`echo '1'$c0'0,93,1,'$iRef'' '1'$c0'0,44,1,'$iDut'' '1'$c0'0,44,1,'$iBRef'' '1'$c0'0,93,0,'$iDut'' '1'$c0'0,44,0,'$iRef'' '1'$c0'0,44,0,'$iBRef'' '1'$c0'00,41,1,-'$iRef'' '1'$c0'0,93,0,'$iRef'' '1'$c0'00,41,0,-'$iDut'' '1'$c0'0,93,1,'$iRef''`
+;;
+2)
+nIter=2
+alist=`echo '1'$c0'0,92,1,'$iRef'' '1'$c0'00,64,1,-'$iRef'' '1'$c0'00,64,1,'$iBRef'' '1'$c0'0,92,0,'$iDut'' '1'$c0'00,64,0,-'$iDut'' '1'$c0'00,64,0,'$iBRef'' '1'$c0'0,92,1,'$iRef''`
+;;
+3)
+nIter=3
+alist=`echo '1'$c0',93,1,4' `
+;;
+4)
+nIter=5
+alist=`echo '5'$c0'0,93,1,'$iRef'' '5'$c0'0,93,-2,2' '1'$c0'000,61,-1,-2' '5'$c0'0,93,-3,2' ` 
+;;
+5)
+nIter=2
+alist=`echo '5'$c0'0,93,1,'$iRef'' '5'$c0'0,93,-2,2' '1'$c0'00,64,1,-'$iRef'' '1'$c0'00,64,0,-'$iDut'' '5'$c0'0,93,-3,2' ` 
+;;
+6)
+nIter=2
+alist=`echo '1'$c0'0,93,1,'$iRef'' '1'$c0'00,93,-2,-2' '1'$c0'0,93,0,'$iDut'' '1'$c0'00,93,-2,-2' '1'$c0'0,93,1,'$iRef''` 
+;;
+7)
+nIter=3
+alist=`echo '1'$c0',92,1,4' '1'$c0'00,93,-2,-2' '1'$c0',92,0,3' '1'$c0'00,93,-2,2' '1'$c0',92,1,4'` 
+;;
+8)
+nIter=2
+alist=`echo '1'$c0'0,93,1,'$iRef'' '1'$c0'0,92,0,'$iDut'' '1'$c0'00,61,-1,-2' '1'$c0'00,93,-2,2' '1'$c0'0,93,1,'$iRef''`
+;;
+9)
+nIter=2
+alist=`echo '1'$c0',92,1,4' '1'$c0',92,0,3' '1'$c0'00,61,-1,-2' '1'$c0'0,93,-2,2' '1'$c0',92,1,4'`
+;;
+10)
+nIter=1
+alist=`echo '1'$c0'0,92,1,'$iRef'' '1'$c0'00,93,-3,2' '1'$c0'00,93,-2,2' '1'$c0'00,61,-1,-2' '1'$c0'0,92,1,'$iRef''`
+;;
+11)
+nIter=1
+alist=`echo '1'$c0',92,1,'$iRef'' '1'$c0'00,93,-3,2' '1'$c0'00,93,-2,2' '1'$c0'00,61,-1,-2' '1'$c0'0,92,1,'$iRef''`
+;;
+esac
+
+echo 'Iterate Clusterizer calibration for run '$cRun, Set ${iSet} - execute $nIter iterations of ${alist}
+#lastOpt=''
+
+while [[ $nIter > 0 ]]; do
+for inOpt in $alist 
+do
+
+((iStepLast = ${iStep}))
+((iStep += 1))
+mkdir Iter${iStep}
+cp rootlogon.C Iter${iStep}
+cp .rootrc Iter${iStep}
+cd Iter${iStep}
+
+if [[ ${iStep} = 1 ]]; then 
+    lastOpt=$inOpt
+fi 
+
+if [[ ${lastOpt:+1} ]] ; then
+# echo last round was done with $lastOpt, extract 2. and 3. word
+i1=`expr index $inOpt , `
+i2=($i1+3)
+#echo `expr index $inOpt , ` = $i1
+cMode=${inOpt:$i1:2}
+cSel=${inOpt:$i2:1}
+echo Next iteration: cMode=$cMode, cSel=$cSel 
+if [[ ${cSel} = "-" ]];then 
+    cSel=${inOpt:$i2:2}
+    echo cSel=$cSel 
+    cSel="0"
+fi
+#copy calibration file 
+
+if [[ ${iStep} = 1 ]]; then 
+    cp -v ../${cRun}_set${iSet}_${cMode}_${cSel}tofTestBeamClust.hst.root .
+else 
+    cp -v ../Iter${iStepLast}/tofTestBeamClust_${cRun}_set${iSet}.hst.root ${cRun}_set${iSet}_${cMode}_${cSel}tofTestBeamClust.hst.root
+fi
+fi 
+
+# generate new calibration file
+root -b -q '../../ana_digi.C('$inOpt',0,"'$cRun'",'$iSet',0,'$iSel2') '
+
+lastOpt=$inOpt
+
+#./screenshot.sh 
+cp -v tofTestBeamClust_${cRun}_set${iSet}.hst.root ../${cRun}_set${iSet}_${cMode}_${cSel}${mode}tofTestBeamClust.hst.root
+cp *pdf ../
+cd .. 
+rm ../${cRun}_set${iSet}_${cMode}_${cSel}${mode}tofTestBeamClust.hst.root
+ln -s ./${cRun}/${cRun}_set${iSet}_${cMode}_${cSel}${mode}tofTestBeamClust.hst.root ../${cRun}_set${iSet}_${cMode}_${cSel}${mode}tofTestBeamClust.hst.root
+
+done
+(( nIter -= 1))
+done
+#cp -v tofTestBeamClust_${cRun}_set${iSet}.hst.root ${cRun}_set${iSet}_${cMode}_${cSel}${mode}tofTestBeamClust.hst.root
+
+done 
+#
