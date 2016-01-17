64ee27176297190981aaff86d28822334288d020
jordan.bendarouach <jordan.bendarouach@physik.uni-giessen.de>
2016-01-17 15:45:04 +0000

    D    alignment/mirror_alignment D    alignment/misalignment_correction

--- macro/rich/alignment/mirror_alignment/Misalignment_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
- 
-# Launch simulation, reconstruction and analyze files
-# for a given nb of events and geometry files
- 
-#for RX in 2 4 5 6 8 10 ;do
- 
-root -b -q "run_sim_jordan.C(1, 0)"
-root -b -q "DrawPoints.C(0)"
