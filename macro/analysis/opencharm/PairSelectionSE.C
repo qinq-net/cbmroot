f4716b54d649ff48d50ee7db213fcf683e1c540f
p.sitzmann <p.sitzmann@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2016-01-21 10:42:36 +0000

    new macros for openCharm reco

---
+++ macro/analysis/opencharm/PairSelectionSE.C
@@ -0,0 +1,202 @@
+
+TString caveGeom="";
+TString pipeGeom="";
+TString magnetGeom="";
+TString mvdGeom="";
+TString stsGeom="";
+TString richGeom="";
+TString muchGeom="";
+TString shieldGeom="";
+TString trdGeom="";
+TString tofGeom="";
+TString ecalGeom="";
+TString platformGeom="";
+TString psdGeom="";
+Double_t psdZpos=0.;
+Double_t psdXpos=0.;
+
+TString mvdTag="";
+TString stsTag="";
+TString trdTag="";
+TString tofTag="";  
+
+TString stsDigi="";
+TString trdDigi="";
+TString tofDigi="";
+
+TString mvdMatBudget="";
+TString stsMatBudget="";
+
+TString  fieldMap="";
+Double_t fieldZ=0.;
+Double_t fieldScale=0.;
+Int_t    fieldSymType=0;
+
+
+TString input;
+TString inputGEV;
+TString system;
+TString signal; // "dminus" "dplus" "d0_4B"
+Int_t  iVerbose;
+TString setup;
+
+bool littrack;
+Bool_t useMC;
+
+
+void PairSelectionSE(Int_t nEvents = 100, Int_t ProcID = 1, bool PileUp = false, Int_t PidTyp = 0)
+{
+// -------------------------------------------------------------------------
+
+  TString curDir = gSystem->Getenv("VMCWORKDIR");
+  TString setupDir = curDir + "/macro/analysis/opencharm/CharmSetup.C";
+  gROOT->LoadMacro(setupDir);
+  gInterpreter->ProcessLine("CharmSetup()");
+
+switch (PidTyp)
+{
+case 0:
+    TString pidMode = "NONE";
+    break;
+case 1:
+    TString pidMode = "MC";
+    break;
+case 2:
+    TSTring pidMode = "TOF";
+    break;
+default:
+    TString pidMode = "NONE";
+
+}
+
+// Input file (MC events)
+ TString mcFile = Form("/hera/cbm/users/psitzmann/data/mc/opencharm.mc.urqmd.%s.%s.%i.%i.%s.%s.root",input.Data(), inputGEV.Data(), nEvents, ProcID, signal.Data(), setup.Data());
+
+
+
+  // Parameter file
+  TString parFile = Form("/hera/cbm/users/psitzmann/data/params/paramsunigen.urqmd.%s.%s.%i.%i.%s.%s.root",input.Data(), inputGEV.Data(), nEvents, ProcID, signal.Data(), setup.Data());
+
+    // Reco file
+    TString rcSystem = Form("/hera/cbm/users/psitzmann/data/reco/opencharm.reco.urqmd.%s.%s.%i.%i.%s.%s", input.Data(), inputGEV.Data(), nEvents, ProcID,  signal.Data(), setup.Data());
+    if(!PileUp)
+      {
+      if(littrack)
+         TString rcFile = rcSystem + ".littrack.root";
+      else 
+         TString rcFile = rcSystem + ".l1.root";
+      }
+      else if(littrack)
+         TString rcFile = rcSystem + ".PileUp.littrack.root";
+      else 
+         TString rcFile = rcSystem + ".PileUp.l1.root";
+
+  // Output file
+  TString outSystem = Form("/hera/cbm/users/psitzmann/data/ana/opencharm.pairs.superEvent.urqmd.%s.%s.%i.%i.%s.%s.pidMode_%s", input.Data(), inputGEV.Data(), nEvents, ProcID, signal.Data(), setup.Data(), pidMode.Data());
+  if(useMC) outSystem += ".mcMode";
+  if(!PileUp)
+    {
+    if(littrack)
+       TString outFile = outSystem + ".littrack.root";
+    else 
+       TString outFile = outSystem + ".l1.root";
+    }
+    else if(littrack)
+       TString outFile = outSystem + ".PileUp.littrack.root";
+    else 
+       TString outFile = outSystem + ".PileUp.l1.root";
+    
+
+    // Track file
+  TString trackSystem = Form("/hera/cbm/users/psitzmann/data/ana/opencharm.tracks.urqmd.%s.%s.%i.%i.%s.%s.pidMode_%s", input.Data(), inputGEV.Data(), nEvents, ProcID, signal.Data(), setup.Data(), pidMode.Data());
+  if(!PileUp)
+    {
+    if(littrack)
+       TString trackFile = trackSystem + ".littrack.root";
+    else 
+       TString trackFile = trackSystem + ".l1.root";
+    }
+    else if(littrack)
+       TString trackFile = trackSystem + ".PileUp.littrack.root";
+    else 
+       TString trackFile = trackSystem + ".PileUp.l1.root";
+
+    cout << endl
+	<< "  mcFile  :   " << mcFile  << endl
+	<< "  rcFile  :   " << rcFile  << endl
+        << "  outFile    :   " << outFile << endl;
+
+// -------------------------------------------------------------------------	
+//  Digitisation files.
+  // Add TObjectString containing the different file names to
+  // a TList which is passed as input to the FairParAsciiFileIo.
+  // The FairParAsciiFileIo will take care to create on the fly 
+  // a concatenated input parameter file which is then used during
+  // the reconstruction.
+  TList *parFileList = new TList();
+
+  TString inDir = gSystem->Getenv("VMCWORKDIR");
+  TString paramDir = inDir + "/parameters/";
+
+  TString setupFile = inDir + "/geometry/setup/" + setup + "_setup.C";
+  TString setupFunct = setup;
+  setupFunct += "_setup()";
+
+  gROOT->LoadMacro(setupFile);
+  gInterpreter->ProcessLine(setupFunct);
+
+  TObjString stsDigiFile = paramDir + stsDigi;
+  parFileList->Add(&stsDigiFile);
+  cout << "macro/run/run_reco.C using: " << stsDigi << endl;
+  // -------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------
+    TStopwatch timer;
+    timer.Start();
+
+    gDebug=0;
+    // -------------------------------------------------------------------------
+
+// ------------------------------------------------------------------------
+
+    FairRunAna *fRun = new FairRunAna();
+    fRun->SetInputFile(mcFile);
+    fRun->AddFriend(rcFile);
+    fRun->AddFriend(trackFile);
+    fRun->SetOutputFile(outFile);
+// -------------------------------------------------------------------------
+
+    CbmKF* KF = new CbmKF();
+    fRun->AddTask(KF);
+
+    CbmL1* l1 = new CbmL1();
+    fRun->AddTask(l1);                                                 // name     verbose  ipD0   SvZ
+    
+    CbmD0CandidatesSE* D0selectionSE  = new CbmD0CandidatesSE("d0selectionSE", 1, 0.04,  0.01 );
+    D0selectionSE->SetNegativeFileName(trackFile);
+    fRun->AddTask(D0selectionSE);
+    
+  // -----  Parameter database   --------------------------------------------
+  FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
+  FairParRootFileIo* parIo1 = new FairParRootFileIo();
+  FairParAsciiFileIo* parIo2 = new FairParAsciiFileIo();
+  parIo1->open(parFile.Data());
+  parIo2->open(parFileList, "in");
+  rtdb->setFirstInput(parIo1);
+  rtdb->setSecondInput(parIo2);
+  rtdb->setOutput(parIo1);
+  rtdb->saveOutput();
+  // ------------------------------------------------------------------------
+    
+// -------------------------------------------------------------------------
+    fRun->Init();
+    fRun->Run(0,nEvents);
+// -------------------------------------------------------------------------
+
+    timer.Stop();
+    Double_t rtime = timer.RealTime();
+    Double_t ctime = timer.CpuTime();
+    printf("\nRealTime=%f seconds, CpuTime=%f seconds\n",rtime,ctime);
+    cout << "output file is : " << outFile << endl;
+
+}
---
+++ macro/analysis/opencharm/opencharm_delta.C
@@ -0,0 +1,226 @@
+// -------------------------------------------------------------------------
+//
+// Macro for standard transport simulation using UrQMD input with Pluto signal
+//
+// P.Sitzmann 08/06/2015
+//
+// --------------------------------------------------------------------------
+
+TString caveGeom="";
+TString pipeGeom="";
+TString magnetGeom="";
+TString mvdGeom="";
+TString stsGeom="";
+TString richGeom="";
+TString muchGeom="";
+TString shieldGeom="";
+TString trdGeom="";
+TString tofGeom="";
+TString ecalGeom="";
+TString platformGeom="";
+TString psdGeom="";
+Double_t psdZpos=0.;
+Double_t psdXpos=0.;
+
+TString mvdTag="";
+TString stsTag="";
+TString trdTag="";
+TString tofTag="";  
+
+TString stsDigi="";
+TString trdDigi="";
+TString tofDigi="";
+
+TString mvdMatBudget="";
+TString stsMatBudget="";
+
+TString  fieldMap="";
+Double_t fieldZ=0.;
+Double_t fieldScale=0.;
+Int_t    fieldSymType=0;
+
+TString defaultInputFile="";
+
+
+// Input Parameter
+TString input;
+TString inputGEV;
+TString system;
+TString signal;
+Int_t  iVerbose;
+TString setup;
+bool littrack;
+Bool_t useMC;
+
+void opencharm_delta(Int_t nEvents = 100,Int_t ProcID=1)
+{
+  // ========================================================================
+  //          Adjust this part according to your requirements
+  TString curDir = gSystem->Getenv("VMCWORKDIR");
+  TString setupDir = curDir + "/macro/analysis/opencharm/CharmSetup.C";
+  gROOT->LoadMacro(setupDir);
+  gInterpreter->ProcessLine("CharmSetup()");
+ 
+  // Output file
+  TString outFile = Form("/hera/cbm/users/psitzmann/data/mc/delta/opencharm.mc.delta..%s.%s.%i.%i.%s.%s.root",input.Data(), inputGEV.Data(), nEvents, ProcID, signal.Data(), setup.Data());
+  // ------------------------------------------------------------------------
+
+  // Parameter file name
+  TString parFile = Form("/hera/cbm/users/psitzmann/data/params/paramsunigen.urqmd.%s.%s.%i.%i.%s.%s.root",input.Data(), inputGEV.Data(), nEvents, ProcID, signal.Data(), setup.Data());
+  // ------------------------------------------------------------------------
+
+  TString inDir = gSystem->Getenv("VMCWORKDIR");                                       
+
+  TString setupFile = inDir + "/geometry/setup/" + setup + "_setup.C";
+  TString setupFunct = setup;
+  setupFunct += "_setup()";
+  
+  gROOT->LoadMacro(setupFile);
+  gInterpreter->ProcessLine(setupFunct);
+
+  gRandom->SetSeed(0);
+
+  // In general, the following parts need not be touched
+  // ========================================================================
+
+  // ----    Debug option   -------------------------------------------------
+  gDebug = 0;
+  // ------------------------------------------------------------------------
+
+  gROOT->LoadMacro("$VMCWORKDIR/gconfig/basiclibs.C");
+  basiclibs();
+  
+
+  // -----   Timer   --------------------------------------------------------
+  TStopwatch timer;
+  timer.Start();
+  // ------------------------------------------------------------------------
+
+ 
+  // -----   Create simulation run   ----------------------------------------
+  FairRunSim* fRun = new FairRunSim();
+  fRun->SetName("TGeant3");              // Transport engine
+  fRun->SetOutputFile(outFile);          // Output file
+  FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
+  // ------------------------------------------------------------------------
+
+
+  // -----   Create media   -------------------------------------------------
+  fRun->SetMaterials("media.geo");       // Materials
+  // ------------------------------------------------------------------------
+
+
+  // -----   Create geometry   ----------------------------------------------
+  FairModule* cave= new CbmCave("CAVE");
+  cave->SetGeometryFileName(caveGeom);
+  fRun->AddModule(cave);
+
+  FairModule* pipe= new CbmPipe("PIPE");
+  pipe->SetGeometryFileName(pipeGeom);
+  fRun->AddModule(pipe);
+  
+  FairModule* target= new CbmTarget(79, 0.25);
+  fRun->AddModule(target);		
+
+  FairModule* magnet = new CbmMagnet("MAGNET");
+  magnet->SetGeometryFileName(magnetGeom);
+  fRun->AddModule(magnet);
+  
+  FairDetector* mvd= new CbmMvd("MVD", kTRUE);
+  mvd->SetGeometryFileName(mvdGeom); 
+  mvd->SetMotherVolume("pipevac1");
+  fRun->AddModule(mvd);
+
+  FairDetector* sts = new CbmStsMC(kTRUE);
+  sts->SetGeometryFileName(stsGeom);
+  fRun->AddModule(sts);
+
+  if ( richGeom != "" ) {
+    FairDetector* rich = new CbmRich("RICH", kTRUE);
+    rich->SetGeometryFileName(richGeom);
+    fRun->AddModule(rich);
+  }
+
+  if ( trdGeom != "" ) {
+    FairDetector* trd = new CbmTrd("TRD",kTRUE );
+    trd->SetGeometryFileName(trdGeom);
+    fRun->AddModule(trd);
+  }
+
+  if ( tofGeom != "" ) {
+    FairDetector* tof = new CbmTof("TOF", kTRUE);
+    tof->SetGeometryFileName(tofGeom);
+    fRun->AddModule(tof);
+  }
+  // ------------------------------------------------------------------------
+
+
+
+  // -----   Create magnetic field   ---------------------------------------
+  CbmFieldMap* magField = NULL;
+  if ( 2 == fieldSymType ) {
+      magField = new CbmFieldMapSym2(fieldMap);
+  }  else if ( 3 == fieldSymType ) {
+   magField = new CbmFieldMapSym3(fieldMap);
+  }
+  else
+    magField = new CbmFieldMapSym1(fieldMap);
+
+  magField->SetPosition(0., 0., fieldZ);
+  magField->SetScale(fieldScale);
+  fRun->SetField(magField);
+  cout << endl << "set mag field" << endl;
+  // ------------------------------------------------------------------------
+
+  // -- Nickel delta sim ----------------------------------------------------
+ // FairIon *fIon = new FairIon("My_Ni", 58, 59, 79, 15., 58.693);
+  FairIon *fIon = new FairIon("My_Au", 79, 197, 79, 25.,183.47324);
+  fRun->AddNewIon(fIon);
+
+  // -----   Create PrimaryGenerator   --------------------------------------
+  FairPrimaryGenerator* primGen   = new FairPrimaryGenerator();
+ // FairIonGenerator*     fIongen   = new FairIonGenerator(58, 59, 58,1, 0.,0., 15, 0.,0.,-1.);   // Nickel
+  FairIonGenerator*     fIongen   = new FairIonGenerator(79, 197,79,1, 0.,0., 25, 0.,0.,-1.); // Gold
+  primGen->AddGenerator(fIongen);
+  fRun->SetGenerator(primGen);
+  // ------------------------------------------------------------------------
+  // ------------------------------------------------------------------------
+
+
+  // -----   Run initialisation   -------------------------------------------
+  fRun->Init();
+  // ------------------------------------------------------------------------
+
+  // -----   Runtime database   ---------------------------------------------
+  CbmFieldPar* fieldPar = (CbmFieldPar*) rtdb->getContainer("CbmFieldPar");
+  fieldPar->SetParameters(magField);
+  fieldPar->setChanged();
+  fieldPar->setInputVersion(fRun->GetRunId(),1);
+  Bool_t kParameterMerged = kTRUE;
+  FairParRootFileIo* parOut = new FairParRootFileIo(kParameterMerged);
+  parOut->open(parFile.Data());
+  rtdb->setOutput(parOut);
+  rtdb->saveOutput();
+  rtdb->print();
+  // ------------------------------------------------------------------------
+
+ 
+  // -----   Start run   ----------------------------------------------------
+  fRun->Run(nEvents);
+  // ------------------------------------------------------------------------
+
+
+  // -----   Finish   -------------------------------------------------------
+  timer.Stop();
+  Double_t rtime = timer.RealTime();
+  Double_t ctime = timer.CpuTime();
+  cout << endl << endl;
+  cout << "Macro finished succesfully." << endl;
+  cout << "Output file is "    << outFile << endl;
+  cout << "Parameter file is " << parFile << endl;
+  cout << "Real time " << rtime << " s, CPU time " << ctime 
+       << "s" << endl << endl;
+  // ------------------------------------------------------------------------
+
+}
+
