15f1e1d4843d00cbb21f6a6bea7589de5099a0ef
f.uhlig <f.uhlig@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2016-01-22 14:59:01 +0000

    Add dedicated library for open charm analysis. Refs #534 @1h.

---
+++ analysis/opencharm/CbmOpenCharmAnalysisLinkDef.h
@@ -0,0 +1,22 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class CbmD0Candidate+;
+#pragma link C++ class CbmD0Tools+;
+#pragma link C++ class CbmD0CandidateSelection+;
+#pragma link C++ class CbmD0TrackCandidate+;
+#pragma link C++ class CbmD0CandidatesSE+;
+#pragma link C++ class CbmD0TrackSelection+;
+#pragma link C++ class CbmD0HistogramManager+;
+#pragma link C++ class CbmMapsFileManager+;
+#endif
+
+
+
+
+
+
+
