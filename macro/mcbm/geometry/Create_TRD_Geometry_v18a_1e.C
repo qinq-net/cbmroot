9b98d9b91a79b972dec9b603cc06e3c889411b36
d.emschermann <d.emschermann@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2016-02-04 14:30:49 +0000

    enable CheckOverlaps in mCBM TRD geometries

--- macro/mcbm/geometry/Create_TRD_Geometry_v18a_1e.C
+++ macro/mcbm/geometry/Create_TRD_Geometry_v18a_1e.C
@@ -502,8 +502,8 @@ void Create_TRD_Geometry_v18a_1e() {
     create_mag_field_vector();
   
   gGeoMan->CloseGeometry();
-//  gGeoMan->CheckOverlaps(0.001);
-//  gGeoMan->PrintOverlaps();
+  gGeoMan->CheckOverlaps(0.001);
+  gGeoMan->PrintOverlaps();
   gGeoMan->Test();
 
   //  top->Export(FileNameSim);   // an alternative way of writing the top volume
--- macro/mcbm/geometry/Create_TRD_Geometry_v18c_1e.C
+++ macro/mcbm/geometry/Create_TRD_Geometry_v18c_1e.C
@@ -507,8 +507,8 @@ void Create_TRD_Geometry_v18c_1e() {
     create_mag_field_vector();
   
   gGeoMan->CloseGeometry();
-//  gGeoMan->CheckOverlaps(0.001);
-//  gGeoMan->PrintOverlaps();
+  gGeoMan->CheckOverlaps(0.001);
+  gGeoMan->PrintOverlaps();
   gGeoMan->Test();
 
   //  top->Export(FileNameSim);   // an alternative way of writing the top volume
