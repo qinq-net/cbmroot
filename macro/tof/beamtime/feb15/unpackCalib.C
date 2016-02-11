1d5ffc383c0deedc05a314f3b2c00a049ef9341e
n.herrmann <n.herrmann@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2016-02-11 13:43:31 +0000

    add missin scripts

---
+++ macro/tof/beamtime/feb15/unpackCalib.C
@@ -0,0 +1,11 @@
+void unpackCalib(char* cFileId="CernSps05Mar0041")
+{
+  FairRunOnline *run = new FairRunOnline ();
+  gROOT->LoadMacro("setup_unpack.C");
+  cout << "Process FileId  "<< cFileId <<endl;
+  Char_t *cCom=Form("setup_unpack(0,\"%s\")",cFileId);
+  cout << "Processline "<<cCom<<endl;
+  gInterpreter->ProcessLine(cCom);
+  run->Run(10000000);
+  run->Finish();
+}
