200a850b62631e2751a333483f35f4d176c666a3
p.-a.loizeau <p.-a.loizeau@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2016-02-16 16:41:00 +0000

    Add macros for CDASH test of following beamtime: STS COSY 14 (LMD), TOF CERN Feb15 (LMD), TOF CERN Nov15 (HLD)

---
+++ macro/beamtime/tests/TofCernFeb15Calib.C
@@ -0,0 +1,75 @@
+
+void TofCernFeb15Calib( Int_t nEvents = 10000 )
+{
+   TString outDir  = "data/";
+  
+   // Function needed for CTest runtime dependency
+   TString depFile = Remove_CTest_Dependency_File(outDir, "TofCernFeb15Calib");
+
+   TString sCalibDir  = "./";
+   TString testDir    = sCalibDir;
+   TString sCalibFile = "TofTdcCalibHistos_batch.root";
+   if (gSystem->FindFile(sCalibDir, sCalibFile)) {
+      TString rmCommand = "rm " + sCalibFile;
+      gSystem->Exec(rmCommand);
+   }
+
+   TString sMacroDir  = gSystem->Getenv("VMCWORKDIR");
+   sMacroDir  +=  "/macro/beamtime/tests/";
+   TString sDataDir  = "/data.local1/cdash/data";
+   TString sFileId = "CernSps05Mar0041";
+
+   // -----   Timer   --------------------------------------------------------
+   TStopwatch timer;
+   timer.Start();
+   // ------------------------------------------------------------------------
+
+   FairRunOnline *run = new FairRunOnline ();
+   Bool_t hasFairMonitor = Has_Fair_Monitor();
+   if (hasFairMonitor) {
+    FairMonitor::GetMonitor()->EnableMonitor(kTRUE);
+   }
+
+   gROOT->LoadMacro( sMacroDir + "tof_feb15_setup_unpack.C");
+
+   cout << "Process FileId  "<< sDataDir << " " << sFileId <<endl;
+
+   TString sCom=Form("setup_unpack(0,\"%s\",\"%s\")", sDataDir.Data(), sFileId.Data() );
+   cout << "Processline "<<sCom<<endl;
+   gInterpreter->ProcessLine(sCom);
+   run->Run(nEvents);
+   run->Finish();
+
+   
+   // --- End-of-run info
+   Double_t rtime = timer.RealTime();
+   Double_t ctime = timer.CpuTime();
+   std::cout << std::endl << std::endl;
+   std::cout << ">>> TofCernFeb15: Macro finished successfully." << std::endl;
+   std::cout << ">>> TofCernFeb15: Real time " << rtime << " s, CPU time "
+                << ctime << " s" << std::endl;
+   std::cout << std::endl;
+
+   /// -----   Resource monitoring in automatic tests  ------------------
+   if ( Has_Fair_Monitor() ) {      // FairRoot Version >= 15.11
+      // Extract the maximal used memory an add is as Dart measurement
+      // This line is filtered by CTest and the value send to CDash
+      FairSystemInfo sysInfo;
+      Float_t maxMemory=sysInfo.GetMaxMemory();
+      std::cout << "<DartMeasurement name=\"MaxMemory\" type=\"numeric/double\">";
+      std::cout << maxMemory;
+      std::cout << "</DartMeasurement>" << std::endl;
+
+      Float_t cpuUsage=ctime/rtime;
+      std::cout << "<DartMeasurement name=\"CpuLoad\" type=\"numeric/double\">";
+      std::cout << cpuUsage;
+      std::cout << "</DartMeasurement>" << std::endl;
+   }
+
+   /// --- Screen output for automatic tests  ---------------------------
+   std::cout << " Test passed" << std::endl;
+   std::cout << " All ok " << std::endl;
+   
+   // Function needed for CTest runtime dependency
+   Generate_CTest_Dependency_File(depFile);
+}
---
+++ macro/beamtime/tests/tof_feb15_setup_unpack.C
@@ -0,0 +1,297 @@
+// -----------------------------------------------------------------------------
+// ----- setup_analysis FairRoot macro                                     -----
+// -----                                                                   -----
+// ----- created by N. Herrmann on 2014-04-18                              -----
+// -----                                                                   -----
+// -----------------------------------------------------------------------------
+
+// Max nEvents: 198999999999
+void setup_unpack(Int_t calMode=1, char *cFileDir="/data.local1/cdash/data", char *cFileId="CernSps05Mar0041", Int_t iSet=0) 
+{
+   // Verbosity level (0=quiet, 1=event level, 2=track level, 3=debug, 4=raw debug)
+  Int_t iVerbose = 2;
+  // Specify log level (INFO, DEBUG, DEBUG1, ...)
+  TString logLevel = "FATAL";
+  //TString logLevel = "ERROR";
+  TString logLevel = "INFO";
+  //TString logLevel = "DEBUG";
+  //TString logLevel = "DEBUG1";
+  //TString logLevel = "DEBUG2";
+  //TString logLevel = "DEBUG3";
+  FairLogger* log;  
+
+   //  Parameter files.
+   // Add TObjectString containing the different file names to
+   // a TList which is passed as input to the FairParAsciiFileIo.
+   // The FairParAsciiFileIo will take care to create on the fly 
+   // a concatenated input parameter file.
+   TList *parFileList = new TList();
+   
+   TString workDir  = gSystem->Getenv("PWD");
+   TString cbmDir = gSystem->Getenv("VMCWORKDIR");
+   TString paramDir = cbmDir + "/macro/tof/beamtime/feb15";
+
+   TObjString unpParFile = paramDir + "/parUnpack_basic.txt";
+   parFileList->Add(&unpParFile);
+
+   TObjString calParFile;
+   TString cOutfileId;
+   TString TofGeo="v15b";
+   TObjString mapParFile;
+
+
+   switch(iSet){
+   case 0: 
+     calParFile = paramDir + "/parCalib_batch.txt";										
+     cOutfileId = Form("%s",cFileId);
+     mapParFile = paramDir + "/parMapCernFeb2015.txt";
+     break;
+
+   case 1:
+     calParFile = paramDir + "/parCalib_basic.txt";
+     cOutfileId = Form("%s%s",cFileId,"_up");
+     mapParFile = paramDir + "/parMapCernFeb2015_up.txt";
+     break;
+
+   case 2:
+     calParFile = paramDir + "/parCalib_basic.txt";
+     cOutfileId = Form("%s%s",cFileId,"_do");
+     mapParFile = paramDir + "/parMapCernFeb2015_do.txt";
+     break;
+
+   case 10:
+     calParFile = paramDir + "/parCalib_tsu.txt";
+     cOutfileId = Form("%s%s",cFileId,"_tsu");
+     TofGeo="v14b_tsu";
+     break;
+   default:
+     ;
+   }
+   parFileList->Add(&calParFile);
+
+   cout << " Output File tag "<< cOutfileId << endl;
+
+   parFileList->Add(&mapParFile);
+   //TObjString convParFile = paramDir + "/parConvFeb2015.txt";
+   //parFileList->Add(&convParFile);
+
+   TObjString tofDigiFile = cbmDir + "/parameters/tof/tof_" + TofGeo + ".digi.par"; // TOF digi file
+   //   parFileList->Add(&tofDigiFile);   
+
+   TObjString tofDigiBdfFile =  paramDir + "/tof.digibdf.par";
+   //parFileList->Add(&tofDigiBdfFile);
+
+   TString geoDir  = gSystem->Getenv("VMCWORKDIR");
+   TString geoFile = geoDir + "/geometry/tof/geofile_tof_" + TofGeo + ".root";
+   TFile* fgeo = new TFile(geoFile);
+
+   TGeoManager *geoMan = (TGeoManager*) fgeo->Get("FAIRGeom");
+   if (NULL == geoMan){
+     cout << "<E> FAIRGeom not found in geoFile"<<endl;
+     //     return;
+   }
+   if(0){
+   TGeoVolume* master=geoMan->GetTopVolume();
+   master->SetVisContainers(1); 
+   master->Draw("ogl"); 
+   }
+   // Parameter file output
+   TString parFile = workDir + "/data/unpack_" + cOutfileId + ".params.root";
+   
+   // Output file
+   TString outFile = workDir + "/data/unpack_" + cOutfileId + ".out.root";
+   // ----    Debug option   -------------------------------------------------
+   gDebug = 0;
+   // ------------------------------------------------------------------------
+
+   // -----   Timer   --------------------------------------------------------
+   TStopwatch timer;
+   timer.Start();
+   // ------------------------------------------------------------------------
+   
+   // =========================================================================
+   // ===                           Unpacker                                ===
+   // =========================================================================
+   FairLmdSource* source = new FairLmdSource();
+   Int_t start_run=0;
+   Int_t end_run  =100;
+   for (Int_t irun=start_run; irun<end_run; irun++)
+   {   
+     TString lmdfile = Form("%s/%s_%04d.lmd",cFileDir,cFileId,irun);   
+      if ( !gSystem->FindFile( cFileDir, Form("%s_%04d.lmd",cFileId,irun) ) )
+      {
+         cout << "<W> Stop adding files after not finding " << lmdfile << endl;
+         break; // Stop looping on first file not found
+      } // if ( !gSystem->FindFile( cFileDir, Form("%s_%04d.lmd",cFileId,irun) ) )
+         
+     cout << "<I> Inputdata file(s) " << lmdfile << endl;
+     source->AddFile(lmdfile); 
+   }
+   
+   /*   
+   TTriglogUnpackTof* tofTriglogUnpacker = new TTriglogUnpackTof();
+   //   tofTriglogUnpacker->SetSaveTriglog(kTRUE);
+   tofTriglogUnpacker->SetSaveTriglog(kFALSE);
+   source->AddUnpacker( tofTriglogUnpacker );
+  
+   TMbsUnpackTof* tofMbsDataUnpacker = new TMbsUnpackTof();
+   source->AddUnpacker( tofMbsDataUnpacker );
+   */
+   TTrbUnpackTof* tofTrbDataUnpacker = new TTrbUnpackTof(10,1,31,0,0);
+   source->AddUnpacker( tofTrbDataUnpacker );
+   
+   gLogger->SetLogScreenLevel(logLevel.Data());
+
+   // -----   Online/Offline MBS run   -----------------------------------
+
+   FairRunOnline *run = FairRunOnline::Instance();
+   run->SetSource(source);
+   run->SetOutputFile(outFile);
+   run->SetAutoFinish(kFALSE);
+
+   // --------------------------------------------------------------------
+   // ===                        End of Unpacker                            ===
+   // =========================================================================
+   
+   // =========================================================================
+   // ===                     Unpacker monitoring                           ===
+   // =========================================================================
+   TMbsUnpTofMonitor* tofUnpMonitor = new TMbsUnpTofMonitor("Tof Unp Moni", iVerbose);
+   run->AddTask(tofUnpMonitor);
+   // ===                 End of Unpacker monitoring                        ===
+   // =========================================================================
+   
+   // =========================================================================
+   // ===                         Calibration                               ===
+   // =========================================================================
+   
+   TMbsCalibTof* tofCalibration = new TMbsCalibTof("Tof Calibration", iVerbose);
+   tofCalibration->SetTdcCalibOutFoldername("./");
+   tofCalibration->SetTdcCalibFilename("batch");
+   run->AddTask(tofCalibration);
+   // ===                      End of Calibration                           ===
+   // =========================================================================
+   
+   // =========================================================================
+   // ===                         Mapping                                   ===
+   // =========================================================================
+   if(calMode>0){
+   TMbsMappingTof* tofMapping = new TMbsMappingTof("Tof Mapping", iVerbose);
+   tofMapping->SetSaveDigis(kTRUE);
+   run->AddTask(tofMapping);
+   }
+   // ===                      End of Mapping                               ===
+   // =========================================================================
+   if(0){  // activation leads to crash in FairRootManager !? 
+   CbmTofTestBeamClusterizer* tofTestBeamClust = new CbmTofTestBeamClusterizer("TOF TestBeam Clusterizer",iVerbose, kFALSE);
+   //tofTestBeamClust->SetCalParFileName("MbsTrbBeamtofTestBeamClust.hst.root");
+   Int_t calTrg=-1;
+   Int_t calSmType = 0;
+   Int_t RefTrg=0;
+
+   tofTestBeamClust->SetCalMode(0);
+   tofTestBeamClust->SetCalSel(calTrg);
+   tofTestBeamClust->SetCaldXdYMax(150.);         // geometrical matching window
+   tofTestBeamClust->SetdTRefMax(500000.);
+   tofTestBeamClust->SetCalSmType(calSmType);    // select detectors for walk correction 
+   //tofTestBeamClust->SetTRefDetId(20486);        //0x00005006; big Diamond
+   //tofTestBeamClust->SetTRefDetId(8214);       //0x00002016; Plastic 1
+   //tofTestBeamClust->SetTRefDetId(8198);       //0x00002006;  Plastic 0 
+   tofTestBeamClust->SetTRefId(RefTrg);          //  reference trigger 5 -> Diamond  
+   tofTestBeamClust->SetTotMax(100000.);
+   tofTestBeamClust->SetTotMin(1.); 
+
+   tofTestBeamClust->SetBeamRefType(4);    // Test case 
+   tofTestBeamClust->SetBeamRefSm(0);
+   tofTestBeamClust->SetBeamRefDet(0);
+   tofTestBeamClust->SetBeamAddRefMul(-1);
+
+   switch (calMode) {
+   case 0: 
+     tofTestBeamClust->SetTotMax(100000.);      // 100 ns
+     //tofTestBeamClust->SetTotMin(1.);
+     tofTestBeamClust->SetTRefDifMax(2000000.); // in ps 
+     tofTestBeamClust->PosYMaxScal(2000.);      // in % of length 
+     tofTestBeamClust->SetMaxTimeDist(0.);      // no cluster building  
+     break;
+   case 1:
+     tofTestBeamClust->SetTRefDifMax(100000.);    // in ps 
+     tofTestBeamClust->PosYMaxScal(1000.);         //in % of length 
+     break;
+   case 2:                                      // time difference calibration
+     tofTestBeamClust->SetCalParFileName("MbsTrbBeam02tofTestBeamClust.hst.root");
+     tofTestBeamClust->SetTRefDifMax(300000.);   // in ps 
+     tofTestBeamClust->PosYMaxScal(1000.);         //in % of length
+     break;
+   case 3:
+     //	 tofTestBeamClust->SetCalParFileName("MbsTrbBeam00tofTestBeamClust.hst.root");
+     // tofTestBeamClust->SetCalParFileName("MbsTrbThu0138_01tofTestBeamClust.hst.root");
+     tofTestBeamClust->SetCalParFileName("MbsTrbThu224_03tofTestBeamClust.hst.root");
+     tofTestBeamClust->SetTRefDifMax(200000.);   // in ps 
+     tofTestBeamClust->PosYMaxScal(2.);       //in % of length
+     break;
+   case 13:
+     tofTestBeamClust->SetTRefDifMax(50000.);   // in ps 
+     tofTestBeamClust->PosYMaxScal(2.);       //in % of length
+     break;
+   default:
+     cout << "<E> Calib mode not implemented! stop execution of script"<<endl;
+     return;
+   } 
+   run->AddTask(tofTestBeamClust);
+   }
+   
+   // =========================================================================
+   // ===                  GO4 like output formatting                       ===
+   // =========================================================================
+//   TMbsConvTof* tofConversion = new TMbsConvTof("Tof Conversion", iVerbose);
+//   run->AddTask(tofConversion);
+   // ===                  End of output conversion                         ===
+   // =========================================================================
+   CbmTofOnlineDisplay* display = new CbmTofOnlineDisplay();
+   display->SetUpdateInterval(1000000);
+   run->AddTask(display);   
+
+   // -----  Parameter database   --------------------------------------------
+   FairRuntimeDb* rtdb = run->GetRuntimeDb();
+   
+   Bool_t kParameterMerged = kTRUE;
+   FairParRootFileIo* parIo2 = new FairParRootFileIo(kParameterMerged);
+   parIo2->open(parFile.Data(), "UPDATE");
+   parIo2->print();
+   rtdb->setFirstInput(parIo2);
+   
+   FairParAsciiFileIo* parIo1 = new FairParAsciiFileIo();
+   parIo1->open(parFileList, "in");
+   parIo1->print();
+//   rtdb->setFirstInput(parIo1);
+   rtdb->setSecondInput(parIo1);
+   
+   rtdb->print();
+   rtdb->printParamContexts();
+   // ------------------------------------------------------------------------
+
+   // -----   Intialise and run   --------------------------------------------
+   run->Init();
+
+   cout << "Starting run" << endl;
+//   run->Run(nEvents, 0);
+   // ------------------------------------------------------------------------
+
+   // -----   Finish   -------------------------------------------------------
+/*
+   timer.Stop();
+   Double_t rtime = timer.RealTime();
+   Double_t ctime = timer.CpuTime();
+   cout << endl << endl;
+   cout << "Macro finished successfully." << endl;
+   cout << "Output file is " << outFile << endl;
+   cout << "Parameter file is " << parFile << endl;
+   cout << "Real time " << rtime << " s, CPU time " << ctime << " s" << endl;
+   cout << endl;
+*/
+   // ------------------------------------------------------------------------
+
+//   delete run;
+}
+
