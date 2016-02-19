79bb0e90e5ac46dcfd0d646454f1e3798eeed261
c.bergmann <c.bergmann@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2016-02-19 08:26:15 +0000

    added debug to unpacker in case message is not hit, hit aborted, epoch, info or overflow

--- fles/reader/unpacker/CbmTSUnpackSpadic.cxx
+++ fles/reader/unpacker/CbmTSUnpackSpadic.cxx
@@ -71,12 +71,12 @@ Bool_t CbmTSUnpackSpadic::DoUnpack(const fles::Timeslice& ts, size_t component)
       Int_t link = ts.descriptor(component, 0).eq_id;
       Int_t address = addr;
       /*
-      if ( mp->is_valid() ){
+	if ( mp->is_valid() ){
 	LOG(DEBUG) << counter << " This message is valid" << FairLogger::endl; 
-      } else {
+	} else {
 	LOG(ERROR) << counter <<  " This message is not valid" << FairLogger::endl; 
 	continue;
-      }
+	}
       */
       Bool_t isInfo(false), isHit(false), isEpoch(false);
       if ( mp->is_epoch_out_of_sync() ){
@@ -100,7 +100,7 @@ Bool_t CbmTSUnpackSpadic::DoUnpack(const fles::Timeslice& ts, size_t component)
 	Int_t groupId = mp->group_id();
 	Int_t channel = mp->channel_id();
 	Int_t time = mp->timestamp();
-        Int_t bufferOverflowCounter = mp->buffer_overflow_count();
+        Int_t bufferOverflowCounter = static_cast<Int_t>(mp->buffer_overflow_count());
 	Int_t samples = 1;
 	Int_t* sample_values = new Int_t[samples];
 	sample_values[0] = -256;
@@ -187,6 +187,22 @@ Bool_t CbmTSUnpackSpadic::DoUnpack(const fles::Timeslice& ts, size_t component)
       }
       else {
 	LOG(INFO) <<  counter << " This message type is not hit, info, epoch or overflow and will not be stored in the TClonesArray" << FairLogger::endl; 
+	LOG(INFO) << " valide:" << mp->is_valid() << " epoch marker:" << fEpochMarker << " super epoch marker:" << fSuperEpoch << " time:" << time << " link:" << link << " address:" << address << FairLogger::endl;
+	LOG(INFO) << "Channel ID:" << mp->channel_id() << FairLogger::endl;
+	//if ( mp->is_hit_aborted() )
+	//LOG(INFO) << "hit is aborted" << FairLogger::endl;
+	//if ( mp->is_hit() )
+	LOG(INFO) << "GroupID:" << mp->group_id() << "hit: triggerType:" << static_cast<Int_t>(mp->hit_type()) << " stopType:" << static_cast<Int_t>(mp->stop_type()) << " Nr.of samples:" << mp->samples().size() << FairLogger::endl;
+
+	//if ()
+	for (auto x : mp->samples()) {
+	  LOG(INFO) << " " << x;
+	}
+	LOG(INFO) << FairLogger::endl;
+	//if (mp->is_info())
+	LOG(INFO) << "InfoType:" << static_cast<Int_t>(mp->info_type()) << FairLogger::endl;
+	//if ()
+	LOG(INFO) << "Nr. of overflows:" << static_cast<Int_t>(mp->buffer_overflow_count()) << FairLogger::endl;
 	//print_message(mp);
       }
     }
@@ -194,87 +210,87 @@ Bool_t CbmTSUnpackSpadic::DoUnpack(const fles::Timeslice& ts, size_t component)
   return kTRUE;
 }
 
-void CbmTSUnpackSpadic::print_message(const spadic::Message& m)
-{
-  LOG(INFO) << "v: " << (m.is_valid() ? "o" : "x");
-  LOG(DEBUG) << " / gid: " << static_cast<int>(m.group_id());
-  LOG(DEBUG) << " / chid: " << static_cast<int>(m.channel_id());
-  if ( m.is_hit() ) { 
-    LOG(DEBUG) << " / ts: " << m.timestamp();
-    LOG(DEBUG) << " / samples (" << m.samples().size() << "):";
-    for (auto x : m.samples()) {
-      LOG(DEBUG) << " " << x;
-    }
-    LOG(DEBUG) << FairLogger::endl;
-  } else {
-    if ( m.is_epoch_marker() ) { 
-      LOG(DEBUG) << " This is an Epoch Marker" << FairLogger::endl; 
-    } else if ( m.is_epoch_out_of_sync() ) { 
-      LOG(INFO) << " This is an out of sync Epoch Marker" << FairLogger::endl; 
+  void CbmTSUnpackSpadic::print_message(const spadic::Message& m)
+  {
+    LOG(INFO) << "v: " << (m.is_valid() ? "o" : "x");
+    LOG(DEBUG) << " / gid: " << static_cast<int>(m.group_id());
+    LOG(DEBUG) << " / chid: " << static_cast<int>(m.channel_id());
+    if ( m.is_hit() ) { 
+      LOG(DEBUG) << " / ts: " << m.timestamp();
+      LOG(DEBUG) << " / samples (" << m.samples().size() << "):";
+      for (auto x : m.samples()) {
+	LOG(DEBUG) << " " << x;
+      }
+      LOG(DEBUG) << FairLogger::endl;
     } else {
-      LOG(INFO) << " This is not known" << FairLogger::endl;
+      if ( m.is_epoch_marker() ) { 
+	LOG(DEBUG) << " This is an Epoch Marker" << FairLogger::endl; 
+      } else if ( m.is_epoch_out_of_sync() ) { 
+	LOG(INFO) << " This is an out of sync Epoch Marker" << FairLogger::endl; 
+      } else {
+	LOG(INFO) << " This is not known" << FairLogger::endl;
+      }
     }
   }
-}
 
-void CbmTSUnpackSpadic::FillEpochInfo(Int_t link, Int_t addr, Int_t epoch_count) 
-{
-  auto it=groupToExpMap.find(link);
-  if (it == groupToExpMap.end()) {
-    LOG(FATAL) << "Could not find an entry for equipment ID" << 
-      std::hex << link << std::dec << FairLogger::endl;
-  } else {
-    if ( epoch_count < fEpochMarkerArray[it->second][addr] ) {
-      fSuperEpochArray[it->second][addr]++;
-      LOG(DEBUG) << "Overflow of EpochCounter for Syscore" 
-		<< it->second << "_Spadic"  
-		<< addr << FairLogger::endl;
-    } else if ((epoch_count - fEpochMarkerArray[it->second][addr]) !=1 ) {
-      LOG(INFO) << "Missed epoch counter for Syscore" 
-		<< it->second << "_Spadic"  
-		<< addr << FairLogger::endl;
+  void CbmTSUnpackSpadic::FillEpochInfo(Int_t link, Int_t addr, Int_t epoch_count) 
+  {
+    auto it=groupToExpMap.find(link);
+    if (it == groupToExpMap.end()) {
+      LOG(FATAL) << "Could not find an entry for equipment ID" << 
+	std::hex << link << std::dec << FairLogger::endl;
+    } else {
+      if ( epoch_count < fEpochMarkerArray[it->second][addr] ) {
+	fSuperEpochArray[it->second][addr]++;
+	LOG(DEBUG) << "Overflow of EpochCounter for Syscore" 
+		   << it->second << "_Spadic"  
+		   << addr << FairLogger::endl;
+      } else if ((epoch_count - fEpochMarkerArray[it->second][addr]) !=1 ) {
+	LOG(INFO) << "Missed epoch counter for Syscore" 
+		  << it->second << "_Spadic"  
+		  << addr << FairLogger::endl;
+      }
+      fEpochMarkerArray[it->second][addr] = epoch_count; 
     }
-    fEpochMarkerArray[it->second][addr] = epoch_count; 
+
   }
 
-}
-
-void CbmTSUnpackSpadic::GetEpochInfo(Int_t link, Int_t addr) 
-{
-  auto it=groupToExpMap.find(link);
-  if (it == groupToExpMap.end()) {
-    LOG(FATAL) << "Could not find an entry for equipment ID" << 
-      std::hex << link << std::dec << FairLogger::endl;
-  } else {
-    fEpochMarker = fEpochMarkerArray[it->second][addr]; 
-    fSuperEpoch = fSuperEpochArray[it->second][addr]; 
-  }
+  void CbmTSUnpackSpadic::GetEpochInfo(Int_t link, Int_t addr) 
+  {
+    auto it=groupToExpMap.find(link);
+    if (it == groupToExpMap.end()) {
+      LOG(FATAL) << "Could not find an entry for equipment ID" << 
+	std::hex << link << std::dec << FairLogger::endl;
+    } else {
+      fEpochMarker = fEpochMarkerArray[it->second][addr]; 
+      fSuperEpoch = fSuperEpochArray[it->second][addr]; 
+    }
   
-}
-
-void CbmTSUnpackSpadic::Reset()
-{
-  fSpadicRaw->Clear();
-}
-
-void CbmTSUnpackSpadic::Finish()
-{
-  for (Int_t i=0; i < NrOfSyscores; ++i) { 
-    for (Int_t j=0; j < NrOfHalfSpadics; ++j) { 
-      LOG(DEBUG) << "There have been " << fSuperEpochArray[i][j] 
-		<< " SuperEpochs for Syscore" << i << "_Spadic" 
-		<< j << " in this file" << FairLogger::endl;
-    }
   }
 
-}
+  void CbmTSUnpackSpadic::Reset()
+  {
+    fSpadicRaw->Clear();
+  }
+
+  void CbmTSUnpackSpadic::Finish()
+  {
+    for (Int_t i=0; i < NrOfSyscores; ++i) { 
+      for (Int_t j=0; j < NrOfHalfSpadics; ++j) { 
+	LOG(DEBUG) << "There have been " << fSuperEpochArray[i][j] 
+		   << " SuperEpochs for Syscore" << i << "_Spadic" 
+		   << j << " in this file" << FairLogger::endl;
+      }
+    }
+
+  }
 
 
-/*
-void CbmTSUnpackSpadic::Register()
-{
-}
-*/
+  /*
+    void CbmTSUnpackSpadic::Register()
+    {
+    }
+  */
 
 
-ClassImp(CbmTSUnpackSpadic)
+  ClassImp(CbmTSUnpackSpadic)
