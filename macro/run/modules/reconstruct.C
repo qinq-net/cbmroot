2d00fa43077aa00503be51a42ba071307c1d817a
v.friese <v.friese@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2016-05-06 13:36:33 +0000

    Added MUCH parameter files needed for local reconstruction in MUCH. Refs #655 @0.5h

--- macro/run/modules/reconstruct.C
+++ macro/run/modules/reconstruct.C
@@ -67,10 +67,23 @@ void reconstruct()
   // -----   Local reconstruction in MUCH   ---------------------------------
   if ( setup->IsActive(kMuch) ) {
 
-  FairTask* muchHitGem = new CbmMuchFindHitsGem(muchDigiFile.Data());
-  run->AddTask(muchHitGem);
-  CbmMuchFindHitsStraws* strawFindHits = new CbmMuchFindHitsStraws(muchDigiFile.Data());
-  run->AddTask(strawFindHits);
+		// --- Parameter file name
+		TString geoTag;
+		setup->GetGeoTag(kMuch, geoTag);
+		std::cout << geoTag(0,4) << std::endl;
+		TString parFile = gSystem->Getenv("VMCWORKDIR");
+		parFile = parFile + "/parameters/much/much_" + geoTag(0,4)
+				    + "_digi_sector.root";
+		std::cout << "Using parameter file " << parFile << std::endl;
+
+		// --- Hit finder for GEMs
+		FairTask* muchHitGem = new CbmMuchFindHitsGem(muchDigiFile.Data());
+		run->AddTask(muchHitGem);
+
+		// --- Hit finder for Straws
+		CbmMuchFindHitsStraws* strawFindHits =
+				new CbmMuchFindHitsStraws(muchDigiFile.Data());
+		run->AddTask(strawFindHits);
 
   }
 
