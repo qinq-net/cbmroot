9186a079650fc627e7bc060cf7d21abbfb1419b8
f.uhlig <f.uhlig@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2016-06-01 13:15:21 +0000

    Move all old setup files to legacy directory.

--- geometry/setup/legacy/sis300_electron_setup.C
+++ geometry/setup/legacy/sis300_electron_setup.C
@@ -1,7 +1,17 @@
 //
 //   comments
 //
-// 2014-07-03 - DE - use PIPE v14h together with RICH v14a
+
+// 2016-02-25 - DE - use STS v16c as new default
+// 2015-12-11 - DE - use STS v15c as new default
+// 2015-07-20 - FU - magnet v15a is v12b with correct keeping volume material
+// 2015-07-20 - FU - add material budget files
+// 2015-03-13 - DE - use STS v13y with correct ladder orientation as new default
+// 2015-01-28 - DE - use STS v13x fitting with with PIPE v14n
+// 2015-01-22 - DE - use MVD v14b with PIPE v14n as default
+// 2015-01-19 - DE - use TRD v15a as new default
+// 2014-07-05 - DE - use PIPE v14y as interim solution
+// 2014-07-03 - DE - use PIPE v14h together with RICH v14a -> overlap with STS
 // 2014-06-30 - DE - make RICH v14a the new default
 // 2014-06-30 - DE - make TRD v14a the new default
 // 2014-06-25 - DE - define digi files through tags for STS, TRD and TOF
@@ -9,41 +19,12 @@
 // 2013-10-11 - DE - add empty string defining the platform
 // 2013-10-07 - DE - pipe_v13d.geo fixes overlap with TOF
 // 2013-10-07 - DE - rich_v13a is not at nominal position 1800mm, but at 1600mm
+// 2013-11-04 - VF - make STS v15b the new default
 //
 
-static TString caveGeom;
-static TString pipeGeom;
-static TString magnetGeom;
-static TString mvdGeom;
-static TString stsGeom;
-static TString richGeom;
-static TString muchGeom;
-static TString shieldGeom;
-static TString trdGeom;
-static TString tofGeom;
-static TString ecalGeom;
-static TString platformGeom;
-static TString psdGeom;
-static Double_t psdZpos;
-static Double_t psdXpos;
-
-static TString stsTag;
-static TString trdTag;
-static TString tofTag;
-
-static TString stsDigi;
-static TString trdDigi;
-static TString tofDigi;
-
-static TString  fieldMap;
-static Double_t fieldZ;
-static Double_t fieldScale;
-static Int_t    fieldSymType;
 
 void sis300_electron_setup()
 {
-  FairLogger* logger = FairLogger::GetLogger();
-
   // -----   Magnetic field   -----------------------------------------------
   fieldMap     = "field_v12b";   // name of field map
   fieldZ       = 40.;            // field centre z position
@@ -51,23 +32,29 @@ void sis300_electron_setup()
   fieldSymType =  3;
 
   // -----  Tags  -----------------------------------------------------------
-  stsTag       = "v13d";
-  trdTag       = "v14a_3e";
-  tofTag       = "v13-5d";
+  mvdTag       = "v15a";
+  stsTag       = "v16c";
+  trdTag       = "v15a_3e";
+  tofTag       = "v16a_3e";
 
   // -----  DigiFiles  ------------------------------------------------------
-  stsDigi      = "sts/sts_" + stsTag + "_std.digi.par";
+  stsDigi      = ""; // "sts/sts_" + stsTag + "_std.digi.par";
+  muchDigi     = "";
   trdDigi      = "trd/trd_" + trdTag + ".digi.par";
-  //  tofDigi      = "tof/tof_" + tofTag + ".digi.par";
-  tofDigi      = "tof/tof_v13b.digi.par";
+  tofDigi      = "tof/tof_" + tofTag + ".digi.par";
+  tofDigiBdf   = "tof/tof_" + tofTag + ".digibdf.par";
+
+  // -----  Material budget files -------------------------------------------
+  mvdMatBudget = "mvd/mvd_matbudget_" + mvdTag + ".root";
+  stsMatBudget = "sts/sts_matbudget_" + stsTag + ".root";
 
   // -----  Geometries  -----------------------------------------------------
   caveGeom     = "cave.geo";
-  pipeGeom     = "pipe/pipe_v13b.geo.root";
-  magnetGeom   = "magnet/magnet_v12b.geo.root";
-  mvdGeom      = "";
+  pipeGeom     = "pipe/pipe_v14n.root";
+  magnetGeom   = "magnet/magnet_v15a.geo.root";
+  mvdGeom      = "mvd/mvd_" + mvdTag + ".geo.root";
   stsGeom      = "sts/sts_" + stsTag + ".geo.root";
-  richGeom     = "rich/rich_v08a.geo";
+  richGeom     = "rich/rich_v16a_3e.geo.root";
   muchGeom     = "";
   shieldGeom   = "";
   platformGeom = ""; // "passive/platform_v13b.geo";
@@ -78,5 +65,11 @@ void sis300_electron_setup()
   psdZpos = 1500.;
   psdXpos = 10.;
 
-  cout<<"Reading geometry setup and magnetic field settings from "<< gSystem->UnixPathName(gInterpreter->GetCurrentMacroName())<<endl;
+  // ------ Input file ------------------------------------------------------
+
+  defaultInputFile = "/input/urqmd.auau.25gev.centr.root";
+
+  cout << "Reading geometry setup and magnetic field settings from "
+       << gSystem->UnixPathName(gInterpreter->GetCurrentMacroName())
+       << endl;
 }
