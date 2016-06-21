f0da90800ffb2f532b97270e502d59b1c34a6182
p.-a.loizeau <p.-a.loizeau@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2016-06-21 11:53:32 +0000

    Remove from compilation deprecated classes in the global folder. Used only in deprecated macro macro/global/Merger.C. refs #587 @1h

--- global/CbmGlobalLinkDef.h
+++ global/CbmGlobalLinkDef.h
@@ -7,14 +7,14 @@
 
 #pragma link C++ class CbmEcalTrackingMuch+;
 #pragma link C++ class CbmFindGlobalTracks+;
-#pragma link C++ class CbmFindGlobalTracksQa+;
+//#pragma link C++ class CbmFindGlobalTracksQa+;
 #pragma link C++ class CbmFindPrimaryVertex+;
 #pragma link C++ class CbmFitGlobalTracks+;
 #pragma link C++ class CbmFitGlobalTracksQa+;
 #pragma link C++ class CbmGlobalTrackFitterIdeal+;
 #pragma link C++ class CbmPVFinderIdeal+;
 #pragma link C++ class CbmTrackMergerIdeal+;
-#pragma link C++ class CbmTofMergerIdeal+;
+//#pragma link C++ class CbmTofMergerIdeal+;
 
 
 #endif
