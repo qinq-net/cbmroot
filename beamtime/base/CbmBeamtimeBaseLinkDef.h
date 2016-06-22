ac464674382392cbd909480441246c465ea91dbe
f.uhlig <f.uhlig@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2016-06-22 13:16:45 +0000

    Add new class to beamtime base library.

--- beamtime/base/CbmBeamtimeBaseLinkDef.h
+++ beamtime/base/CbmBeamtimeBaseLinkDef.h
@@ -5,6 +5,7 @@
 #pragma link off all functions;
 
 #pragma link C++ class CbmROCUnpack;
+#pragma link C++ class CbmTSUnpack;
 #pragma link C++ class CbmTbEventBuilder;
 #pragma link C++ class CbmTbEventBuilderFixedTimeWindow;
 #pragma link C++ class CbmTbDaqBuffer;
