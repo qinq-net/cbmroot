747b1ab4f33ad5ab3d073f4309e574c5c9b28813
t.ablyazimov <t.ablyazimov@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2016-06-24 10:57:14 +0000

    Deversifying Lx implementations. Step 4.

---
+++ tracking/lx/LxLinkDef.h
@@ -0,0 +1,13 @@
+// $Id: LxLinkDef.h,v 1.10 2013/09/30 11:36:47 tablyaz Exp $
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class  LxTBFinder+;
+//#pragma link C++ class  LxTBMLFinder+;
+//#pragma link C++ class  LxTBMPFinder+;
+
+#endif
