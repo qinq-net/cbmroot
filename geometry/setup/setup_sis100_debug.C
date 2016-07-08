80fbd2031c4ca4bebfc9ee48db3a38b0a2ae698e
d.emschermann <d.emschermann@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2016-07-08 14:51:40 +0000

    disable platform until segfault is fixed

--- geometry/setup/setup_sis100_debug.C
+++ geometry/setup/setup_sis100_debug.C
@@ -38,7 +38,7 @@ void setup_sis100_debug()
   TString trdGeoTag       = "v16a_1h";  // exported
   TString tofGeoTag       = "v17a_1h";  // exported
   TString psdGeoTag       = "psd_geo_xy.txt";
-  TString platGeoTag      = "v13a";
+  TString platGeoTag      = "v16a";     // exported
   // ------------------------------------------------------------------------
 
 
@@ -70,7 +70,7 @@ void setup_sis100_debug()
   setup->SetModule(kSts, stsGeoTag);
   setup->SetModule(kTrd, trdGeoTag);
   //  setup->SetModule(kTof, tofGeoTag);
-  setup->SetModule(kPlatform, platGeoTag);
+  //  setup->SetModule(kPlatform, platGeoTag);
   setup->SetPsd(psdGeoFile, psdZpos, psdXpos);
   setup->SetField(fieldTag, fieldScale, 0., 0., fieldZ);
   // ------------------------------------------------------------------------
