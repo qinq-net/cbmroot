bca4911e5d95fb22b0e529eb52c020e354cb3b95
d.emschermann <d.emschermann@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2016-07-11 21:26:52 +0000

    generate all 8 station screenshots in 1 bash script

--- macro/sts/geometry/geom_sts.C
+++ macro/sts/geometry/geom_sts.C
@@ -5,11 +5,19 @@
 // for deatils see
 // https://root.cern.ch/download/doc/ROOTUsersGuideHTML/ch09s12.html
 
-void geom_sts()
+void geom_sts(const int statid = 1, const char* ststag = "v17a")
 {
    TEveManager::Create();
 
    TFile::SetCacheFileDir(".");
+
+   //   TString thests = "sts_" + ststag.Data() + "_1";
+   TString thests = Form("sts_%s_1", ststag);
+   std::cout << thests << std::endl;   
+
+   TString thestat = Form("Station%02d_1", statid);
+   TString pngstat = Form("sts_%s_station%02d.png", ststag, statid);
+   std::cout << thestat << std::endl << std::endl;
  
    gGeoManager = gEve->GetGeometry("http://cbm.uni-muenster.de/sts/more/sts_v17a_geo.root");
 //   gGeoManager->DefaultColors();
@@ -25,39 +33,44 @@ void geom_sts()
 //   gEve->AddGlobalElement(stat1);
 //   
 //   TGeoVolume* top = gGeoManager->GetTopVolume()->FindNode("sts_v17a_1")->GetVolume();
-//   TEveGeoTopNode* stat2 = new TEveGeoTopNode(gGeoManager, top->FindNode("Station02_2"));
+//   TEveGeoTopNode* stat2 = new TEveGeoTopNode(gGeoManager, top->FindNode("Station02_1"));
 //   stat2->SetVisLevel(6);
 //   gEve->AddGlobalElement(stat2);
 //   
 //   TGeoVolume* top = gGeoManager->GetTopVolume()->FindNode("sts_v17a_1")->GetVolume();
-//   TEveGeoTopNode* stat3 = new TEveGeoTopNode(gGeoManager, top->FindNode("Station03_3"));
+//   TEveGeoTopNode* stat3 = new TEveGeoTopNode(gGeoManager, top->FindNode("Station03_1"));
 //   stat3->SetVisLevel(6);
 //   gEve->AddGlobalElement(stat3);
 //   
 //   TGeoVolume* top = gGeoManager->GetTopVolume()->FindNode("sts_v17a_1")->GetVolume();
-//   TEveGeoTopNode* stat4 = new TEveGeoTopNode(gGeoManager, top->FindNode("Station04_4"));
+//   TEveGeoTopNode* stat4 = new TEveGeoTopNode(gGeoManager, top->FindNode("Station04_1"));
 //   stat4->SetVisLevel(6);
 //   gEve->AddGlobalElement(stat4);
 //      
 //   TGeoVolume* top = gGeoManager->GetTopVolume()->FindNode("sts_v17a_1")->GetVolume();
-//   TEveGeoTopNode* stat5 = new TEveGeoTopNode(gGeoManager, top->FindNode("Station05_5"));
+//   TEveGeoTopNode* stat5 = new TEveGeoTopNode(gGeoManager, top->FindNode("Station05_1"));
 //   stat5->SetVisLevel(6);
 //   gEve->AddGlobalElement(stat5);
 //   
 //   TGeoVolume* top = gGeoManager->GetTopVolume()->FindNode("sts_v17a_1")->GetVolume();
-//   TEveGeoTopNode* stat6 = new TEveGeoTopNode(gGeoManager, top->FindNode("Station06_6"));
+//   TEveGeoTopNode* stat6 = new TEveGeoTopNode(gGeoManager, top->FindNode("Station06_1"));
 //   stat6->SetVisLevel(6);
 //   gEve->AddGlobalElement(stat6);
 //   
 //   TGeoVolume* top = gGeoManager->GetTopVolume()->FindNode("sts_v17a_1")->GetVolume();
-//   TEveGeoTopNode* stat7 = new TEveGeoTopNode(gGeoManager, top->FindNode("Station07_7"));
+//   TEveGeoTopNode* stat7 = new TEveGeoTopNode(gGeoManager, top->FindNode("Station07_1"));
 //   stat7->SetVisLevel(6);
 //   gEve->AddGlobalElement(stat7);
+//   
+//   TGeoVolume* top = gGeoManager->GetTopVolume()->FindNode("sts_v17a_1")->GetVolume();
+//   TEveGeoTopNode* stat8 = new TEveGeoTopNode(gGeoManager, top->FindNode("Station08_1"));
+//   stat8->SetVisLevel(6);
+//   gEve->AddGlobalElement(stat8);
    
-   TGeoVolume* top = gGeoManager->GetTopVolume()->FindNode("sts_v17a_1")->GetVolume();
-   TEveGeoTopNode* stat8 = new TEveGeoTopNode(gGeoManager, top->FindNode("Station08_8"));
-   stat8->SetVisLevel(6);
-   gEve->AddGlobalElement(stat8);
+   TGeoVolume* top = gGeoManager->GetTopVolume()->FindNode(thests.Data())->GetVolume();
+   TEveGeoTopNode* stat = new TEveGeoTopNode(gGeoManager, top->FindNode(thestat.Data()));
+   stat->SetVisLevel(6);
+   gEve->AddGlobalElement(stat);
    
    gEve->FullRedraw3D(kTRUE);
 
@@ -83,16 +96,17 @@ void geom_sts()
 
    v->RefreshPadEditor(v);
 
-   v->DoDraw();
-   v->SavePicture("sts_1.png");
+//   v->DoDraw();
+//   v->SavePicture("sts_1.png");
 
    //   v->CurrentCamera().RotateRad(-.7, 0.5);
    v->CurrentCamera().RotateRad(0, acos(-1)/2.);  // check from frontside
    v->DoDraw();
-   v->SavePicture("sts_2.png");
+   v->SavePicture(pngstat.Data());
+   //   v->SavePicture("sts_2.png");
 
-   v->CurrentCamera().RotateRad(0, acos(-1));  // check from backside
-   v->DoDraw();
-   v->SavePicture("sts_3.png");
+//   v->CurrentCamera().RotateRad(0, acos(-1));  // check from backside
+//   v->DoDraw();
+//   v->SavePicture("sts_3.png");
 
 }
---
+++ macro/sts/geometry/geom_sts.sh
@@ -0,0 +1,4 @@
+for STA in 1 2 3 4 5 6 7 8 
+do
+  root -l -q geom_sts.C\($STA\)
+done
