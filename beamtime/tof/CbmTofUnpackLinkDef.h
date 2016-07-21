d6c50fb40b2c3cc5c4f6ed28ba01ba278612e918
p.-a.loizeau <p.-a.loizeau@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2016-07-21 12:20:05 +0000

    Re-Enable most of the TOF beamtime library, independent on the FairRoot version. Now the only class not compiled for FairRoot above v15.11b is the HLD file unpacker CbmHldSource

--- beamtime/tof/CbmTofUnpackLinkDef.h
+++ beamtime/tof/CbmTofUnpackLinkDef.h
@@ -12,7 +12,9 @@
 #pragma link C++ class hadaq::LocalStampConverter+;
 #pragma link C++ class hadaq::TdcIterator+;
 #pragma link C++ struct hadaq::TdcMessage+;
-#pragma link C++ class CbmHldSource+;
+#ifdef BUILD_TOF_BEAMTIME
+ #pragma link C++ class CbmHldSource+;
+#endif // NOT BUILD_BEAMTIME
 
 #pragma link C++ class TMbsUnpackTofPar+;
 #pragma link C++ class TMbsUnpackTofContFact+;
