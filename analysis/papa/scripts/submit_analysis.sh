5e17b3eabd6e69a468539d782b490167a7f106f7
j.book <j.book@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2016-08-10 14:08:02 +0000

    add fast run list in test case

--- analysis/papa/scripts/submit_analysis.sh
+++ analysis/papa/scripts/submit_analysis.sh
@@ -48,7 +48,11 @@ if grep -Fq "$indir/" runListSort.txt; then
     echo "sorted run list already there, only split the list";
 else
     ## only collect file with validated reconstruction output
-    find $indir -type f -name "run_reco_${SETUP}_ok" -exec dirname {} \; > runList.txt;
+    if [[ $TEST == "1" ]] ; then
+	find $indir -type f -name "run_reco_${SETUP}_ok" -exec dirname {} \; -quit > runList.txt;
+    else
+	find $indir -type f -name "run_reco_${SETUP}_ok" -exec dirname {} \; > runList.txt;
+    fi
     sort runList.txt > runListSort.txt
 fi
 
@@ -59,50 +63,52 @@ ListOfFilelist=`ls filelist_${train}_*`
 # loop over all lists
 I=0
 for filelist in $ListOfFilelist ; do
-  let I=$I+1
+    let I=$I+1
 
-  ## create output directory for job $I
-  outdir=$out/`printf "%04d/" $I`
-  echo "-$outdir-"
-  mkdir -p $outdir
+    ## create output directory for job $I
+    outdir=$out/`printf "%04d/" $I`
+    echo "-$outdir-"
+    mkdir -p $outdir
 
-  ## test analysis (abort after first job)
-  if [[ $TEST == "1" ]] ; then
+    ## test analysis (abort after first job)
+    if [[ $TEST == "1" ]] ; then
 
-      rm $(ls filelist_${train}_* | grep -v aaaa)        # clean up
-      ./analysis.sh $indir $filelist $outdir $NEVENT     # LOCAL run
-      break;
+	# clean up
+	rm $(ls filelist_${train}_* | grep -v aaaa)
+	rm -v runListSort.txt;
+	./analysis.sh $indir $filelist $outdir $NEVENT     # LOCAL run
+	break;
 
-  else
-      #### gsi batch environement for kronos (slurm) ####
-      #### NOTE: possible partitions: sinfo -o "%9P  %6g %10l %5w %5D %13C %N"
-      ####                      and : scontrol show partition main
-      #### long: upto 7d (CPUs=3880)
-      #### main upto 8h (CPUs=7840)
-      #### debug: upto 20m (CPUs=200)
-      #### memory max 4096M, default 2048M
+    else
+	#### gsi batch environement for kronos (slurm) ####
+	#### NOTE: possible partitions: sinfo -o "%9P  %6g %10l %5w %5D %13C %N"
+	####                      and : scontrol show partition main
+	#### long: upto 7d (CPUs=3880)
+	#### main upto 8h (CPUs=7840)
+	#### debug: upto 20m (CPUs=200)
+	#### memory max 4096M, default 2048M
 
-      ## configure batch job
-      tmpjobscriptname=$PWD/ana_${train}_$I.sh
-      ####
-      echo "#! /bin/sh"                                   > $tmpjobscriptname
-      echo "#SBATCH --output=$outdir/ana.slurm.out"       >> $tmpjobscriptname
-      echo "#SBATCH --error=$outdir/ana.slurm.err"        >> $tmpjobscriptname
-      echo "#SBATCH --job-name=ana_${train}_${I}"         >> $tmpjobscriptname
-      echo "#SBATCH --mail-type=FAIL"                     >> $tmpjobscriptname
-      echo "#SBATCH --mail-user=j.book@gsi.de"            >> $tmpjobscriptname
-      echo "#SBATCH --mem-per-cpu=2000"                   >> $tmpjobscriptname
-      echo "#SBATCH --time=0-03:00:00"                    >> $tmpjobscriptname
-      echo "#SBATCH --partition=main"                     >> $tmpjobscriptname
-      ####
-      echo "srun ./analysis.sh $indir $filelist $outdir $NEVENT"  >> $tmpjobscriptname
+	## configure batch job
+	tmpjobscriptname=$PWD/ana_${train}_$I.sh
+	####
+	echo "#! /bin/sh"                                   > $tmpjobscriptname
+	echo "#SBATCH --output=$outdir/ana.slurm.out"       >> $tmpjobscriptname
+	echo "#SBATCH --error=$outdir/ana.slurm.err"        >> $tmpjobscriptname
+	echo "#SBATCH --job-name=ana_${train}_${I}"         >> $tmpjobscriptname
+	echo "#SBATCH --mail-type=FAIL"                     >> $tmpjobscriptname
+	echo "#SBATCH --mail-user=j.book@gsi.de"            >> $tmpjobscriptname
+	echo "#SBATCH --mem-per-cpu=2000"                   >> $tmpjobscriptname
+	echo "#SBATCH --time=0-03:00:00"                    >> $tmpjobscriptname
+	echo "#SBATCH --partition=main"                     >> $tmpjobscriptname
+	####
+	echo "srun ./analysis.sh $indir $filelist $outdir $NEVENT"  >> $tmpjobscriptname
 
-      chmod u+x $tmpjobscriptname
+	chmod u+x $tmpjobscriptname
 
-      ## submit job
-      sbatch $tmpjobscriptname
+	## submit job
+	sbatch $tmpjobscriptname
 
-  fi
+    fi
 
 
 done
