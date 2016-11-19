1f816f77b88c675fb26e90c93c4f8b642732fc54
p.-a.loizeau <p.-a.loizeau@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2016-11-19 14:31:23 +0000

    Update DEV version of FairRoot from 16.06a to 16.06b in autoinstall script

--- autoinstall_framework.sh
+++ autoinstall_framework.sh
@@ -27,10 +27,10 @@ export ROOTVER=5
 
 # put your desired variants here:
 export FSOFTDEV=may16p1
-export FROOTDEV=v-16.06a
+export FROOTDEV=v-16.06b
 
 export FSOFTPRO=nov15p7
-export FROOTPRO=v-15.11f 
+export FROOTPRO=v-15.11f
 
 # former versions
 # fairsoft: mar15p2
