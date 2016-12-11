6cd319bad30e056782de51938ae9a6f788393ba6
f.uhlig <f.uhlig@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2016-12-11 09:43:16 +0000

    Refactor unit tests. Add CbmTestHit class derrived from CbmHit to be able to test the copy constructor and assignment operator of CbmHit. Add unit tests ti test copy constructor and assignment operator.

--- test/cbmdata/_GTestCbmHit.cxx
+++ test/cbmdata/_GTestCbmHit.cxx
@@ -5,240 +5,176 @@
 #include "gtest/gtest.h"
 #include "gtest/gtest-spi.h"
 
-TEST(_GTestCbmHit , CheckDefaultConstructor)
-{
-  CbmHit test;
+// Since some functions in CbmHit are protected we have
+// to create a derived class without any data members
+// which simply forwards the function
+// calls to the base class
 
+class CbmTestHit : public CbmHit
+{
+  public:
+    CbmTestHit() : CbmHit() {;}
+
+    /** Copy constructor  **/
+    CbmTestHit(const CbmTestHit& hit) : CbmHit(hit) {;}
+
+    /** Destructor  **/
+    virtual ~CbmTestHit() {;}
+
+    /** Assignment operator  **/
+    CbmTestHit& operator=(const CbmTestHit& other)
+    {
+      if (this != &other) {
+        CbmHit::operator=(other);
+      }
+      return *this;
+    }
+};
+
+void compareHitDataMembers(CbmHit& test, HitType type, Double_t z, Double_t dz,
+    Int_t refid, Int_t address, CbmMatch* match, Double_t time, Double_t errortime)
+{
   Int_t retValInt{-111};
   Float_t retValFloat{-111.};
 
-  EXPECT_EQ(kHIT, test.GetType());
+  EXPECT_EQ(type, test.GetType());
+
   retValFloat = test.GetZ();
-  EXPECT_FLOAT_EQ(0., retValFloat);
-  retValFloat = test.GetZ();
-  EXPECT_FLOAT_EQ(0., retValFloat);
+  EXPECT_FLOAT_EQ(z, retValFloat);
+
+  retValFloat = test.GetDz();
+  EXPECT_FLOAT_EQ(dz, retValFloat);
+
   retValInt = test.GetRefId();
-  EXPECT_EQ(-1, retValInt);
+  EXPECT_EQ(refid, retValInt);
+
   retValInt = test.GetAddress();
-  EXPECT_EQ(-1, retValInt);
-  EXPECT_EQ(nullptr, test.GetMatch());
+  EXPECT_EQ(address, retValInt);
+
+  EXPECT_EQ(match, test.GetMatch());
+
   retValFloat =  test.GetTime();
-  EXPECT_FLOAT_EQ(-1., retValFloat );
+  EXPECT_FLOAT_EQ(time, retValFloat );
+
   retValFloat = test.GetTimeError();
-  EXPECT_FLOAT_EQ(-1., retValFloat);
+  EXPECT_FLOAT_EQ(errortime, retValFloat);
 }
 
-TEST(_GTestCbmHit , CheckStandardConstructor)
+void compareMatchDataMembers(CbmMatch* testMatch, Int_t noflinks, Double_t weight)
 {
-  CbmHit test{kHIT, 0., 0., -1, -1};
-
-  Int_t retValInt{-111};
-  Float_t retValFloat{-111.};
-
-  EXPECT_EQ(kHIT, test.GetType());
-  retValFloat = test.GetZ();
-  EXPECT_FLOAT_EQ(0., retValFloat);
-  retValFloat = test.GetZ();
-  EXPECT_FLOAT_EQ(0., retValFloat);
-  retValInt = test.GetRefId();
-  EXPECT_EQ(-1, retValInt);
-  retValInt = test.GetAddress();
-  EXPECT_EQ(-1, retValInt);
-  EXPECT_EQ(nullptr, test.GetMatch());
-  retValFloat =  test.GetTime();
-  EXPECT_FLOAT_EQ(-1., retValFloat );
-  retValFloat = test.GetTimeError();
-  EXPECT_FLOAT_EQ(-1., retValFloat);
-}
-
-TEST(_GTestCbmHit , CheckStandardConstructorWithTime)
-{
-  CbmHit test{kHIT, 0., 0., -1, -1, -2.,-2.};
-
-  Int_t retValInt{-111};
-  Float_t retValFloat{-111.};
-
-  EXPECT_EQ(kHIT, test.GetType());
-  retValFloat = test.GetZ();
-  EXPECT_FLOAT_EQ(0., retValFloat);
-  retValFloat = test.GetZ();
-  EXPECT_FLOAT_EQ(0., retValFloat);
-  retValInt = test.GetRefId();
-  EXPECT_EQ(-1, retValInt);
-  retValInt = test.GetAddress();
-  EXPECT_EQ(-1, retValInt);
-  EXPECT_EQ(nullptr, test.GetMatch());
-  retValFloat =  test.GetTime();
-  EXPECT_FLOAT_EQ(-2., retValFloat );
-  retValFloat = test.GetTimeError();
-  EXPECT_FLOAT_EQ(-2., retValFloat);
-}
-
-TEST(_GTestCbmHit , TestSettersAndGetters)
-{
-  CbmHit test;
-
-  Int_t retValInt{-111};
-  Float_t retValFloat{-111.};
-
-  EXPECT_EQ(kHIT, test.GetType());
-  retValFloat = test.GetZ();
-  EXPECT_FLOAT_EQ(0., retValFloat);
-  retValFloat = test.GetDz();
-  EXPECT_FLOAT_EQ(0., retValFloat);
-  retValInt = test.GetRefId();
-  EXPECT_EQ(-1, retValInt);
-  retValInt = test.GetAddress();
-  EXPECT_EQ(-1, retValInt);
-  EXPECT_EQ(nullptr, test.GetMatch());
-  retValFloat =  test.GetTime();
-  EXPECT_FLOAT_EQ(-1., retValFloat );
-  retValFloat = test.GetTimeError();
-  EXPECT_FLOAT_EQ(-1., retValFloat);
-
-  test.SetZ(-2.);
-
-  EXPECT_EQ(kHIT, test.GetType());
-  retValFloat = test.GetZ();
-  EXPECT_FLOAT_EQ(-2., retValFloat);
-  retValFloat = test.GetDz();
-  EXPECT_FLOAT_EQ(0., retValFloat);
-  retValInt = test.GetRefId();
-  EXPECT_EQ(-1, retValInt);
-  retValInt = test.GetAddress();
-  EXPECT_EQ(-1, retValInt);
-  EXPECT_EQ(nullptr, test.GetMatch());
-  retValFloat =  test.GetTime();
-  EXPECT_FLOAT_EQ(-1., retValFloat );
-  retValFloat = test.GetTimeError();
-  EXPECT_FLOAT_EQ(-1., retValFloat);
-
-  test.SetDz(-2.);
-
-  EXPECT_EQ(kHIT, test.GetType());
-  retValFloat = test.GetZ();
-  EXPECT_FLOAT_EQ(-2., retValFloat);
-  retValFloat = test.GetDz();
-  EXPECT_FLOAT_EQ(-2., retValFloat);
-  retValInt = test.GetRefId();
-  EXPECT_EQ(-1, retValInt);
-  retValInt = test.GetAddress();
-  EXPECT_EQ(-1, retValInt);
-  EXPECT_EQ(nullptr, test.GetMatch());
-  retValFloat =  test.GetTime();
-  EXPECT_FLOAT_EQ(-1., retValFloat );
-  retValFloat = test.GetTimeError();
-  EXPECT_FLOAT_EQ(-1., retValFloat);
-
-  test.SetRefId(-2);
-
-  EXPECT_EQ(kHIT, test.GetType());
-  retValFloat = test.GetZ();
-  EXPECT_FLOAT_EQ(-2., retValFloat);
-  retValFloat = test.GetDz();
-  EXPECT_FLOAT_EQ(-2., retValFloat);
-  retValInt = test.GetRefId();
-  EXPECT_EQ(-2, retValInt);
-  retValInt = test.GetAddress();
-  EXPECT_EQ(-1, retValInt);
-  EXPECT_EQ(nullptr, test.GetMatch());
-  retValFloat =  test.GetTime();
-  EXPECT_FLOAT_EQ(-1., retValFloat );
-  retValFloat = test.GetTimeError();
-  EXPECT_FLOAT_EQ(-1., retValFloat);
-
-  test.SetAddress(-2);
-
-  EXPECT_EQ(kHIT, test.GetType());
-  retValFloat = test.GetZ();
-  EXPECT_FLOAT_EQ(-2., retValFloat);
-  retValFloat = test.GetDz();
-  EXPECT_FLOAT_EQ(-2., retValFloat);
-  retValInt = test.GetRefId();
-  EXPECT_EQ(-2, retValInt);
-  retValInt = test.GetAddress();
-  EXPECT_EQ(-2, retValInt);
-  EXPECT_EQ(nullptr, test.GetMatch());
-  retValFloat =  test.GetTime();
-  EXPECT_FLOAT_EQ(-1., retValFloat );
-  retValFloat = test.GetTimeError();
-  EXPECT_FLOAT_EQ(-1., retValFloat);
-
-  test.SetTime(-2.);
-
-  EXPECT_EQ(kHIT, test.GetType());
-  retValFloat = test.GetZ();
-  EXPECT_FLOAT_EQ(-2., retValFloat);
-  retValFloat = test.GetDz();
-  EXPECT_FLOAT_EQ(-2., retValFloat);
-  retValInt = test.GetRefId();
-  EXPECT_EQ(-2, retValInt);
-  retValInt = test.GetAddress();
-  EXPECT_EQ(-2, retValInt);
-  EXPECT_EQ(nullptr, test.GetMatch());
-  retValFloat =  test.GetTime();
-  EXPECT_FLOAT_EQ(-2., retValFloat );
-  retValFloat = test.GetTimeError();
-  EXPECT_FLOAT_EQ(-1., retValFloat);
-
-  test.SetTimeError(-2.);
-
-  EXPECT_EQ(kHIT, test.GetType());
-  retValFloat = test.GetZ();
-  EXPECT_FLOAT_EQ(-2., retValFloat);
-  retValFloat = test.GetDz();
-  EXPECT_FLOAT_EQ(-2., retValFloat);
-  retValInt = test.GetRefId();
-  EXPECT_EQ(-2, retValInt);
-  retValInt = test.GetAddress();
-  EXPECT_EQ(-2, retValInt);
-  EXPECT_EQ(nullptr, test.GetMatch());
-  retValFloat =  test.GetTime();
-  EXPECT_FLOAT_EQ(-2., retValFloat );
-  retValFloat = test.GetTimeError();
-  EXPECT_FLOAT_EQ(-2., retValFloat);
-
-  test.SetTime(-3., -3.);
-
-  EXPECT_EQ(kHIT, test.GetType());
-  retValFloat = test.GetZ();
-  EXPECT_FLOAT_EQ(-2., retValFloat);
-  retValFloat = test.GetDz();
-  EXPECT_FLOAT_EQ(-2., retValFloat);
-  retValInt = test.GetRefId();
-  EXPECT_EQ(-2, retValInt);
-  retValInt = test.GetAddress();
-  EXPECT_EQ(-2, retValInt);
-  EXPECT_EQ(nullptr, test.GetMatch());
-  retValFloat =  test.GetTime();
-  EXPECT_FLOAT_EQ(-3., retValFloat );
-  retValFloat = test.GetTimeError();
-  EXPECT_FLOAT_EQ(-3., retValFloat);
-
-  retValInt = test.GetPlaneId();
-  EXPECT_EQ(-1, retValInt);
-
-  // Test if we can add an empty CbmMatch, get it back, and extract
-  // the expected values
-  CbmMatch* testMatch = new CbmMatch();
-
   Int_t linkLength{-111};
-  Double_t totalWeight{-111};
+  Double_t totalWeight{-111.};
 
   linkLength = testMatch->GetNofLinks();
   EXPECT_EQ(0, linkLength);
 
   totalWeight = testMatch->GetTotalWeight();
   EXPECT_FLOAT_EQ(0., totalWeight);
+}
+
+
+TEST(_GTestCbmHit, CheckDefaultConstructor)
+{
+  CbmHit test;
+  compareHitDataMembers(test, kHIT, 0., 0., -1, -1, nullptr, -1., -1.);
+}
+
+TEST(_GTestCbmHit, CheckStandardConstructor)
+{
+  CbmHit test{kHIT, 0., 0., -1, -1};
+  compareHitDataMembers(test, kHIT, 0., 0., -1, -1, nullptr, -1., -1.);
+}
+
+TEST(_GTestCbmHit, CheckStandardConstructorWithTime)
+{
+  CbmHit test{kHIT, 0., 0., -1, -1, -2.,-2.};
+  compareHitDataMembers(test, kHIT, 0., 0., -1, -1, nullptr, -2., -2.);
+}
+
+// Can't test the copy constructor yet since it is protected
+
+
+TEST(_GTestCbmHit, CheckAssignmentOperator)
+{
+  CbmTestHit test;
+  {
+    SCOPED_TRACE("CheckAssignmentOperator: Initial Test");
+    compareHitDataMembers(test, kHIT, 0., 0., -1, -1, nullptr, -1., -1.);
+  }
+
+  CbmMatch* testMatch = new CbmMatch();
+
+  compareMatchDataMembers(testMatch, 0, 0.);
+
+  test.SetMatch(testMatch);
+  {
+    SCOPED_TRACE("CheckAssignmentOperator: Test with CbmMatch");
+    compareHitDataMembers(test, kHIT, 0., 0., -1, -1, testMatch, -1., -1.);
+  }
+
+  CbmTestHit test2;
+  test2=test;
+
+  // Test if the new object has the same values for all data members
+  // as the original object
+  CbmMatch* testMatch1 = test2.GetMatch();
+
+  EXPECT_NE(nullptr, testMatch1);
+  if (testMatch1) {
+    compareMatchDataMembers(testMatch1, 0, 0.);
+  }
+
+  {
+    SCOPED_TRACE("CheckAssignmentOperator: Test new object after assignment");
+    compareHitDataMembers(test2, kHIT, 0., 0., -1, -1, testMatch, -1., -1.);
+  }
+  // Test if the original object wasn't changed
+  {
+    SCOPED_TRACE("CheckAssignmentOperator: Test original object after assignment");
+    compareHitDataMembers(test, kHIT, 0., 0., -1, -1, testMatch, -1., -1.);
+  }
+}
+
+
+TEST(_GTestCbmHit, TestSettersAndGetters)
+{
+  CbmHit test;
+  compareHitDataMembers(test, kHIT, 0., 0., -1, -1, nullptr, -1., -1.);
+
+  test.SetZ(-2.);
+  compareHitDataMembers(test, kHIT, -2., 0., -1, -1, nullptr, -1., -1.);
+
+  test.SetDz(-2.);
+  compareHitDataMembers(test, kHIT, -2., -2., -1, -1, nullptr, -1., -1.);
+
+  test.SetRefId(-2);
+  compareHitDataMembers(test, kHIT, -2., -2., -2, -1, nullptr, -1., -1.);
+
+  test.SetAddress(-2);
+  compareHitDataMembers(test, kHIT, -2., -2., -2, -2, nullptr, -1., -1.);
+
+  test.SetTime(-2.);
+  compareHitDataMembers(test, kHIT, -2., -2., -2, -2, nullptr, -2., -1.);
+
+  test.SetTimeError(-2.);
+  compareHitDataMembers(test, kHIT, -2., -2., -2, -2, nullptr, -2., -2.);
+
+  test.SetTime(-3., -3.);
+  compareHitDataMembers(test, kHIT, -2., -2., -2, -2, nullptr, -3., -3.);
+
+  Int_t retValInt = test.GetPlaneId();
+  EXPECT_EQ(-1, retValInt);
+
+  // Test if we can add an empty CbmMatch, get it back, and extract
+  // the expected values
+  CbmMatch* testMatch = new CbmMatch();
+
+  compareMatchDataMembers(testMatch, 0, 0.);
 
   test.SetMatch(testMatch);
   CbmMatch* testMatch1 = test.GetMatch();
 
-  linkLength = testMatch1->GetNofLinks();
-  EXPECT_EQ(0, linkLength);
-
-  totalWeight = testMatch1->GetTotalWeight();
-  EXPECT_FLOAT_EQ(0., totalWeight);
+  compareMatchDataMembers(testMatch1, 0, 0.);
 }
 
 TEST(_GTestCbmHit , ToString)
