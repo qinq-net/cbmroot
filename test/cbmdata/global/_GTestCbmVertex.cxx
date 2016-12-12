4d45b5fa20e31a6487a9cccbafb03ad7246c01f6
f.uhlig <f.uhlig@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2016-12-12 16:41:41 +0000

    Add more unit tests. Now class is completely covered.

--- test/cbmdata/global/_GTestCbmVertex.cxx
+++ test/cbmdata/global/_GTestCbmVertex.cxx
@@ -1,6 +1,7 @@
 #include "CbmVertex.h"
 
 #include "TMatrixFSym.h"
+#include "TVector3.h"
 
 #include "gtest/gtest.h"
 #include "gtest/gtest-spi.h"
@@ -92,22 +93,200 @@
 
  TEST(_GTestCbmVertex, CheckConstructorAllArguments)
  {
-   TMatrixFSym tempCov(3);
-   tempCov(0, 0) = 0.;
-   tempCov(0, 1) = 1.;
-   tempCov(0, 2) = 2.;
-   tempCov(1, 0) = 3.;
-   tempCov(1, 1) = 4.;
-   tempCov(1, 2) = 5.;
-   tempCov(2, 0) = 6.;
-   tempCov(2, 1) = 7.;
-   tempCov(2, 2) = 8.;
+   TMatrixFSym Cov(3);
+   Cov(0, 0) = 0.;      // 0 1 2
+   Cov(0, 1) = 1.;      // 1 3 4
+   Cov(0, 2) = 2.;      // 2 4 5
+   Cov(1, 0) = 1.;
+   Cov(1, 1) = 3.;
+   Cov(1, 2) = 4.;
+   Cov(2, 0) = 2.;
+   Cov(2, 1) = 4.;
+   Cov(2, 2) = 5.;
 
-   Double_t val[6] = {0., 1., 2., 4., 5., 8.};
-   CbmVertex test{"Vertex","Vertex", 1., 2., 3., 4., 5, 6, tempCov};
+   Double_t val[6] = {0., 1., 2., 3., 4., 5.};
+   CbmVertex test{"Vertex","Vertex", 1., 2., 3., 4., 5, 6, Cov};
    {
      SCOPED_TRACE("CheckConstructorAllArguments");
      compareVertexDataMembers(test, 1., 2., 3., 4., 5, 6, val);
    }
  }
 
+ TEST(_GTestCbmVertex, CheckReset)
+ {
+   TMatrixFSym Cov(3);
+   Cov(0, 0) = 0.;
+   Cov(0, 1) = 1.;
+   Cov(0, 2) = 2.;
+   Cov(1, 0) = 1.;
+   Cov(1, 1) = 3.;
+   Cov(1, 2) = 4.;
+   Cov(2, 0) = 2.;
+   Cov(2, 1) = 4.;
+   Cov(2, 2) = 5.;
+
+   Double_t val[6] = {0., 1., 2., 3., 4., 5.};
+   CbmVertex test{"Vertex","Vertex", 1., 2., 3., 4., 5, 6, Cov};
+   {
+     SCOPED_TRACE("CheckReset: Initial Test");
+     compareVertexDataMembers(test, 1., 2., 3., 4., 5, 6, val);
+   }
+
+   Double_t val1[6] = {0., 0., 0., 0., 0., 0.};
+   test.Reset();
+   {
+     SCOPED_TRACE("CheckReset: Check after reset");
+     compareVertexDataMembers(test, 0., 0., 0., 0., 0, 0, val1);
+   }
+ }
+
+ TEST(_GTestCbmVertex, CheckGetPosition)
+ {
+   TMatrixFSym Cov(3);
+   Cov(0, 0) = 0.;
+   Cov(0, 1) = 1.;
+   Cov(0, 2) = 2.;
+   Cov(1, 0) = 1.;
+   Cov(1, 1) = 3.;
+   Cov(1, 2) = 4.;
+   Cov(2, 0) = 2.;
+   Cov(2, 1) = 4.;
+   Cov(2, 2) = 5.;
+
+   Double_t val[6] = {0., 1., 2., 3., 4., 5.};
+   CbmVertex test{"Vertex","Vertex", 1., 2., 3., 4., 5, 6, Cov};
+   {
+     SCOPED_TRACE("CheckGetPosition: Initial Test");
+     compareVertexDataMembers(test, 1., 2., 3., 4., 5, 6, val);
+   }
+
+   TVector3 testVect;
+
+   test.Position(testVect);
+   {
+     SCOPED_TRACE("CheckGetPosition: Check after Position");
+     compareVertexDataMembers(test, 1., 2., 3., 4., 5, 6, val);
+   }
+   EXPECT_FLOAT_EQ(1., testVect.X());
+   EXPECT_FLOAT_EQ(2., testVect.Y());
+   EXPECT_FLOAT_EQ(3., testVect.Z());
+ }
+
+
+ TEST(_GTestCbmVertex, CheckGetCovMatrix)
+  {
+    TMatrixFSym Cov(3);
+    Cov(0, 0) = 0.;
+    Cov(0, 1) = 1.;
+    Cov(0, 2) = 2.;
+    Cov(1, 0) = 1.;
+    Cov(1, 1) = 3.;
+    Cov(1, 2) = 4.;
+    Cov(2, 0) = 2.;
+    Cov(2, 1) = 4.;
+    Cov(2, 2) = 5.;
+
+    Double_t val[6] = {0., 1., 2., 3., 4., 5.};
+    CbmVertex test{"Vertex","Vertex", 1., 2., 3., 4., 5, 6, Cov};
+    {
+      SCOPED_TRACE("CheckGetCovMatrix: Initial Test");
+      compareVertexDataMembers(test, 1., 2., 3., 4., 5, 6, val);
+    }
+
+    TMatrixFSym testCov(3);
+
+    test.CovMatrix(testCov);
+    {
+      SCOPED_TRACE("CheckGetCovMatrix: Check after Position");
+      compareVertexDataMembers(test, 1., 2., 3., 4., 5, 6, val);
+    }
+    for(Int_t i=0; i<3; ++i) {
+      for(Int_t j=0; j<3; ++j) {
+        Double_t origVal = Cov[i][j];
+        Double_t testVal = testCov[i][j];
+        EXPECT_FLOAT_EQ(testVal, origVal);
+      }
+    }
+  }
+
+ TEST(_GTestCbmVertex, CheckSetVertex)
+  {
+    TMatrixFSym Cov(3);
+    Cov(0, 0) = 0.;
+    Cov(0, 1) = 1.;
+    Cov(0, 2) = 2.;
+    Cov(1, 0) = 1.;
+    Cov(1, 1) = 3.;
+    Cov(1, 2) = 4.;
+    Cov(2, 0) = 2.;
+    Cov(2, 1) = 4.;
+    Cov(2, 2) = 5.;
+
+    Double_t val[6] = {0., 1., 2., 3., 4., 5.};
+    CbmVertex test{"Vertex","Vertex", 1., 2., 3., 4., 5, 6, Cov};
+    {
+      SCOPED_TRACE("CheckSetVertex: Initial Test");
+      compareVertexDataMembers(test, 1., 2., 3., 4., 5, 6, val);
+    }
+
+    TMatrixFSym testCov(3);
+    testCov(0, 0) = -10.;
+    testCov(0, 1) = -1.;
+    testCov(0, 2) = -2.;
+    testCov(1, 0) = -1.;
+    testCov(1, 1) = -3.;
+    testCov(1, 2) = -4.;
+    testCov(2, 0) = -2.;
+    testCov(2, 1) = -4.;
+    testCov(2, 2) = -5.;
+
+    Double_t testval[6] = {-10., -1., -2., -3., -4., -5.};
+
+    test.SetVertex(-1., -2., -3., -4., -5, -6, testCov);
+    {
+      SCOPED_TRACE("CheckSetVertex: Check after SetVertex");
+      compareVertexDataMembers(test, -1., -2., -3., -4., -5, -6, testval);
+    }
+  }
+
+ TEST(_GTestCbmVertex, CheckPrint)
+   {
+     TMatrixFSym Cov(3);
+     Cov(0, 0) = 0.;
+     Cov(0, 1) = 1.;
+     Cov(0, 2) = 2.;
+     Cov(1, 0) = 1.;
+     Cov(1, 1) = 3.;
+     Cov(1, 2) = 4.;
+     Cov(2, 0) = 2.;
+     Cov(2, 1) = 4.;
+     Cov(2, 2) = 5.;
+
+     Double_t val[6] = {0., 1., 2., 3., 4., 5.};
+     CbmVertex test{"Vertex","Vertex", 1., 2., 3., 4., 5, 6, Cov};
+     {
+       SCOPED_TRACE("CheckSetVertex: Initial Test");
+       compareVertexDataMembers(test, 1., 2., 3., 4., 5, 6, val);
+     }
+
+     testing::internal::CaptureStdout();
+     test.Print();
+     std::string output = testing::internal::GetCapturedStdout();
+     EXPECT_STREQ("[INFO   ] Vertex coord. (1,2,3) cm, chi2/ndf = 0.8, 6 tracks used\n", output.c_str());
+
+     EXPECT_STREQ("Vertex: position (1.0000, 2.0000, 3.0000) cm, chi2/ndf = 0.8000, tracks used: 6", test.ToString().c_str());
+
+
+     CbmVertex test1{"Vertex","Vertex", 1., 2., 3., 4., 0, 6, Cov};
+     {
+        SCOPED_TRACE("CheckSetVertex: Initial Test");
+        compareVertexDataMembers(test1, 1., 2., 3., 4., 0, 6, val);
+      }
+
+      testing::internal::CaptureStdout();
+      test1.Print();
+      output = testing::internal::GetCapturedStdout();
+      EXPECT_STREQ("[INFO   ] Vertex coord. (1,2,3) cm, chi2/ndf = 0, 6 tracks used\n", output.c_str());
+
+      EXPECT_STREQ("Vertex: position (1.0000, 2.0000, 3.0000) cm, chi2/ndf = 0.0000, tracks used: 6", test1.ToString().c_str());
+}
