c5c2fce073518091dbc001412622681e3ba96347
f.uhlig <f.uhlig@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2016-12-12 09:39:59 +0000

    Add new unit tests for CbmDetectorList.

--- test/cbmdata/_GTestCbmDetectorList.cxx
+++ test/cbmdata/_GTestCbmDetectorList.cxx
@@ -15,6 +15,7 @@ struct InOutStructure {
   DetectorId detIdFromEnum;
   int detId;
   TString detName;
+  TString detNameUppercase;
 } ;
 
 // Base class to use the same basic setup for parameterized and
@@ -48,6 +49,7 @@ class CbmDetectorListParamTest : public _TestCbmDetectorListBase<
   DetectorId detIdFromEnum;
   Int_t detId;
   TString detName;
+  TString detNameUppercase;
 
   virtual void SetUp() {
     InOutStructure const& p = GetParam();
@@ -55,36 +57,61 @@ class CbmDetectorListParamTest : public _TestCbmDetectorListBase<
     detIdFromEnum = p.detIdFromEnum;
     detId = p.detId;
     detName = p.detName;
+    detNameUppercase = p.detNameUppercase;
   }
 };
 
+TEST(CbmDetectorList, outOfBounds)
+{
+  CbmDetectorList fList;
+  TString retName;
+
+  fList.GetSystemName(111, retName);
+  EXPECT_EQ("unknown", retName);
+
+  fList.GetSystemName(-111, retName);
+  EXPECT_EQ("unknown", retName);
+
+  fList.GetSystemNameCaps(111, retName);
+  EXPECT_EQ("unknown", retName);
+
+  fList.GetSystemNameCaps(-111, retName);
+  EXPECT_EQ("unknown", retName);
+}
 
 TEST_P(CbmDetectorListParamTest, checkUniqueIdCreation)
 {
   TString retName; 
   fList.GetSystemName(detIdFromEnum, retName);
   EXPECT_EQ(detName, retName);
-//  fList.GetSystemName(detId, retName);
-//  EXPECT_EQ(detName, retName);
+
+  fList.GetSystemName(detId, retName);
+  EXPECT_EQ(detName, retName);
+
+  fList.GetSystemNameCaps(detIdFromEnum, retName);
+  EXPECT_EQ(detNameUppercase, retName);
+
+  fList.GetSystemNameCaps(detId, retName);
+  EXPECT_EQ(detNameUppercase, retName);
 }
 
-InOutStructure val1 = {kREF, 0, "ref"};
-InOutStructure val2 = {kMVD, 1, "mvd"};
-InOutStructure val3 = {kSTS, 2, "sts"};
-InOutStructure val4 = {kRICH, 3, "rich"};
-InOutStructure val5 = {kMUCH, 4, "much"};
-InOutStructure val6 = {kTRD, 5, "trd"};
-InOutStructure val7 = {kTOF, 6, "tof"};
-InOutStructure val8 = {kECAL, 7, "ecal"};
-InOutStructure val9 = {kZDC, 8, "unknown"};
-InOutStructure val10 = {kSTT, 9, "unknown"};
-InOutStructure val11 = {kTutDet, 10, "unknown"};
-//InOutStructure val12 = {kBLA, 11, "unknown"};
-
+InOutStructure val1 = {kREF, 0, "ref", "REF"};
+InOutStructure val2 = {kMVD, 1, "mvd", "MVD"};
+InOutStructure val3 = {kSTS, 2, "sts", "STS"};
+InOutStructure val4 = {kRICH, 3, "rich", "RICH"};
+InOutStructure val5 = {kMUCH, 4, "much", "MUCH"};
+InOutStructure val6 = {kTRD, 5, "trd", "TRD"};
+InOutStructure val7 = {kTOF, 6, "tof", "TOF"};
+InOutStructure val8 = {kECAL, 7, "ecal", "ECAL"};
+InOutStructure val9 = {kPSD, 8, "unknown", "PSD"};
+InOutStructure val10 = {kSTT, 9, "unknown", "UNKNOWN"};
+InOutStructure val11 = {kFHODO, 10, "fhodo", "FHODO"};
+InOutStructure val12 = {kTutDet, 11, "tutdet", "TUTDET"};
+InOutStructure val13 = {kNOFDETS, 12, "unknown", "UNKNOWN"};
 
 INSTANTIATE_TEST_CASE_P(TestAllParameters,
                         CbmDetectorListParamTest,
 			::testing::Values(val1, val2, val3, val4, val5,
                                           val6, val7, val8, val9, val10,
-                                          val11));
+                                          val11,val12));
 
