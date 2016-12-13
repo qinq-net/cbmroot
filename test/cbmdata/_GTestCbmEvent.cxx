60e6b63e4a7c50cff18fe6aa5d47b12867b3f493
f.uhlig <f.uhlig@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2016-12-13 14:47:28 +0000

    Add more unit tests. Now full coverage of the class is reached.

--- test/cbmdata/_GTestCbmEvent.cxx
+++ test/cbmdata/_GTestCbmEvent.cxx
@@ -139,148 +139,123 @@ TEST(_GTestCbmCluster, CheckSettersAndGetters)
      compareEventMap(test, 2, -1, Cbm::kStsTrack,   ststrack);
    }
 
+   UInt_t retValUInt=test.GetIndex(Cbm::kMCTrack, 25);
+   EXPECT_EQ(-2, retValUInt);
+
+   stspoint.push_back(1);
+   test.AddData(Cbm::kStsPoint, 1);
+   {
+     SCOPED_TRACE("CheckAddData: Add StsPoint");
+     compareEventDataMembers(test, -111, 1., 2., 3, nullptr);
+     compareEventMap(test, 3,  2, Cbm::kMCTrack,    mctrack);
+     compareEventMap(test, 3,  1, Cbm::kStsPoint,   stspoint);
+     compareEventMap(test, 3, -1, Cbm::kStsDigi,    stsdigi);
+     compareEventMap(test, 3, -1, Cbm::kStsCluster, stscluster);
+     compareEventMap(test, 3, -1, Cbm::kStsHit,     stshit);
+     compareEventMap(test, 3, -1, Cbm::kStsTrack,   ststrack);
+   }
+
+   stsdigi.push_back(2);
+   test.AddData(Cbm::kStsDigi, 2);
+   {
+     SCOPED_TRACE("CheckAddData: Add StsDigi");
+     compareEventDataMembers(test, -111, 1., 2., 4, nullptr);
+     compareEventMap(test, 4,  2, Cbm::kMCTrack,    mctrack);
+     compareEventMap(test, 4,  1, Cbm::kStsPoint,   stspoint);
+     compareEventMap(test, 4,  1, Cbm::kStsDigi,    stsdigi);
+     compareEventMap(test, 4, -1, Cbm::kStsCluster, stscluster);
+     compareEventMap(test, 4, -1, Cbm::kStsHit,     stshit);
+     compareEventMap(test, 4, -1, Cbm::kStsTrack,   ststrack);
+   }
+
+   stscluster.push_back(3);
+   test.AddData(Cbm::kStsCluster, 3);
+   {
+     SCOPED_TRACE("CheckAddData: Add StsCluster");
+     compareEventDataMembers(test, -111, 1., 2., 5, nullptr);
+     compareEventMap(test, 5,  2, Cbm::kMCTrack,    mctrack);
+     compareEventMap(test, 5,  1, Cbm::kStsPoint,   stspoint);
+     compareEventMap(test, 5,  1, Cbm::kStsDigi,    stsdigi);
+     compareEventMap(test, 5,  1, Cbm::kStsCluster, stscluster);
+     compareEventMap(test, 5, -1, Cbm::kStsHit,     stshit);
+     compareEventMap(test, 5, -1, Cbm::kStsTrack,   ststrack);
+   }
+
+   stshit.push_back(4);
+   test.AddData(Cbm::kStsHit, 4);
+   {
+     SCOPED_TRACE("CheckAddData: Add StsHit");
+     compareEventDataMembers(test, -111, 1., 2., 6, nullptr);
+     compareEventMap(test, 6,  2, Cbm::kMCTrack,    mctrack);
+     compareEventMap(test, 6,  1, Cbm::kStsPoint,   stspoint);
+     compareEventMap(test, 6,  1, Cbm::kStsDigi,    stsdigi);
+     compareEventMap(test, 6,  1, Cbm::kStsCluster, stscluster);
+     compareEventMap(test, 6,  1, Cbm::kStsHit,     stshit);
+     compareEventMap(test, 6, -1, Cbm::kStsTrack,   ststrack);
+   }
+
+   ststrack.push_back(5);
+   test.AddData(Cbm::kStsTrack, 5);
+   {
+     SCOPED_TRACE("CheckAddData: Add StsTrack");
+     compareEventDataMembers(test, -111, 1., 2., 7, nullptr);
+     compareEventMap(test, 7,  2, Cbm::kMCTrack,    mctrack);
+     compareEventMap(test, 7,  1, Cbm::kStsPoint,   stspoint);
+     compareEventMap(test, 7,  1, Cbm::kStsDigi,    stsdigi);
+     compareEventMap(test, 7,  1, Cbm::kStsCluster, stscluster);
+     compareEventMap(test, 7,  1, Cbm::kStsHit,     stshit);
+     compareEventMap(test, 7,  1, Cbm::kStsTrack,   ststrack);
+   }
+
+   ststrack.push_back(6);
+   test.AddStsTrack(6);
+   {
+     SCOPED_TRACE("CheckAddData: Add StsTrack via AddStsTrack");
+     compareEventDataMembers(test, -111, 1., 2., 8, nullptr);
+     compareEventMap(test, 8,  2, Cbm::kMCTrack,    mctrack);
+     compareEventMap(test, 8,  1, Cbm::kStsPoint,   stspoint);
+     compareEventMap(test, 8,  1, Cbm::kStsDigi,    stsdigi);
+     compareEventMap(test, 8,  1, Cbm::kStsCluster, stscluster);
+     compareEventMap(test, 8,  1, Cbm::kStsHit,     stshit);
+     compareEventMap(test, 8,  2, Cbm::kStsTrack,   ststrack);
+   }
+
+   retValUInt=test.GetStsTrackIndex(0);
+   EXPECT_EQ(5, retValUInt);
+
+   retValUInt=test.GetStsTrackIndex(1);
+   EXPECT_EQ(6, retValUInt);
+
+   retValUInt=test.GetStsTrackIndex(2);
+   EXPECT_EQ(-2, retValUInt);
+
+   retValUInt=test.GetNofStsTracks();
+   EXPECT_EQ(2, retValUInt);
+
+   ststrack.clear();
+   ststrack.push_back(11);
+   ststrack.push_back(12);
+   ststrack.push_back(13);
+
+   test.SetStsTracks(ststrack);
+   retValUInt=test.GetNofStsTracks();
+   EXPECT_EQ(3, retValUInt);
+   {
+     SCOPED_TRACE("CheckAddData: Add StsTracks via StsStsTrack");
+     compareEventDataMembers(test, -111, 1., 2., 9, nullptr);
+     compareEventMap(test, 9,  2, Cbm::kMCTrack,    mctrack);
+     compareEventMap(test, 9,  1, Cbm::kStsPoint,   stspoint);
+     compareEventMap(test, 9,  1, Cbm::kStsDigi,    stsdigi);
+     compareEventMap(test, 9,  1, Cbm::kStsCluster, stscluster);
+     compareEventMap(test, 9,  1, Cbm::kStsHit,     stshit);
+     compareEventMap(test, 9,  3, Cbm::kStsTrack,   ststrack);
+   }
+
+   EXPECT_STREQ("Event -111 at t = 1 ns. Registered data types: 6, data objects: 9\n          -- Data type 1, number of data 2\n          -- Data type 2, number of data 1\n          -- Data type 3, number of data 1\n          -- Data type 4, number of data 1\n          -- Data type 5, number of data 1\n          -- Data type 6, number of data 3\n", test.ToString().c_str());
 
  }
 
-    /*
-    kMCTrack,
-            kStsPoint,
-            kStsDigi,
-            kStsCluster,
-            kStsHit,
-            kStsTrack};
-     */
-
-/*
-TEST(_GTestCbmCluster, CheckDefaultConstructor)
-{
-  CbmCluster test;
-  std::vector<Int_t> testvector;
-  {
-    SCOPED_TRACE("CheckDefaultConstructor");
-    compareClusterDataMembers(test, 0, testvector, 0, nullptr);
-  }
-}
-
-TEST(_GTestCbmCluster, CheckStandardConstructor)
-{
-  std::vector<Int_t> testvector{1,2,3,4,5};
-  CbmCluster test{testvector,-11};
-  {
-    SCOPED_TRACE("CheckStandardConstructor");
-    compareClusterDataMembers(test, 5, testvector, -11, nullptr);
-  }
-}
-
-TEST(_GTestCbmCluster, CheckSetDigis)
-{
-  std::vector<Int_t> testvector{1,2,3,4,5};
-  CbmCluster test{testvector,-11};
-  {
-    SCOPED_TRACE("CheckSetDigis: InitialTest");
-    compareClusterDataMembers(test, 5, testvector, -11, nullptr);
-  }
-
-  std::vector<Int_t> testvector1{11,21,31,41,51,61};
-  test.SetDigis(testvector1);
-  {
-    SCOPED_TRACE("CheckSetDigis: SetDigis");
-    compareClusterDataMembers(test, 6, testvector1, -11, nullptr);
-  }
-}
-
-TEST(_GTestCbmCluster, CheckAddDigi)
-{
-  std::vector<Int_t> testvector{1,2,3,4,5};
-  std::vector<Int_t> testvector1{1,2,3,4,5,111};
-  std::vector<Int_t> testvector2{1,2,3,4,5,111,222};
-
-  CbmCluster test{testvector,-11};
-  {
-    SCOPED_TRACE("CheckAddDigi: InitialTest");
-    compareClusterDataMembers(test, testvector.size(), testvector, -11, nullptr);
-  }
-
-  test.AddDigi(111);
-  {
-    SCOPED_TRACE("CheckAddDigis: AddDigi 1");
-    compareClusterDataMembers(test, testvector.size()+1, testvector1, -11, nullptr);
-  }
-
-  test.AddDigi(222);
-  {
-    SCOPED_TRACE("CheckAddDigis: AddDigi 2");
-    compareClusterDataMembers(test, testvector.size()+2, testvector2, -11, nullptr);
-  }
-}
-
-TEST(_GTestCbmCluster, CheckAddDigis)
-{
-  std::vector<Int_t> testvector{1,2,3,4,5};
-  std::vector<Int_t> testvector1{11,21,31,41,51,61};
-  std::vector<Int_t> testvector2{1,2,3,4,5,11,21,31,41,51,61};
-
-  CbmCluster test{testvector,-11};
-  {
-    SCOPED_TRACE("CheckAddDigis: InitialTest");
-    compareClusterDataMembers(test, testvector.size(), testvector, -11, nullptr);
-  }
-
-  test.AddDigis(testvector1);
-  {
-    SCOPED_TRACE("CheckAddDigis: AddDigis");
-    compareClusterDataMembers(test, testvector2.size(), testvector2, -11, nullptr);
-  }
-}
-
-TEST(_GTestCbmCluster, CheckClearDigis)
-{
-  std::vector<Int_t> testvector{1,2,3,4,5};
-  std::vector<Int_t> testvector1;
-
-  CbmCluster test{testvector,-11};
-  {
-    SCOPED_TRACE("CheckClearDigis: InitialTest");
-    compareClusterDataMembers(test, testvector.size(), testvector, -11, nullptr);
-  }
-
-  test.ClearDigis();
-  {
-    SCOPED_TRACE("CheckClearDigis: AddDigis");
-    compareClusterDataMembers(test, 0, testvector1, -11, nullptr);
-  }
-}
-
-TEST(_GTestCbmCluster, CheckSetters)
-{
-  std::vector<Int_t> testvector{1,2,3,4,5};
-
-  CbmCluster test{testvector,-11};
-  {
-    SCOPED_TRACE("CheckSetters: InitialTest");
-    compareClusterDataMembers(test, testvector.size(), testvector, -11, nullptr);
-  }
-
-  test.SetAddress(-111);
-  {
-    SCOPED_TRACE("CheckSetters: SetAddress");
-    compareClusterDataMembers(test, testvector.size(), testvector, -111, nullptr);
-  }
-
-  CbmMatch testMatch;
-  test.SetMatch(&testMatch);
-  {
-    SCOPED_TRACE("CheckSetters: SetAddress");
-    compareClusterDataMembers(test, testvector.size(), testvector, -111, &testMatch);
-  }
-
-}
-
 TEST(_GTestCbmCluster, CheckToString)
 {
-  std::vector<Int_t> testvector{1,2,3,4,5};
-  CbmCluster test{testvector,-11};
 
-  EXPECT_STREQ("CbmCluster: nofDigis=5 | 1 2 3 4 5  | address=-11\n", test.ToString().c_str());
 }
-*/
