fe8eed750c60d3dc24b9833b261a3345edf43016
v.friese <v.friese@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2016-12-21 14:28:25 +0000

    Clean-up of CbmVertex class. Use code style. Remove Print method.

--- data/global/CbmVertex.cxx
+++ data/global/CbmVertex.cxx
@@ -1,7 +1,7 @@
-// -------------------------------------------------------------------------
-// -----                       CbmVertex source file                   -----
-// -----                  Created 28/11/05  by V. Friese               -----
-// -------------------------------------------------------------------------
+/** @file CbmVertex.cxx
+ ** @author V.Friese <v.friese@gsi.de>
+ **/
+
 #include "CbmVertex.h"
 
 #include <iomanip>
@@ -10,138 +10,83 @@
 
 using namespace std;
 
-
 // -----   Default constructor   -------------------------------------------
-CbmVertex::CbmVertex() 
-  : TNamed("Vertex", "Global"),
-    fX(0.),
-    fY(0.),
-    fZ(0.),
-    fChi2(0.),
-    fNDF(0),
-    fNTracks(0),
-    fCovMatrix()
-{
+CbmVertex::CbmVertex() :
+        TNamed("Vertex", "Global"),
+        fX(0.),
+        fY(0.),
+        fZ(0.),
+        fChi2(0.),
+        fNDF(0),
+        fNTracks(0),
+        fCovMatrix() {
 }
 // -------------------------------------------------------------------------
 
 // -----   Constructor with name and title   -------------------------------
-CbmVertex::CbmVertex(const char* name, const char* title) 
-  : TNamed(name, title),
-    fX(0.),
-    fY(0.),
-    fZ(0.),
-    fChi2(0.),
-    fNDF(0),
-    fNTracks(0),
-    fCovMatrix()
-{
+CbmVertex::CbmVertex(const char* name, const char* title) :
+        TNamed(name, title),
+        fX(0.),
+        fY(0.),
+        fZ(0.),
+        fChi2(0.),
+        fNDF(0),
+        fNTracks(0),
+        fCovMatrix() {
 }
 // -------------------------------------------------------------------------
-  
-
 
 // -----   Constructor with all parameters   -------------------------------
-CbmVertex::CbmVertex(const char* name, const char* title,
-		     Double_t x, Double_t y, Double_t z, Double_t chi2,
-		     Int_t ndf, Int_t nTracks, 
-		     const TMatrixFSym& covMat) 
-  : TNamed(name, title),
-    fX(x),
-    fY(y),
-    fZ(z),
-    fChi2(chi2),
-    fNDF(ndf),
-    fNTracks(nTracks),
-    fCovMatrix(covMat)
-{
-  /*
-  fTitle   = title;
-  fX       = x;
-  fY       = y;
-  fZ       = z;
-  fChi2    = chi2;
-  fNDF     = ndf;
-  fNTracks = nTracks;
-  */
+CbmVertex::CbmVertex(const char* name, const char* title, Double_t x,
+                     Double_t y, Double_t z, Double_t chi2, Int_t ndf,
+                     Int_t nTracks, const TMatrixFSym& covMat) :
+        TNamed(name, title),
+        fX(x),
+        fY(y),
+        fZ(z),
+        fChi2(chi2),
+        fNDF(ndf),
+        fNTracks(nTracks),
+        fCovMatrix(covMat) {
 }
 // -------------------------------------------------------------------------
-
-
 
 // -----   Destructor   ----------------------------------------------------
-CbmVertex::~CbmVertex() { }
-// -------------------------------------------------------------------------
-
-
-
-// -----   Public method Print   -------------------------------------------
-void CbmVertex::Print(Option_t*) const {
-  Double_t chi2ndf;
-  if (fNDF) chi2ndf = fChi2 / Double_t(fNDF);
-  else chi2ndf = 0.;	       
-  LOG(INFO) << "Vertex coord. (" << fX << "," << fY << "," << fZ << ") cm, "
-            << "chi2/ndf = " << chi2ndf << ", " << fNTracks
-            << " tracks used" << FairLogger::endl;
+CbmVertex::~CbmVertex() {
 }
 // -------------------------------------------------------------------------
 
-
-
-// -----   Accessor to covariance matrix    --------------------------------
-void CbmVertex::CovMatrix(TMatrixFSym& covMat) const {
-	covMat = fCovMatrix;
-}
-// -------------------------------------------------------------------------
-
-
-
-// -----   Accessor to covariance matrix elements   ------------------------
-/*Double_t CbmVertex::GetCovariance(Int_t i, Int_t j) const {
-	return fCovMatrix(i,j);
-}
-*/
-// -------------------------------------------------------------------------
-
-
-
-// -----   Public method SetVertex   ---------------------------------------
+// -----   Set vertex parameters   -----------------------------------------
 void CbmVertex::SetVertex(Double_t x, Double_t y, Double_t z, Double_t chi2,
-			  Int_t ndf, Int_t nTracks, 
-			  const TMatrixFSym& covMat) {
-  fX       = x;
-  fY       = y;
-  fZ       = z;
-  fChi2    = chi2;
-  fNDF     = ndf;
+                          Int_t ndf, Int_t nTracks,
+                          const TMatrixFSym& covMat) {
+  fX = x;
+  fY = y;
+  fZ = z;
+  fChi2 = chi2;
+  fNDF = ndf;
   fNTracks = nTracks;
   fCovMatrix = covMat;
 }
 // -------------------------------------------------------------------------
 
-
-
-// -----   Public method Reset   -------------------------------------------
+// -----   Reset all parameters   ------------------------------------------
 void CbmVertex::Reset() {
   fX = fY = fZ = fChi2 = 0.;
   fNDF = fNTracks = 0;
   fCovMatrix.Clear();
-}  
-// -------------------------------------------------------------------------
-
-
-
-// --- String output  ------------------------------------------------------
-string CbmVertex::ToString() const {
-
-  Double_t chi2ndf = ( fNDF ? fChi2 / Double_t(fNDF) : 0.);
-  stringstream ss;
-  ss << "Vertex: position (" << fixed << setprecision(4) << fX << ", " << fY << ", " << fZ
-	 << ") cm, chi2/ndf = " << chi2ndf << ", tracks used: " << fNTracks;
-   return ss.str();
 }
 // -------------------------------------------------------------------------
 
-
+// --- String output  ------------------------------------------------------
+string CbmVertex::ToString() const {
+  Double_t chi2ndf = ( fNDF ? fChi2 / Double_t(fNDF) : 0. );
+  stringstream ss;
+  ss << "Vertex: position (" << fixed << setprecision(4) << fX << ", " << fY
+     << ", " << fZ << ") cm, chi2/ndf = " << chi2ndf << ", tracks used: "
+     << fNTracks;
+  return ss.str();
+}
+// -------------------------------------------------------------------------
 
 ClassImp(CbmVertex)
