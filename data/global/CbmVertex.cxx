fd1ab1eb3609800469e8ad2ee6b5dfe58ac9fca5
f.uhlig <f.uhlig@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2017-01-18 16:27:45 +0000

    Revert back to the old internal representation of the class. This is needed due to crashes when using the new version with ROOT5.

--- data/global/CbmVertex.cxx
+++ data/global/CbmVertex.cxx
@@ -1,114 +1,168 @@
-/** @file CbmVertex.cxx
- ** @author V.Friese <v.friese@gsi.de>
- **/
-
+// -------------------------------------------------------------------------
+// -----                       CbmVertex source file                   -----
+// -----                  Created 28/11/05  by V. Friese               -----
+// -------------------------------------------------------------------------
 #include "CbmVertex.h"
 
 #include <iomanip>
 #include <sstream>
-
 #include "FairLogger.h"
 
 using namespace std;
 
+
 // -----   Default constructor   -------------------------------------------
-CbmVertex::CbmVertex() :
-        TNamed("Vertex", "Global"),
-        fX(0.),
-        fY(0.),
-        fZ(0.),
-        fChi2(0.),
-        fNDF(0),
-        fNTracks(0),
-        fCovMatrix(3) {
+CbmVertex::CbmVertex() 
+  : TNamed("Vertex", "Global"),
+    fX(0.),
+    fY(0.),
+    fZ(0.),
+    fChi2(0.),
+    fNDF(0),
+    fNTracks(0),
+    fCovMatrix()
+{
+  for(Int_t i=0; i<6; i++) fCovMatrix[i] = 0;
 }
 // -------------------------------------------------------------------------
 
 // -----   Constructor with name and title   -------------------------------
-CbmVertex::CbmVertex(const char* name, const char* title) :
-        TNamed(name, title),
-        fX(0.),
-        fY(0.),
-        fZ(0.),
-        fChi2(0.),
-        fNDF(0),
-        fNTracks(0),
-        fCovMatrix(3) {
+CbmVertex::CbmVertex(const char* name, const char* title) 
+  : TNamed(name, title),
+    fX(0.),
+    fY(0.),
+    fZ(0.),
+    fChi2(0.),
+    fNDF(0),
+    fNTracks(0),
+    fCovMatrix()
+{
+  for(Int_t i=0; i<6; i++) fCovMatrix[i] = 0;
 }
 // -------------------------------------------------------------------------
+  
+
 
 // -----   Constructor with all parameters   -------------------------------
-CbmVertex::CbmVertex(const char* name, const char* title, Double_t x,
-                     Double_t y, Double_t z, Double_t chi2, Int_t ndf,
-                     Int_t nTracks, const TMatrixFSym& covMat) :
-        TNamed(name, title),
-        fX(x),
-        fY(y),
-        fZ(z),
-        fChi2(chi2),
-        fNDF(ndf),
-        fNTracks(nTracks),
-        fCovMatrix(3)
+CbmVertex::CbmVertex(const char* name, const char* title,
+		     Double_t x, Double_t y, Double_t z, Double_t chi2,
+		     Int_t ndf, Int_t nTracks, 
+		     const TMatrixFSym& covMat) 
+  : TNamed(name, title),
+    fX(x),
+    fY(y),
+    fZ(z),
+    fChi2(chi2),
+    fNDF(ndf),
+    fNTracks(nTracks),
+    fCovMatrix()
 {
   if ( (covMat.GetNrows() != 3) && (covMat.GetNcols() != 3) ) {
     LOG(ERROR) << "Wrong dimension of passed covariance matrix. Clear the covariance matrix." << FairLogger::endl;
+    for(Int_t i=0; i<6; i++) fCovMatrix[i] = 0;
   } else {
-    fCovMatrix = covMat;
+    Int_t index = 0;
+    for (Int_t i=0; i<3; i++) {
+      for (Int_t j=i; j<3; j++) fCovMatrix[index++] = covMat[i][j];
+    }
   }
 }
 // -------------------------------------------------------------------------
+
+
 
 // -----   Destructor   ----------------------------------------------------
-CbmVertex::~CbmVertex() {
+CbmVertex::~CbmVertex() { }
+// -------------------------------------------------------------------------
+
+
+
+// -----   Public method Print   -------------------------------------------
+void CbmVertex::Print(Option_t*) const {
+  Double_t chi2ndf;
+  if (fNDF) chi2ndf = fChi2 / Double_t(fNDF);
+  else chi2ndf = 0.;	       
+  LOG(INFO) << "Vertex coord. (" << fX << "," << fY << "," << fZ << ") cm, "
+            << "chi2/ndf = " << chi2ndf << ", " << fNTracks
+            << " tracks used" << FairLogger::endl;
 }
 // -------------------------------------------------------------------------
 
-// -----   Set vertex parameters   -----------------------------------------
-void CbmVertex::SetVertex(Double_t x, Double_t y, Double_t z, Double_t chi2,
-                          Int_t ndf, Int_t nTracks,
-                          const TMatrixFSym& covMat) {
-  fX = x;
-  fY = y;
-  fZ = z;
-  fChi2 = chi2;
-  fNDF = ndf;
-  fNTracks = nTracks;
-  if ( (covMat.GetNrows() != 3) && (covMat.GetNcols() != 3) ) {
-    for(Int_t i=0; i<3; ++i) {
-      for(Int_t j=0; j<3; ++j) {
-        fCovMatrix(i,j)= 0.;
-      }
+
+
+// -----   Accessor to covariance matrix    --------------------------------
+void CbmVertex::CovMatrix(TMatrixFSym& covMat) const {
+  Int_t index = 0;
+  for (int i=0; i<3; i++) {
+    for (int j=i; j<3; j++) {
+      covMat[i][j] = fCovMatrix[index];
+      covMat[j][i] = fCovMatrix[index];
+      index++;
     }
-    LOG(ERROR) << "Wrong dimension of passed covariance matrix. Clear the covariance matrix." << FairLogger::endl;
-  }  else {
-    fCovMatrix = covMat;
   }
 }
 // -------------------------------------------------------------------------
 
-// -----   Reset all parameters   ------------------------------------------
+
+
+// -----   Accessor to covariance matrix elements   ------------------------
+Double_t CbmVertex::GetCovariance(Int_t i, Int_t j) const {
+  TMatrixFSym* mat = new TMatrixFSym(3);
+  CovMatrix(*mat);
+  Double_t element = (*mat)[i][j];
+  delete mat;
+  return element;
+}
+// -------------------------------------------------------------------------
+
+
+
+// -----   Public method SetVertex   ---------------------------------------
+void CbmVertex::SetVertex(Double_t x, Double_t y, Double_t z, Double_t chi2,
+			  Int_t ndf, Int_t nTracks, 
+			  const TMatrixFSym& covMat)
+{
+  fX       = x;
+  fY       = y;
+  fZ       = z;
+  fChi2    = chi2;
+  fNDF     = ndf;
+  fNTracks = nTracks;
+  if ( (covMat.GetNrows() != 3) && (covMat.GetNcols() != 3) ) {
+    LOG(ERROR) << "Wrong dimension of passed covariance matrix. Clear the covariance matrix." << FairLogger::endl;
+    for(Int_t i=0; i<6; i++) fCovMatrix[i] = 0;
+  } else {
+    Int_t index = 0;
+    for (Int_t i=0; i<3; i++) {
+      for (Int_t j=i; j<3; j++) fCovMatrix[index++] = covMat[i][j];
+    }
+  }
+}
+// -------------------------------------------------------------------------
+
+
+
+// -----   Public method Reset   -------------------------------------------
 void CbmVertex::Reset() {
   fX = fY = fZ = fChi2 = 0.;
   fNDF = fNTracks = 0;
-  // Clear loescht das komplette Objekt anstatt die Werte auf Null zu setzen
-  //  fCovMatrix.Clear();
-  for(Int_t i=0; i<3; ++i) {
-    for(Int_t j=0; j<3; ++j) {
-      fCovMatrix(i,j)= 0.;
-    }
-  }
-}
+  for(Int_t i=0; i<6; i++) fCovMatrix[i] = 0;
+}  
 // -------------------------------------------------------------------------
+
+
 
 // --- String output  ------------------------------------------------------
 string CbmVertex::ToString() const {
-  Double_t chi2ndf = ( fNDF ? fChi2 / Double_t(fNDF) : 0. );
+
+  Double_t chi2ndf = ( fNDF ? fChi2 / Double_t(fNDF) : 0.);
   stringstream ss;
-  ss << "Vertex: position (" << fixed << setprecision(4) << fX << ", " << fY
-     << ", " << fZ << ") cm, chi2/ndf = " << chi2ndf << ", tracks used: "
-     << fNTracks;
-  return ss.str();
+  ss << "Vertex: position (" << fixed << setprecision(4) << fX << ", " << fY << ", " << fZ
+	 << ") cm, chi2/ndf = " << chi2ndf << ", tracks used: " << fNTracks;
+   return ss.str();
 }
 // -------------------------------------------------------------------------
+
+
 
 ClassImp(CbmVertex)
--- data/global/CbmVertex.h
+++ data/global/CbmVertex.h
@@ -1,158 +1,121 @@
-/** @file CbmVertex.h
- ** @author V.Friese <v.friese@gsi.de>
+// -------------------------------------------------------------------------
+// -----                      CbmVertex header file                    -----
+// -----                  Created 28/11/05  by V. Friese               -----
+// -------------------------------------------------------------------------
+
+
+/** CbmVertex.h
+ *@author V.Friese <v.friese@gsi.de>
+ **
+ ** Data class for a vertex in CBM.
+ ** Data level: RECO
  **/
+
 
 #ifndef CBMVERTEX_H
 #define CBMVERTEX_H 1
+
 
 #include <string>
 #include "TMatrixFSym.h"
 #include "TNamed.h"
 #include "TVector3.h"
 
-/** @class CbmVertex
- ** @brief Data class for a vertex in CBM.
- ** @author V. Friese <v.friese@gsi.de>
- ** @version 2.0
- **/
+
 class CbmVertex : public TNamed
 {
 
-  public:
+ public:
 
-    /** Default constructor  **/
-    CbmVertex();
-
-    /** Constructor with name and title
-     ** @param name  Vertex name
-     ** @param title Vertex title
-     **/
-    CbmVertex(const char* name, const char* title);
-
-    /** Constructor with all member variables
-     ** @param name      Name of object
-     ** @param title     Title of object
-     ** @param x         x coordinate [cm]
-     ** @param y         y coordinate [cm]
-     ** @param z         z coordinate [cm]
-     ** @param chi2      chi square of vertex fit
-     ** @param ndf       Number of degrees of freedom of vertex fit
-     ** @param nTracks   Number of tracks used for vertex fit
-     ** @param covMat    Covariance Matrix (symmetric, 3x3)
-     **/
-    CbmVertex(const char* name, const char* title, Double_t x, Double_t y,
-              Double_t z, Double_t chi2, Int_t ndf, Int_t nTracks,
-              const TMatrixFSym& covMat);
-
-    /** Destructor **/
-    virtual ~CbmVertex();
-
-    /** x coordinate
-     ** value  x coordinate of vertex [cm]
-     **/
-    Double_t GetX() const {
-      return fX;
-    }
-
-    /** y coordinate
-     ** value  y coordinate of vertex [cm]
-     **/
-    Double_t GetY() const {
-      return fY;
-    }
-
-    /** z coordinate
-     ** value  z coordinate of vertex [cm]
-     **/
-    Double_t GetZ() const {
-      return fZ;
-    }
-
-    /** chi square
-     ** @value chi square of vertex fit
-     **/
-    Double_t GetChi2() const {
-      return fChi2;
-    }
-
-    /** Degrees-of-freedom
-     ** value  Number of degrees-of-freedom for vertex fit
-     **/
-    Int_t GetNDF() const {
-      return fNDF;
-    }
-
-    /** Number of tracks used for the vertex fit
-     ** @value  Number of tracks used for the vertex fit
-     **/
-    Int_t GetNTracks() const {
-      return fNTracks;
-    }
-    ;
-
-    /** Vertex position
-     ** @param[out] Vertex position vector
-     */
-    void Position(TVector3& pos) const {
-      pos.SetXYZ(fX, fY, fZ);
-    }
-
-    /** Covariance matrix
-     ** @param[out] Covariance matrix
-     **/
-    void CovMatrix(TMatrixFSym& covMat) const {
-      covMat = fCovMatrix;
-    }
-
-    /** Covariance matrix elements
-     ** @param i  Matrix row index
-     ** @param j  Matrix column index
-     ** @value Covariance matrix element (i,j)
-     **/
-    Double_t GetCovariance(Int_t i, Int_t j) const {
-      return fCovMatrix(i, j);
-    }
-
-    /** Reset the member variables **/
-    void Reset();
-
-    /** Set the member variables
-     ** @param x         x coordinate [cm]
-     ** @param y         y coordinate [cm]
-     ** @param z         z coordinate [cm]
-     ** @param chi2      chi square of vertex fit
-     ** @param ndf       Number of degrees of freedom of vertex fit
-     ** @param nTracks   Number of tracks used for vertex fit
-     ** @param covMat    Covariance Matrix (symmetric, 3x3)
-     **/
-    void SetVertex(Double_t x, Double_t y, Double_t z, Double_t chi2,
-                   Int_t ndf, Int_t nTracks, const TMatrixFSym& covMat);
-
-    /** String output **/
-    virtual std::string ToString() const;
-
-  private:
-
-    /** Position coordinates  [cm] **/
-    Double32_t fX, fY, fZ;
-
-    /** Chi2 of vertex fit **/
-    Double32_t fChi2;
-
-    /** Number of degrees of freedom of vertex fit **/
-    Int_t fNDF;
-
-    /** Number of tracks used for the vertex fit **/
-    Int_t fNTracks;
-
-    /** Covariance matrix for x, y, and z stored in an array. The
-     ** sequence is a[0,0], a[0,1], a[0,2], a[1,1], a[1,2], a[2,2]
-     **/
-    TMatrixFSym fCovMatrix;
+  /** Default constructor  **/
+  CbmVertex();
 
 
-  ClassDef(CbmVertex,2);
+  /** Constructor with name and title **/
+  CbmVertex(const char* name, const char* title);
+
+
+  /** Constructor with all member variables 
+   *@param name      Name of object
+   *@param title     Title of object
+   *@param x         x coordinate [cm]
+   *@param y         y coordinate [cm]
+   *@param z         z coordinate [cm]
+   *@param chi2      chi square of vertex fit
+   *@param ndf       Number of degrees of freedom of vertex fit
+   *@param nTracks   Number of tracks used for vertex fit
+   *@param covMat    Covariance Matrix (symmetric, 3x3)
+   **/
+  CbmVertex(const char* name, const char* title, 
+	    Double_t x, Double_t y, Double_t z, Double_t chi2,
+	    Int_t ndf, Int_t nTracks, const TMatrixFSym& covMat);
+
+
+  /** Destructor **/
+  virtual ~CbmVertex();
+
+
+  /** Ouput to screen **/
+  virtual void Print(Option_t* opt = "") const ;
+
+
+  /** Accessors **/
+  Double_t GetX()    const { return fX;       };  // x position [cm]
+  Double_t GetY()    const { return fY;       };  // y position [cm]
+  Double_t GetZ()    const { return fZ;       };  // z posiiton [cm]
+  Double_t GetChi2() const { return fChi2;    };  // chi2
+  Int_t GetNDF()     const { return fNDF;     };  // nof degrees of freedom
+  Int_t GetNTracks() const { return fNTracks; };  // nof tracks used
+  void Position(TVector3& pos) const { pos.SetXYZ(fX,fY,fZ); };
+  void CovMatrix(TMatrixFSym& covMat) const;
+  Double_t GetCovariance(Int_t i, Int_t j) const;
+
+
+  /** Reset the member variables **/
+  void Reset();
+
+
+  /** Set the member variables
+   *@param x         x coordinate [cm]
+   *@param y         y coordinate [cm]
+   *@param z         z coordinate [cm]
+   *@param chi2      chi square of vertex fit
+   *@param ndf       Number of degrees of freedom of vertex fit
+   *@param nTracks   Number of tracks used for vertex fit
+   *@param covMat    Covariance Matrix (symmetric, 3x3)
+   **/
+  void SetVertex(Double_t x, Double_t y, Double_t z, Double_t chi2,
+		 Int_t ndf, Int_t nTracks, const TMatrixFSym& covMat);
+
+
+  /** String output **/
+  virtual std::string ToString() const;
+
+		    
+
+ private:
+
+  /** Position coordinates  [cm] **/
+  Double32_t fX, fY, fZ;
+
+  /** Chi2 of vertex fit **/
+  Double32_t fChi2;
+
+  /** Number of degrees of freedom of vertex fit **/
+  Int_t fNDF;
+
+  /** Number of tracks used for the vertex fit **/
+  Int_t fNTracks;
+
+  /** Covariance matrix for x, y, and z stored in an array. The
+   ** sequence is a[0,0], a[0,1], a[0,2], a[1,1], a[1,2], a[2,2]
+   **/
+  Double32_t fCovMatrix[6];
+
+
+  ClassDef(CbmVertex,1);
 
 };
+
 
 #endif
