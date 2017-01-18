d6566c5b44c680a29316078f031c04e77b359d1b
f.uhlig <f.uhlig@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2017-01-18 08:37:42 +0000

    Use the new setup files.

--- macro/run/run_qa.C
+++ macro/run/run_qa.C
@@ -5,43 +5,7 @@
 // V. Friese   13/01/2006
 //
 // --------------------------------------------------------------------------
-
-TString caveGeom="";
-TString pipeGeom="";
-TString magnetGeom="";
-TString mvdGeom="";
-TString stsGeom="";
-TString richGeom="";
-TString muchGeom="";
-TString shieldGeom="";
-TString trdGeom="";
-TString tofGeom="";
-TString ecalGeom="";
-TString platformGeom="";
-TString psdGeom="";
-Double_t psdZpos=0.;
-Double_t psdXpos=0.;
-
-TString mvdTag="";
-TString stsTag="";
-TString trdTag="";
-TString tofTag="";
-
-TString stsDigi="";
-TString trdDigi="";
-TString tofDigi="";
-
-TString mvdMatBudget="";
-TString stsMatBudget="";
-
-TString  fieldMap="";
-Double_t fieldZ=0.;
-Double_t fieldScale=0.;
-Int_t    fieldSymType=0;
-
-TString defaultInputFile="";
-
-void run_qa(Int_t nEvents = 1, const char* setup = "sis100_electron")
+void run_qa(Int_t nEvents = 1, const char* setupName = "sis100_electron")
 {
 
   // ========================================================================
@@ -53,26 +17,29 @@ void run_qa(Int_t nEvents = 1, const char* setup = "sis100_electron")
   TString outDir  = "data/";
 
   // MC file
-  TString simFile = outDir + setup + "_test.mc.root";   // Input file (MC events)
+  TString simFile = outDir + setupName + "_test.mc.root";   // Input file (MC events)
 
   // Reco file
-  TString recFile = outDir + setup + "_test.eds.root";  // Output file
+  TString recFile = outDir + setupName + "_test.eds.root";  // Output file
 
   // Parameter file
-  TString parFile = outDir + setup + "_params.root";    // Parameter file
+  TString parFile = outDir + setupName + "_params.root";    // Parameter file
 
   // Output file
-  TString outFile = outDir + setup + "_test.qa.root";   // Output file
+  TString outFile = outDir + setupName + "_test.qa.root";   // Output file
   
-  TString inDir = gSystem->Getenv("VMCWORKDIR");
-  TString paramDir = inDir + "/parameters/";
+  TString srcDir = gSystem->Getenv("VMCWORKDIR");
+  TString paramDir = srcDir + "/parameters/";
 
-  TString setupFile = inDir + "/geometry/setup/" + setup + "_setup.C";
-  TString setupFunct = setup;
-  setupFunct += "_setup()";
 
+  // -----   Load the geometry setup   -------------------------------------
+  std::cout << std::endl;
+  TString setupFile = srcDir + "/geometry/setup/setup_" + setupName + ".C";
+  TString setupFunct = "setup_";
+  setupFunct = setupFunct + setupName + "()";
+  std::cout << "-I- " << "run_qa" << ": Loading macro " << setupFile << std::endl;
   gROOT->LoadMacro(setupFile);
-  gInterpreter->ProcessLine(setupFunct);
+  gROOT->ProcessLine(setupFunct);
 
   // In general, the following parts need not be touched
   // ========================================================================
