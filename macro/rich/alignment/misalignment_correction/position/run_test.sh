b94a834b26c28c04e5022836e2455c53304e6572
jordan.bendarouach <jordan.bendarouach@physik.uni-giessen.de>
2017-01-18 09:16:37 +0000

    D    position/run_sim.sge D    position/run_test.sh D    position/sim_old.sge D    position/test.sh D    position/test1.sh

--- macro/rich/alignment/misalignment_correction/position/run_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sbatch -J position -D /lustre/nyx/cbm/users/jbendar -o %j_%N.out.log -e %j_%N.err.log --time=0:30:00 --array=1-3 ./test1.sh
-
-# sbatch --array=1-3 ./test.sh
--- macro/rich/alignment/misalignment_correction/position/test1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Execute application code
-hostname ; uptime ; sleep 5 ; uname -a
-echo 'task ID: ' $SLURM_ARRAY_TASK_ID
