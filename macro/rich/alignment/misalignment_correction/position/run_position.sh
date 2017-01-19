5dcc3514896945c456e4698839fb7acc974edefa
jordan.bendarouach <jordan.bendarouach@physik.uni-giessen.de>
2017-01-19 15:25:17 +0000

    M    position/run_position.sh

--- macro/rich/alignment/misalignment_correction/position/run_position.sh
+++ macro/rich/alignment/misalignment_correction/position/run_position.sh
@@ -11,5 +11,6 @@
 FLAG=1
 NEVTS=1000	# nb of events to run
 COLL_ENERGY=8gev
-	sbatch -J test_position -D /lustre/nyx/cbm/users/jbendar/Sim_Outputs/Ring_Track_VS_Position/Aligned -o %j_%N.out.log -e %j_%N.err.log --time=1:00:00 --array=1-2 ./position.sh $FLAG $NEVTS $COLL_ENERGY
+##	sbatch -J test_position -D /lustre/nyx/cbm/users/jbendar/Sim_Outputs/Ring_Track_VS_Position/Aligned -o %j_%N.out.log -e %j_%N.err.log --time=12:00:00 --array=1-100 ./position.sh $FLAG $NEVTS $COLL_ENERGY
+	sbatch -J test_position -D /lustre/nyx/cbm/users/jbendar/Sim_Outputs/Ring_Track_VS_Position/Misaligned -o %j_%N.out.log -e %j_%N.err.log --time=12:00:00 --array=1-100 ./position.sh $FLAG $NEVTS $COLL_ENERGY
 # done
