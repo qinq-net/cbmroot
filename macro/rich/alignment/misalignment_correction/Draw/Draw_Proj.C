22404a603c4f5e54a2162a056ea2413302c66ac7
jordan.bendarouach <jordan.bendarouach@physik.uni-giessen.de>
2017-03-06 14:35:27 +0000

    M    Draw/Draw_Proj.C

--- macro/rich/alignment/misalignment_correction/Draw/Draw_Proj.C
+++ macro/rich/alignment/misalignment_correction/Draw/Draw_Proj.C
@@ -1,25 +1,41 @@
 #include <iomanip>
 #include <vector>
-#include "/u/jbendar/CBMSRC/base/draw/CbmDrawHist.h"
+#include "/u/jbendar/cbmroot5/CBMSRC/base/draw/CbmDrawHist.h"
 
 void Draw_Proj()
 {
-	gROOT->LoadMacro("/lustre/nyx/cbm/users/jbendar/CBMINSTALL/share/cbmroot/macro/littrack/loadlibs.C");
+//	gROOT->LoadMacro("/lustre/nyx/cbm/users/jbendar/CBMINSTALL_Root5/share/cbmroot/macro/littrack/loadlibs.C");
+	gROOT->LoadMacro("$VMCWORKDIR/macro/littrack/loadlibs.C");
 	loadlibs();
 
 	TH3D *Histo;
-	TString Histo_Name = "fh_ring_track_distance_vs_xy_truematch";
+	TString Histo_Name_1 = "fhRingTrackDistVsXYTruematchPi";
+	TString Histo_Name_2 = "fhRingTrackDistVsXYTruematchPrimel";
 
-	TString Path_Histo = "/lustre/nyx/cbm/users/jbendar/Sim_Outputs/Ring_Track_VS_Position/Misaligned/merge.root";
+	TString Path_Histo = "/lustre/nyx/cbm/users/jbendar/Sim_Outputs/Ring_Track_VS_Position/Misaligned_5mrad_Full/merge.root";
 	TFile* Histograms = new TFile(Path_Histo, "READ");
 	// cout << Histograms << endl;
-	Histo = (TH3D*) gDirectory->Get(Histo_Name);
+	Histo = (TH3D*) gDirectory->Get(Histo_Name_1);
 
+	Draw(Histo);
+
+/*	TCanvas *Can = new TCanvas("Can","Can", 10, 10, 1010, 510);
+	Can->Divide(2,1);
+	Can->cd(1);
+	Histo->Draw();
+	TH2D *Proj;
+	Proj = DrawH3Profile(Histo, false, false, 0., 1.);
+	Can->cd(2);
+	Proj->Draw("colz");
+*/
+}
+
+void Draw(TH3D* Histo)
+{
 	TCanvas *Can = new TCanvas("Can","Can", 10, 10, 1010, 510);
 	Can->Divide(2,1);
 	Can->cd(1);
 	Histo->Draw();
-
 	TH2D *Proj;
 	Proj = DrawH3Profile(Histo, false, false, 0., 1.);
 	Can->cd(2);
