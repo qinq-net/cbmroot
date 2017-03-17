cf4ed023c0a610f7c21b95426a3b1bba7b5c0a5b
jordan.bendarouach <jordan.bendarouach@physik.uni-giessen.de>
2017-03-17 10:50:12 +0000

    D    misalignment_correction/matching/geosetup D    misalignment_correction/position/geosetup

--- macro/rich/alignment/misalignment_correction/Import_GDML_Export_ROOT.c
+++ macro/rich/alignment/misalignment_correction/Import_GDML_Export_ROOT.c
@@ -1,6 +1,7 @@
 void Import_GDML_Export_ROOT(TString axis="", TString nb="")
 {
-	TString vmcDir = TString(gSystem->Getenv("VMCWORKDIR"));
+//	TString vmcDir = TString(gSystem->Getenv("VMCWORKDIR"));
+	TString vmcDir = "/u/jbendar/cbmroot5/CBMSRC/geometry/rich/";
 
 	TGeoManager *gdml = new TGeoManager("gdml", "FAIRGeom");
 
@@ -9,7 +10,9 @@ void Import_GDML_Export_ROOT(TString axis="", TString nb="")
 //	TGeoVolume* gdmlTop = parser.GDMLReadFile (vmcDir + "/geometry/rich/rich_v16a_1e.gdml");
 //	TGeoVolume* gdmlTop = parser.GDMLReadFile (vmcDir + "/geometry/rich/Rich_jan2016_misalign.gdml");
 //	TGeoVolume* gdmlTop = parser.GDMLReadFile (vmcDir + "/geometry/rich/Rich_jan2016_misalign_5mrad_study.gdml");
-	TGeoVolume* gdmlTop = parser.GDMLReadFile (vmcDir + "/geometry/rich/RICH_21Nov2016_simplified_test.gdml");
+//	TGeoVolume* gdmlTop = parser.GDMLReadFile (vmcDir + "/geometry/rich/RICH_21Nov2016_simplified_test.gdml");
+
+	TGeoVolume* gdmlTop = parser.GDMLReadFile (vmcDir + "matching/misalign_1mrad_jan16.gdml");
 	TGeoVolume* rootTop = new TGeoVolumeAssembly("TOP");
 
 	gGeoManager->SetTopVolume(rootTop);
@@ -29,9 +32,11 @@ void Import_GDML_Export_ROOT(TString axis="", TString nb="")
 //	TFile* outfile = new TFile(vmcDir + "/geometry/rich/rich_v16a_1e.root", "RECREATE");
 //	TFile* outfile = new TFile(vmcDir + "/geometry/rich/Rich_jan2016_misalign_1pt5.root", "RECREATE");
 //	TFile* outfile = new TFile(vmcDir + "/geometry/rich/Rich_jan2016_misalign_5mradXY_Tile1_3.root", "RECREATE");
-	TFile* outfile = new TFile(vmcDir + "/geometry/rich/RICH_21Nov2016_simplified_test.root", "RECREATE");
+//	TFile* outfile = new TFile(vmcDir + "/geometry/rich/RICH_21Nov2016_simplified_test.root", "RECREATE");
 //	TFile* outfile = new TFile("/data/misalignment_correction/Sim_Outputs/RICH_Geometries/Tile_2_8/Rich_jan2016_misalign_" + nb + "mrad_" + axis + ".root", "RECREATE");
 //	TFile* outfile = new TFile("/home/aghoehne/Desktop/Rich_jan2016_misalign_" + nb + "mrad_" + axis + ".root", "RECREATE");
+
+	TFile* outfile = new TFile(vmcDir + "rich_v16a_matching_misalign_1mrad_jan16.geo.root", "RECREATE");
 	rootTop->Write();
 	outfile->Close();
 }
