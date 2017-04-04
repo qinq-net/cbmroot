61c05b846725cecd28b1e1eaf5f74b7956286b6c
n.herrmann <n.herrmann@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2017-04-04 16:02:34 +0000

    efficiency calculation with proper errors

--- macro/beamtime/pl_Datime.C
+++ macro/beamtime/pl_Datime.C
@@ -1,7 +1,39 @@
-void pl_Datime(){
+void pl_Datime(TString sysinfo=""){
+  TCanvas *can = (TCanvas *)gROOT->FindObjectAny("can");
+  //cout <<" got can = "<<can<<endl;
+  if(NULL == can) return;
+  //can->Draw();
+  can->cd();
+
+/*
+    https://root-forum.cern.ch/t/writing-text-on-foreground-on-a-canvas/17435/2
+*/
+  TPad *newpad=new TPad("newpad","a transparent pad",0,0,1,1);
+  newpad->SetFillStyle(4000);
+  newpad->Draw();
+  newpad->cd();
+/*
+TPaveLabel *title = new TPaveLabel(0.1,0.94,0.9,0.98,"Drawing options for one dimensional histograms");
+title->SetFillColor(16);
+title->SetTextFont(52);
+title->Draw();
+*/
   pTime=new TDatime();
-  cout << " add time stamp to canvas " <<pTime->GetDate() << ", "<<pTime->GetTime() <<endl;
-  TPaveText *pt = new TPaveText(.01,.97,.1,.99);
+  //cout << " add time stamp to canvas " <<pTime->GetDate() << ", "<<pTime->GetTime() <<endl;
+  TPaveText *pt = new TPaveText(.90,.975,.99,.99,"blNDC");
+  pt->SetBorderSize(0);
+  pt->SetFillColor(0);
   pt->AddText(Form("%d,%d",pTime->GetDate(),pTime->GetTime()));
   pt->Draw();
+  if(sysinfo!=""){
+    cout << " add to canvas: sysinfo = " << sysinfo.Data() << endl; 
+    TPaveText *st = new TPaveText(.96,.9,.99,.4,"blNDC");
+    st->SetBorderSize(0);
+    st->SetFillColor(0);
+    TText *text = (TText *) st->AddText(Form("%s",sysinfo.Data()));
+    text->SetTextAngle(-90);
+    text->SetTextSize(0.016);
+    text->SetTextColor(4);
+    st->Draw();
+  }
 }
--- macro/beamtime/pl_TIS.C
+++ macro/beamtime/pl_TIS.C
@@ -1,4 +1,5 @@
-void pl_TIS(Double_t dFracMax=0.1){
+void pl_TIS(TString sysinfo="", Double_t dFracMax=0.1){
+ gROOT->LoadMacro("pl_Datime.C");
   //  TCanvas *can = new TCanvas("can22","can22");
   //  can->Divide(2,2); 
   TCanvas *can = new TCanvas("can","can",48,55,700,700);
@@ -32,7 +33,7 @@ can->cd(1);
  if (h1!=NULL) {
   h1->Draw("same");
   h1->SetLineColor(3);
-  h1->GetXaxis()->SetTitle("time [10ns]");
+  h1->GetXaxis()->SetTitle("time [s]");
  }else  { cout << hname << " not found" << endl; }
  TH1 *hTIS_all = (TH1 *)h1->Clone();
 
@@ -42,7 +43,8 @@ can->cd(2);
  h1=(TH1 *)gROOT->FindObjectAny(hname);
  if (h1!=NULL) {
    h1->Draw();
-   h1->GetXaxis()->SetTitle("time [10ns]");
+   h1->GetXaxis()->SetTitle("time [s]");
+   gPad->SetLogy();
  }else  { cout << hname << " not found" << endl; }
  TH1 *hTIS_sel = (TH1 *)h1->Clone();
 
@@ -64,6 +66,7 @@ can->cd(2);
 
 can->cd(3);
  gROOT->cd();
+ /*
  TH1F *hTISselfrac = (TH1F *)hTIS_all->Clone();
  hTISselfrac->SetName("hTISselfrac");
  hTISselfrac->SetTitle("MRef - selector probability");
@@ -72,22 +75,36 @@ can->cd(3);
  hTISselfrac->SetMinimum(0.0001);
  hTISselfrac->Draw();
  hTISselfrac->SetLineColor(hTIS_sel->GetLineColor()); 
+ */
 
+ TEfficiency *pEffSel = new TEfficiency(*hTIS_sel2,*hTIS_all);
+ pEffSel->SetTitle("Selector (MRef & Sel2)  efficiency");
+ pEffSel->Draw("AP");
+
+ /*
  TH1F *hTISsel1frac = (TH1F *)hTIS_all->Clone();
  hTISsel1frac->SetName("hTISsel1frac");
  hTISsel1frac->SetTitle("Dut & MRef coinicidence probability");
  hTISsel1frac->Divide(hTIS_sel1, hTIS_all, 1., 1., "B");
  hTISsel1frac->Draw("same");
  hTISsel1frac->SetLineColor(hTIS_sel1->GetLineColor()); 
+ */
+ gPad->SetLogy();
 
- // gPad->SetLogy();
 can->cd(4);
+/*
  TH1F *hselsel1frac = (TH1F *)hTIS_sel->Clone();
  hselsel1frac->SetName("hselsel1frac");
  hselsel1frac->SetTitle("Relative efficiency of DUT");
- hselsel1frac->Divide(hTIS_sel1, hTIS_sel, 1., 1., "B");
- hselsel1frac->Draw("");
+ // hselsel1frac->Divide(hTIS_sel1, hTIS_sel, 1., 1., "B");
+ hselsel1frac->Divide(hTIS_sel1, hTIS_sel, 1., 1., "");
+ hselsel1frac->Draw("E1");
  hselsel1frac->SetLineColor(hTIS_sel1->GetLineColor()); 
+*/
+ TEfficiency *pEffDut = new TEfficiency(*hTIS_sel1,*hTIS_sel2);
+ pEffDut->SetTitle("Relative efficiency of DUT");
+ pEffDut->Draw("AP");
+ // gPad->SetLogy();
 
 can->cd(5);
  gROOT->cd();
@@ -102,7 +119,10 @@ can->cd(6);
  h2pfx->Draw();
  TH1D *h2pfxes=(TH1D *)h2pfx->ProjectionX("hTISDT04D4best_RMS","C=E");
  h2pfxes->Draw("");
- h2pfxes->GetYaxis()->SetTitle("RMS(#Deltat) [ps]");
+ h2pfxes->GetYaxis()->SetTitle("RMS(#Deltat) [ns]");
+
+ TString FADD=Form("pl_Datime(\"%s\")",sysinfo.Data());
+ gInterpreter->ProcessLine(FADD.Data());
 
  can->SaveAs(Form("pl_TIS.pdf"));
 }
