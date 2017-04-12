0c063f7c2fe6b226480bfddc3810986e41aabb4f
v.friese <v.friese@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2017-04-12 06:59:46 +0000

    Update of CbmTofPoint to cope with the creation of only one ToFPoint per track (C. Simon)

--- data/tof/CbmTofPoint.cxx
+++ data/tof/CbmTofPoint.cxx
@@ -1,16 +1,24 @@
+/** @file CbmTofPoint.cxx
+ ** @author Volker Friese <v.friese@gsi.de>
+ ** @author Christian Simon <c.simon@physi.uni-heidelberg.de>
+ ** @since 16.06.2014
+ ** @date 11.04.2017
+ **/
 
-// -------------------------------------------------------------------------
-// -----                      CbmTofPoint source file                  -----
-// -----                   Created  28/07/04  by V. Friese             -----
-// -----                   Modified 30/08/06  by D. Gonzalez           -----
-// -------------------------------------------------------------------------
 
 #include "CbmTofPoint.h"
 
-#include "FairLogger.h"
+#include <cassert>
+#include <limits>
+#include <sstream>
+
+using std::endl;
+using std::string;
+using std::stringstream;
+
 
 // -----   Default constructor   -------------------------------------------
-CbmTofPoint::CbmTofPoint() : FairMCPoint() { }
+CbmTofPoint::CbmTofPoint() : FairMCPoint(), fNofCells(0), fGapMask(0) { }
 // -------------------------------------------------------------------------
 
 
@@ -19,7 +27,8 @@ CbmTofPoint::CbmTofPoint() : FairMCPoint() { }
 CbmTofPoint::CbmTofPoint(Int_t trackID, Int_t detID, TVector3 pos, 
 			 TVector3 mom, Double_t tof, Double_t length, 
 			 Double_t eLoss)
-  : FairMCPoint(trackID, detID, pos, mom, tof, length, eLoss) { }
+  : FairMCPoint(trackID, detID, pos, mom, tof, length, eLoss),
+    fNofCells(0), fGapMask(0) { }
 // -------------------------------------------------------------------------
 
 
@@ -28,17 +37,87 @@ CbmTofPoint::~CbmTofPoint() { }
 // -------------------------------------------------------------------------
 
 
+// -----   Get the number of gaps   ----------------------------------------
+Int_t CbmTofPoint::GetNGaps() const
+{
+  Int_t iNGaps(0);
 
-// -----   Public method Print   -------------------------------------------
-void CbmTofPoint::Print(const Option_t* /*opt*/) const {
-  LOG(INFO) << "TOF point for track " << fTrackID 
-       << " in detector " << fDetectorID << FairLogger::endl;
-  LOG(INFO) << "    Position (" << fX << ", " << fY << ", " << fZ
-       << ") cm" << FairLogger::endl;
-  LOG(INFO) << "    Momentum (" << fPx << ", " << fPy << ", " << fPz
-       << ") GeV" << FairLogger::endl;
-  LOG(INFO) << "    Time " << fTime << " ns,  Length " << fLength 
-       << " cm,  Energy loss " << fELoss*1.0e06 << " keV" << FairLogger::endl;
+  for( Int_t iGapBit = 0; iGapBit < std::numeric_limits<UShort_t>::digits;
+       iGapBit++ )
+  {
+    if( fGapMask & ( 0x1 << iGapBit ) )
+    {
+      iNGaps++;
+    }
+  }
+
+  return iNGaps;
 }
+// -------------------------------------------------------------------------
+
+
+// -----   Get the index of the first gap   --------------------------------
+Int_t CbmTofPoint::GetFirstGap() const
+{
+  for( Int_t iGapBit = 0; iGapBit < std::numeric_limits<UShort_t>::digits;
+       iGapBit++ )
+  {
+    if( fGapMask & ( 0x1 << iGapBit ) )
+    {
+      return iGapBit;
+    }
+  }
+
+  return -1;
+}
+// -------------------------------------------------------------------------
+
+
+// -----   Get the index of the last gap   ---------------------------------
+Int_t CbmTofPoint::GetLastGap() const
+{
+  Int_t iLastGap(-1);
+
+  for( Int_t iGapBit = 0; iGapBit < std::numeric_limits<UShort_t>::digits;
+       iGapBit++ )
+  {
+    if( fGapMask & ( 0x1 << iGapBit ) )
+    {
+      iLastGap = iGapBit;
+    }
+  }
+
+  return iLastGap;
+}
+// -------------------------------------------------------------------------
+
+
+// -----   Add one gap to the gap mask   -----------------------------------
+void CbmTofPoint::SetGap(Int_t iGap)
+{
+  assert( 0 <= iGap && std::numeric_limits<UShort_t>::digits > iGap );
+  fGapMask |= 0x1 << iGap;
+}
+// -------------------------------------------------------------------------
+
+
+// -----   String output   -------------------------------------------------
+string CbmTofPoint::ToString() const
+{
+   stringstream ss;
+   ss << "STofPoint: track ID " << fTrackID << ", detector ID "
+      << fDetectorID << "\n";
+   ss << "    Position (" << fX << ", " << fY << ", " << fZ
+      << ") cm \n";
+   ss << "    Momentum (" << fPx << ", " << fPy << ", " << fPz
+      << ") GeV \n";
+   ss << "    Time " << fTime << " ns,  Length " << fLength
+       << " cm,  Energy loss " << fELoss*1.0e06 << " keV \n";
+   ss << "    Number of cells " << fNofCells << ", gap mask "
+      << std::bitset<std::numeric_limits<UShort_t>::digits>(fGapMask)
+      << endl;
+   return ss.str();
+}
+// -------------------------------------------------------------------------
 
 ClassImp(CbmTofPoint)
--- data/tof/CbmTofPoint.h
+++ data/tof/CbmTofPoint.h
@@ -1,58 +1,132 @@
-// -------------------------------------------------------------------------
-// -----                     CbmTofPoint header file                   -----
-// -----                 Created 28/07/04  by V. Friese                -----
-// -------------------------------------------------------------------------
-// modified 23/10/2012 nh
-//
-/**  CbmTofPoint.h
- *@author V. Friese
- **
- ** Interception of MC track with a TOF detector.
+/** @file CbmTofPoint.h
+ ** @author Volker Friese <v.friese@gsi.de>
+ ** @author Christian Simon <c.simon@physi.uni-heidelberg.de>
+ ** @since 16.06.2014
+ ** @date 11.04.2017
  **/
 
 
 #ifndef CBMTOFPOINT_H
 #define CBMTOFPOINT_H 1
 
-
+#include <string>
 #include "FairMCPoint.h"
 
 class TVector3;
 
+
+/** @class CbmTofPoint
+ ** @brief Geometric intersection of a MC track with a TOFb detector
+ ** @author Volker Friese <v.friese@gsi.de>
+ ** @author Christian Simon <c.simon@physi.uni-heidelberg.de>
+ ** @since 16.06.2014
+ ** @date 11.04.2017
+ ** @version 3.0
+ **
+ ** This data class holds the geometric properties of the intersection
+ ** of a Monte-Carlo track with an active TOF detector element.
+ ** The TOF detector class produces CbmTofPoints in two modes. In the
+ ** first one, a TofPoint is created for each gap the track enters.
+ ** In the second mode, only one TofPoint per track and module is created.
+ ** The space and momentum coordinates then correspond to the respective
+ ** averages over all traversed gaps. In this case, the variables fNofCells
+ ** gives the number of transversal segments the track has touched, and
+ ** fGapMask gives the pattern of traversed gaps.
+ **/
 class CbmTofPoint : public FairMCPoint 
 {
 
  public:
 
-  /** Default constructor **/
+  /** @brief Default constructor **/
   CbmTofPoint();
 
 
-  /** Constructor with arguments
-   *@param trackID  Index of MCTrack
-   *@param detID    Detector ID
-   *@param pos      Ccoordinates at entrance to active volume [cm]
-   *@param mom      Momentum of track at entrance [GeV]
-   *@param tof      Time since event start [ns]
-   *@param length   Track length since creation [cm]
-   *@param eLoss    Energy deposit [GeV]
+  /** @brief Constructor with arguments
+   ** @param trackID  Index of MCTrack
+   ** @param detID    Detector ID
+   ** @param pos      Coordinates at entrance to active volume [cm]
+   ** @param mom      Momentum of track at entrance [GeV]
+   ** @param tof      Time since event start [ns]
+   ** @param length   Track length since creation [cm]
+   ** @param eLoss    Energy deposit [GeV]
    **/
   CbmTofPoint(Int_t trackID, Int_t detID, TVector3 pos, TVector3 mom, 
 	       Double_t tof, Double_t length, Double_t eLoss);
 
 
-  /** Copy constructor **/
- CbmTofPoint(const CbmTofPoint& point) : FairMCPoint(){ *this = point; };
-
-
-  /** Destructor **/
+  /** @brief Destructor **/
   virtual ~CbmTofPoint();
 
 
-  /** Output to screen **/
-  virtual void Print(const Option_t* opt) const;
+  /** @brief Number of traversed cells
+   ** @value Number of traversed cells
+   **/
+  Int_t GetNCells() const { return fNofCells; }
 
-  ClassDef(CbmTofPoint,2)
+
+  /** @brief Number of traversed gaps
+   ** @value Number of traversed gaps
+   **/
+  Int_t GetNGaps() const;
+
+
+  /** @brief Index of first traversed gap
+   ** @value First gap index
+   **/
+  Int_t GetFirstGap() const;
+
+
+  /** @brief Index of last traversed gap
+   ** @value Last gap index
+   **/
+  Int_t GetLastGap() const;
+
+
+  /** @brief Set x component of momentum
+   ** @param pX  x component of momentum [GeV]
+   **/
+  void SetPx(Double_t pX) { fPx = pX; }
+
+
+  /** @brief Set y component of momentum
+   ** @param pY  y component of momentum [GeV]
+   **/
+  void SetPy(Double_t pY) { fPy = pY; }
+
+
+  /** @brief Set z component of momentum
+   ** @param pZ  z component of momentum [GeV]
+   **/
+  void SetPz(Double_t pZ) { fPz = pZ; }
+
+
+  /** @brief Set the number of traversed cells
+   ** @param nCells  Number of traversed cells
+   **/
+  void SetNCells(Int_t nCells) { fNofCells = nCells; }
+
+
+  /** @brief Set a gap in the gap mask
+   ** @param iGap  Gap number
+   **/
+  void SetGap(Int_t iGap);
+
+
+  /** @brief String representation of the object.
+   ** @value String representation of the object.
+   **/
+  virtual std::string ToString() const;
+
+
+
+ private:
+
+  Int_t fNofCells;      /// Number of cells traversed
+  UShort_t fGapMask;    /// Set of gaps traversed
+
+
+  ClassDef(CbmTofPoint,3)
 
 };
 
