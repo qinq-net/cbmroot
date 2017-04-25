036fddb1416f57f2589118381aa44d83c50d31d9
p.-a.loizeau <p.-a.loizeau@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2017-04-25 15:02:23 +0000

    Add files from macro folder for the Heidelberg TOF Cosmics setup

---
+++ macro/beamtime/hd2017/init_calib.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+# shell script to initialize clusterizer calibrations
+cRun=$1
+
+nIter=1
+echo 'Initialize clusterizer calibration for run '$cRun, execute $nIter iterations
+
+iCalSet=$2
+((iTmp  = $iCalSet ))
+((iBRef = $iTmp % 1000))
+((iTmp  = $iTmp - $iBRef))
+((iSet  = $iTmp / 1000))
+((iMRef = $iTmp % 1000000))
+((iMRef = $iMRef / 1000))
+((iTmp  = $iTmp - $iMRef))
+((iDut  = $iTmp / 1000000))
+echo Calib setup is ${iCalSet}, iSet=$iSet, iDut=$iDut, iMRef=$iMRef, iBRef=$iBRef
+#return
+#iSet=0
+#lastOpt=''
+nEvi=100000000
+mkdir ${cRun}
+cp rootlogon.C ${cRun}
+cp .rootrc ${cRun}
+cd ${cRun}
+
+# Global variables, for for-loops. Runs from 1-15 and 16-79 in two for-loops
+iRestart=0
+iStep=0
+iStepLast=0
+# ************************** Starting while Loop ***************************** #
+while [[ $nIter > 0 ]]; do 
+echo start from scratch with $nIter iterations 
+if((0)); then
+echo really start from scratch with $nIter iterations 
+
+for inOpt in ''$nEvi',0,0,0,50' ''$nEvi',03,0,0,50' ''$nEvi',13,0,0,50' ''$nEvi',23,0,0,50' ''$nEvi',23,0,0,50' ''$nEvi',33,0,0,50' ''$nEvi',33,0,0,50'  ''$nEvi',33,0,0,50' ''$nEvi',43,0,0,50' ''$nEvi',43,0,0,50' ''$nEvi',43,0,0,50' ''$nEvi',53,0,0,50' ''$nEvi',53,0,0,50' ''$nEvi',63,0,0,50' ''$nEvi',63,0,0,50'
+do
+
+((iStepLast = ${iStep}))
+((iStep += 1))
+mkdir Init${iStep}
+cp rootlogon.C Init${iStep}
+cp .rootrc Init${iStep}
+cd Init${iStep}
+
+if [[ ${lastOpt:+1} ]] ; then
+# echo last round was done with $lastOpt, extract 2. and 3. word
+i1=`expr index $inOpt , `
+i2=($i1+3)
+#echo `expr index $inOpt , ` = $i1
+cMode=${inOpt:$i1:2}
+cSel=${inOpt:$i2:1}
+echo next iteration: cMode=$cMode, cSel=$cSel 
+if [[ ${cSel} = "-" ]];then 
+    cSel=${inOpt:$i2:2}
+    echo cSel=$cSel 
+    cSel="0"
+fi
+#copy calibration file 
+  if (($iStep > $iRestart)) ; then 
+    cp -v ../Init${iStepLast}/tofTestBeamClust_${cRun}_set${iCalSet}.hst.root ${cRun}_set${iCalSet}_${cMode}_${cSel}tofTestBeamClust.hst.root
+  fi
+fi 
+lastOpt=$inOpt
+
+if (($iStep > $iRestart)) ; then
+  # generate new calibration file
+  root -b -q '../../ana_digi_ini.C('$inOpt',"'${cRun}'",'${iCalSet}','${iBRef}') '
+  #./screenshot.sh
+  cp *pdf ../
+fi
+cd .. 
+done
+
+cp -v  ./Init${iStep}/${cRun}_set${iCalSet}_${cMode}_${cSel}tofTestBeamClust.hst.root ${cRun}_set${iCalSet}_03_0tofTestBeamClust.hst.root 
+ 
+fi
+
+echo execute main loop at $nIter. iteration 
+
+for inOpt in ''$nEvi',0,0,0,50' ''$nEvi',03,0,0,50' ''$nEvi',03,1,'${iMRef}',0' ''$nEvi',03,0,'${iDut}',0' ''$nEvi',03,1,'${iMRef}',0' ''$nEvi',03,0,'${iDut}',0' ''$nEvi',13,0,0,50' ''$nEvi',13,0,0,50' ''$nEvi',13,1,'${iMRef}',0' ''$nEvi',13,0,'${iDut}',0' ''$nEvi',13,1,'${iMRef}',0'  ''$nEvi',13,0,'${iDut}',0' ''$nEvi',23,0,0,50' ''$nEvi',23,0,0,50' ''$nEvi',23,1,'${iMRef}',0'  ''$nEvi',23,0,'${iDut}',0' ''$nEvi',23,1,'${iMRef}',0' ''$nEvi',23,0,'${iDut}',0' ''$nEvi',33,0,0,50' ''$nEvi',33,0,0,50' ''$nEvi',33,1,'${iMRef}',0' ''$nEvi',33,0,'${iDut}',0'  ''$nEvi',33,1,'${iMRef}',0' ''$nEvi',43,0,0,50' ''$nEvi',43,0,0,50' ''$nEvi',43,1,'${iMRef}',0' ''$nEvi',43,0,'${iDut}',0' ''$nEvi',43,1,'${iMRef}',0' ''$nEvi',43,0,'${iDut}',0' ''$nEvi',43,1,'${iMRef}',0' ''$nEvi',43,0,0,50' ''$nEvi',43,0,0,50' ''$nEvi',43,1,'${iMRef}',0' ''$nEvi',43,0,'${iDut}',0' ''$nEvi',43,1,'${iMRef}',0' ''$nEvi',43,0,'${iDut}',0' ''$nEvi',53,0,0,50' ''$nEvi',53,0,0,50' ''$nEvi',53,1,'${iMRef}',0' ''$nEvi',53,0,'${iDut}',0' ''$nEvi',53,1,'${iMRef}',0' ''$nEvi',53,0,'${iDut}',0' '500000,14,0,-'${iDut}',0' '500000,14,1,-'${iMRef}',0' ''$nEvi',53,0,'${iDut}',0' ''$nEvi',53,1,'${iMRef}',0' ''$nEvi',11,0,-'${iDut}',0' ''$nEvi',11,1,-'${iMRef}',0' ''$nEvi',53,0,'${iDut}',0' ''$nEvi',53,1,'${iMRef}',0' ''$nEvi',63,0,'${iDut}',0' ''$nEvi',63,1,'${iMRef}',0' ''$nEvi',63,0,0,50' ''$nEvi',63,0,0,50' ''$nEvi',63,0,'${iDut}',0' ''$nEvi',63,1,'${iMRef}',0' ''$nEvi',21,0,-'${iDut}',0' ''$nEvi',21,1,-'${iMRef}',0' ''$nEvi',63,0,'${iDut}',0' ''$nEvi',63,1,'${iMRef}',0' ''$nEvi',73,0,'${iDut}',0' ''$nEvi',73,1,'${iMRef}',0' ''$nEvi',73,0,0,50' ''$nEvi',73,0,0,50' ''$nEvi',73,0,'${iDut}',0' ''$nEvi',73,1,'${iMRef}',0'  ''$nEvi',31,0,-'${iDut}',0' ''$nEvi',31,1,-'${iMRef}',0' ''$nEvi',73,0,'${iDut}',0' ''$nEvi',73,1,'${iMRef}',0' ''$nEvi',73,0,'${iDut}',0' '500000,24,0,-'${iDut}',0' ''$nEvi',73,1,'${iMRef}',0' '500000,24,1,-'${iMRef}',0' ''$nEvi',73,0,0,50' ''$nEvi',73,0,0,50' ''$nEvi',73,0,'${iDut}',0' ''$nEvi',73,1,'${iMRef}',0' ''$nEvi',73,0,0,50' ''$nEvi',73,0,0,50' ''$nEvi',73,0,'${iDut}',0' ''$nEvi',73,1,'${iMRef}',0' ''$nEvi',83,0,'${iDut}',0'  ''$nEvi',83,1,'${iMRef}',0'  ''$nEvi',83,0,'${iDut}',0'  ''$nEvi',83,1,'${iMRef}',0' '500000,34,0,-'${iDut}',0' ''$nEvi',83,1,'${iMRef}',0' '500000,34,1,-'${iMRef}',0' ''$nEvi',93,0,'${iDut}',0' ''$nEvi',93,1,'${iMRef}',0' ''$nEvi',93,0,'${iDut}',0'  ''$nEvi',93,1,'${iMRef}',0' 
+do   
+
+((iStepLast = ${iStep}))
+((iStep += 1))
+mkdir Init${iStep}
+cp rootlogon.C Init${iStep}
+cp .rootrc Init${iStep}
+cd Init${iStep}
+
+if [[ ${lastOpt:+1} ]] ; then
+# echo last round was done with $lastOpt, extract 2. and 3. word
+i1=`expr index $inOpt , `
+i2=($i1+3)
+#echo `expr index $inOpt , ` = $i1
+cMode=${inOpt:$i1:2}
+cSel=${inOpt:$i2:1}
+echo next iteration: cMode=$cMode, cSel=$cSel 
+if [[ ${cSel} = "-" ]];then 
+    cSel=${inOpt:$i2:2}
+    echo cSel=$cSel 
+    cSel="0"
+fi
+#copy calibration file 
+if (($iStep > $iRestart)) ; then
+  cp -v ../Init${iStepLast}/tofTestBeamClust_${cRun}_set${iCalSet}.hst.root ${cRun}_set${iCalSet}_${cMode}_${cSel}tofTestBeamClust.hst.root
+fi
+fi 
+
+lastOpt=$inOpt
+# generate new calibration file
+if (($iStep > $iRestart)) ; then 
+  root -b -q '../../ana_digi_cal.C('$inOpt',"'${cRun}'",'${iCalSet}','${iBRef}') '
+
+  cp -v tofTestBeamClust_${cRun}_set${iCalSet}.hst.root ../${cRun}_set${iCalSet}_${cMode}_${cSel}tofTestBeamClust.hst.root
+  cp *pdf ../
+#./screenshot.sh
+  cd .. 
+  rm ../${cRun}_set${iCalSet}_${cMode}_${cSel}tofTestBeamClust.hst.root
+  ln -s ./${cRun}/${cRun}_set${iCalSet}_${cMode}_${cSel}tofTestBeamClust.hst.root ../${cRun}_set${iCalSet}_${cMode}_${cSel}tofTestBeamClust.hst.root
+echo Init step $iStep with mode ${cMode}, option $inOpt  finished
+else 
+  cd ..
+  echo Init step $iStep with mode ${cMode}, option $inOpt  skipped
+fi
+done
+(( nIter -= 1))
+done 
+#
---
+++ macro/beamtime/hd2017/unpack_tsa.C
@@ -0,0 +1,149 @@
+/** @file FHodoLabSetup
+ ** @author Florian Uhlig <f.uhlig@gsi.de>
+ ** @date 20.06.2016
+ **
+ ** ROOT macro to read tsa files which have been produced with the new data transport
+ ** Convert data into cbmroot format.
+ ** Uses CbmFlibTestSource as source task.
+ */
+
+//void unpack_tsa(Int_t nEvt=100, TString FileId = "cosmic_2016110701_safe_4links_4")
+void unpack_tsa(Int_t nEvt=10000, Double_t dDeltaT=50., Int_t iReqDet=2, Bool_t bEpSupp=kTRUE, TString FileId = "r0002_20170413_0756")
+{
+  TString srcDir = gSystem->Getenv("VMCWORKDIR");
+  //  TString inDir  = "./input/";
+  TString inDir  = "./input/" + FileId + "/";
+  //  TString inDir  = "/lustre/nyx/cbm/prod/beamtime/2016/11/cern/phase1/";
+  //  inFile = inDir + FileId + ".tsa"; 
+  // for Phase2 data
+  //TString inDir  = "/lustre/nyx/cbm/prod/beamtime/2016/11/cern/phase2/" + FileId;
+  //TString inDir  = "/lustre/nyx/cbm/prod/beamtime/2016/11/cern/phase3/" + FileId;
+  TString inFile = "*.tsa";
+
+  // --- Specify number of events to be produced.
+  // --- -1 means run until the end of the input file.
+  Int_t bEvt=-1;
+  Int_t nEvents = 10;
+  if(nEvt>0) { 
+   bEvt=0;
+   nEvents = nEvt;
+  }
+  // --- Specify output file name (this is just an example)
+  TString outDir = srcDir + "/macro/beamtime/hd2017/data/";
+  TString Mode = Form("_DT%d_Req%d",(Int_t)dDeltaT,iReqDet);
+  TString outFile = outDir + FileId + Mode + ".root";
+  TString parFile = outDir + FileId + Mode + ".params.root";
+
+  // --- Set log output levels
+  FairLogger::GetLogger();
+  gLogger->SetLogScreenLevel("INFO");
+  //gLogger->SetLogScreenLevel("DEBUG");
+  gLogger->SetLogVerbosityLevel("MEDIUM");
+
+  // --- Define parameter files
+  TList *parFileList = new TList();
+  TString paramDir = "./";
+  /*
+  TString paramFile = paramDir + "FHodoUnpackPar.par";
+  TObjString* tutDetDigiFile = new TObjString(paramFile);
+  parFileList->Add(tutDetDigiFile);
+  */
+  TString paramFileTof = paramDir + "MapTofHdCosmics2017.par";
+  TObjString* parTofFileName = new TObjString(paramFileTof);
+  parFileList->Add(parTofFileName);
+
+  // --- Set debug level
+  gDebug = 0;
+  
+  std::cout << std::endl;
+  std::cout << ">>> unpack_tsa: output file is " << outFile << std::endl;
+
+  // ========================================================================
+  // ========================================================================
+  std::cout << std::endl;
+  std::cout << ">>> unpack_tsa: Initialising..." << std::endl;
+
+
+  // NXyter Unpacker
+  CbmTSUnpackFHodo*   test_unpacker = new CbmTSUnpackFHodo();
+  //  test_unpacker->CreateRawMessageOutput(kTRUE);
+  
+  // Get4 Unpacker
+  CbmTSUnpackTof* test_unpacker_tof = new CbmTSUnpackTof(7);  //argument is number of gDpb
+  test_unpacker_tof->SetEpochSuppressedMode(bEpSupp);
+
+  // --- Source task
+  CbmFlibTestSource* source = new CbmFlibTestSource();
+  source->SetMaxDeltaT(dDeltaT);
+  switch(iReqDet){
+    case 0:                               // no request 
+      break;
+    case 1:
+      source->SetReqDigiAddr(0x00019006);  // request USTC 1
+      break;
+    case 2:
+      source->AddReqDigiAddr(0x00019006);  // request USTC 1 for output events
+      source->AddReqDigiAddr(0x00019026);  // request THU  1 for output events
+      break;
+    case 3:
+      source->AddReqDigiAddr(0x00006006);  // request BUC 0 for output events
+      source->AddReqDigiAddr(0x00016006);  // request BUC 1 for output events
+      break;
+    default:
+      std::cout<<"Request mode not implemented"<<std::endl;
+      return;
+  }
+  //source->SetFileName(inFile);
+  source->AddPath(inDir,inFile);
+  source->AddUnpacker(test_unpacker_tof, 0x60, 6);//gDPB A & B
+  //source->AddUnpacker(test_unpacker,     0x10, 10);//nDPB A & B = HODO 1 + 2
+
+  // --- Event header
+  FairEventHeader* event = new CbmTbEvent();
+  event->SetRunId(1);
+
+  // --- Run
+  FairRunOnline *run = new FairRunOnline(source);
+  run->SetOutputFile(outFile);
+  //  run->SetEventHeader(event);
+
+  FairRuntimeDb* rtdb = run->GetRuntimeDb();
+
+  // -----   Runtime database   ---------------------------------------------
+  Bool_t kParameterMerged = kTRUE;
+  FairParRootFileIo* parOut = new FairParRootFileIo(kParameterMerged);
+  FairParAsciiFileIo* parIn = new FairParAsciiFileIo();
+  parOut->open(parFile.Data());
+  parIn->open(parFileList, "in");
+  rtdb->setFirstInput(parIn);
+  rtdb->setOutput(parOut);
+
+  run->Init();
+
+  // --- Start run
+  TStopwatch timer;
+  timer.Start();
+  std::cout << ">>> unpack_tsa: Starting run..." << std::endl;
+  if(bEvt == -1)
+  run->Run(bEvt, 0); // run until end of input file
+  else 
+  run->Run(0, nEvents); // process nEvents
+  
+  timer.Stop();
+  
+  std::cout << "Processed " << std::dec << source->GetTsCount() << " timeslices" << std::endl;
+    
+  // --- End-of-run info
+  Double_t rtime = timer.RealTime();
+  Double_t ctime = timer.CpuTime();
+  std::cout << std::endl << std::endl;
+  std::cout << ">>> unpack_tsa: Macro finished successfully." << std::endl;
+  std::cout << ">>> unpack_tsa: Output file is " << outFile << std::endl;
+  std::cout << ">>> unpack_tsa: Real time " << rtime << " s, CPU time "
+	    << ctime << " s" << std::endl;
+  std::cout << std::endl;
+
+  /// --- Screen output for automatic tests
+  std::cout << " Test passed" << std::endl;
+  std::cout << " All ok " << std::endl;
+}
