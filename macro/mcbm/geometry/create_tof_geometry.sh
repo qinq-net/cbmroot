d6720c498d1fffeb0a8d7f8bc0e9e711a76ce4d1
n.herrmann <n.herrmann@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2017-05-03 08:02:35 +0000

    adjust file names

--- macro/mcbm/geometry/create_tof_geometry.sh
+++ macro/mcbm/geometry/create_tof_geometry.sh
@@ -2,10 +2,10 @@
 VGEO=$1
 
 root -l -q Create_TOF_Geometry_${VGEO}.C
-cp mtof_v14a.geo.root ../../../geometry/tof/tof_${VGEO}_mcbm.geo.root
+cp mtof_v14a.geo.root ../../../geometry/tof/mtof_${VGEO}.root
 cp mtof_v14a.geo.root mtof_${VGEO}.geo.root
 cp mtof_v14a_geo.root mtof_${VGEO}_geo.root
-#root -l -q 'make_geofile_noBfield.C("mtof_'${VGEO}'")'
+root -l -q 'make_geofile_noBfield.C("mtof_'${VGEO}'")'
 #cp geofile_mtof_${VGEO}.root ../../../geometry/tof/
 root -l 'create_tof_digipar.C("mtof_'${VGEO}'")'
 cp mtof_${VGEO}.digi.par ../../../parameters/tof/tof_${VGEO}_mcbm.digi.par
