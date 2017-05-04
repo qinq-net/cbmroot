e61fc84aafa7ffeb25ba08570a107a2d38321ae2
s.lebedev <s.lebedev@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2017-05-04 10:03:50 +0000

    Add possibility to calculate X an Y components of the distance separetly

--- rich/utils/CbmRichUtil.h
+++ rich/utils/CbmRichUtil.h
@@ -20,9 +20,32 @@ public:
     
 	static Double_t GetRingTrackDistance(Int_t globalTrackId)
 	{
-		Double_t errorValue = 999.;
+		vector<Double_t> v = GetRingTrackDistanceImpl(globalTrackId);
+		return v[0];
+	}
+
+	static Double_t GetRingTrackDistanceX(Int_t globalTrackId)
+	{
+		vector<Double_t> v = GetRingTrackDistanceImpl(globalTrackId);
+		return v[1];
+	}
+
+	static Double_t GetRingTrackDistanceY(Int_t globalTrackId)
+	{
+		vector<Double_t> v = GetRingTrackDistanceImpl(globalTrackId);
+		return v[2];
+	}
+
+private:
+
+	/**
+	 * \brief Return a vector with distance ans x, y components. [0] - total distance, [1] - x component, [2] - y component
+	 */
+	static vector<Double_t> GetRingTrackDistanceImpl(Int_t globalTrackId)
+	{
+		vector<Double_t> errorVec = {999., 999., 999.};
 		FairRootManager* ioman = FairRootManager::Instance();
-		if (ioman == NULL) return errorValue;
+		if (ioman == NULL) return errorVec;
 		// Do we really need static here, depends on ioman->GetObject() method
 		static TClonesArray* globalTracks = NULL;
 		static TClonesArray* richRings = NULL;
@@ -39,32 +62,35 @@ public:
 
 		if (globalTracks == NULL || richRings == NULL || richProjections == NULL) {
 			LOG(ERROR) << "CbmRichUtil::GetRingTrackDistance globalTracks, richRings, richProjections NOT INITIALIZED" << endl;
-			return errorValue;
+			return errorVec;
 		}
 
 		const CbmGlobalTrack* globalTrack = static_cast<const CbmGlobalTrack*>(globalTracks->At(globalTrackId));
-		if (globalTrack == NULL) return errorValue;
+		if (globalTrack == NULL) return errorVec;
 
 		Int_t stsId = globalTrack->GetStsTrackIndex();
-		if (stsId < 0) return errorValue;
+		if (stsId < 0) return errorVec;
 
 		const FairTrackParam* pTrack = static_cast<const FairTrackParam*>(richProjections->At(stsId));
-		if (pTrack == NULL) return errorValue;
+		if (pTrack == NULL) return errorVec;
 
-		if (pTrack->GetX() == 0 && pTrack->GetY() == 0) return errorValue;
+		if (pTrack->GetX() == 0 && pTrack->GetY() == 0) return errorVec;
 
 		Int_t richId = globalTrack->GetRichRingIndex();
-		if (richId < 0) return errorValue;
+		if (richId < 0) return errorVec;
 
 		const CbmRichRing* richRing = static_cast<const CbmRichRing*>(richRings->At(richId));
-		if (richRing == NULL) return errorValue;
+		if (richRing == NULL) return errorVec;
 
 		Double_t xRing = richRing->GetCenterX();
 		Double_t yRing = richRing->GetCenterY();
-		Double_t dist = TMath::Sqrt( (richRing->GetCenterX() - pTrack->GetX()) * (richRing->GetCenterX() - pTrack->GetX()) +
-				(richRing->GetCenterY() - pTrack->GetY()) * (richRing->GetCenterY() - pTrack->GetY()) );
+		Double_t dx =  richRing->GetCenterX() - pTrack->GetX();
+		Double_t dy = richRing->GetCenterY() - pTrack->GetY();
 
-		return dist;
+		Double_t dist = TMath::Sqrt( dx * dx + dy * dy );
+
+		vector<Double_t> v = {dist, dx, dy};
+		return v;
 	}
     
 };
