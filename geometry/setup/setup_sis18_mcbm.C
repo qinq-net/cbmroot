97e34a45b3b27c9ecf329e5da26262c508cd4327
d.emschermann <d.emschermann@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2017-05-08 11:35:12 +0000

    use z-aligned TRD v18g, shifted downstream by 5 cm to avoid beampipe overlaps

--- geometry/setup/setup_sis18_mcbm.C
+++ geometry/setup/setup_sis18_mcbm.C
@@ -22,7 +22,7 @@ void setup_sis18_mcbm()
   TString mvdGeoTag       = "v18b_mcbm";    // "v18a_mcbm";						  
   TString stsGeoTag       = "v18e_mcbm";    // "v18d_mcbm";						  
   TString muchGeoTag      = "v18a_mcbm";
-  TString trdGeoTag       = "v18e_1e_mcbm"; // "v18d_1e_mcbm"; // "v18c_1e_mcbm";  // "v18b_1e_mcbm";  // "v18a_1e_mcbm";
+  TString trdGeoTag       = "v18g_1e_mcbm"; // "v18e_1e_mcbm"; // "v18d_1e_mcbm"; // "v18c_1e_mcbm";  // "v18b_1e_mcbm";  // "v18a_1e_mcbm";
   TString tofGeoTag       = "v13_6a_mcbm";  // "v13-7a_mcbm";  // "v13_5a_mcbm";
   // ------------------------------------------------------------------------
 
