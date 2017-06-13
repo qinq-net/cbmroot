a02925287d443a5c1cba7706d7f7ccff9d252b1a
d.emschermann <d.emschermann@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2017-06-13 08:09:03 +0000

    patch for TRD v17o

--- trd/tools/CbmTrdUtils.cxx
+++ trd/tools/CbmTrdUtils.cxx
@@ -224,8 +224,9 @@ Int_t CbmTrdUtils::GetModuleType(Int_t moduleAddress, CbmTrdModule *fModuleInfo,
   {
     if      (nRows == 24)   type = 5;  // v17a - not used
     else if (nRows == 16)   type = 6;  // v17a
+    else if (nRows == 12)   type = 6;  // + v17o
     else if (nRows ==  8)   type = 7;  // v17a
-    else if (nRows ==  4)   type = 8;  // v17a
+    else if (nRows ==  4)   type = 8;  // v17a + v17o
   }
   else if (nCols == 144)   // FEB-9x1
   {
