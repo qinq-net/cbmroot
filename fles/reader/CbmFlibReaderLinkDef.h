b3fda6bf55ae268651f84e4fb638b4d5ed92dfb9
p.munkes <p.munkes@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2017-10-09 17:05:55 +0000

    Fix build errors caused by new paths in external and by the spadic 2.0 unpackers.

--- fles/reader/CbmFlibReaderLinkDef.h
+++ fles/reader/CbmFlibReaderLinkDef.h
@@ -10,10 +10,10 @@
 
 //#pragma link C++ class CbmTSUnpack;
 #pragma link C++ class CbmTSUnpackSpadic;
-#pragma link C++ class CbmTSUnpackSpadic20;
+//#pragma link C++ class CbmTSUnpackSpadic20;
 #pragma link C++ class CbmTSUnpackSpadic11OnlineMonitor;
-#pragma link C++ class CbmTSUnpackSpadic20OnlineMonitor;
-#pragma link C++ class CbmTSUnpackSpadic20DesyOnlineMonitor;
+//#pragma link C++ class CbmTSUnpackSpadic20OnlineMonitor;
+//#pragma link C++ class CbmTSUnpackSpadic20DesyOnlineMonitor;
 #pragma link C++ class CbmTSUnpackSpadicLegacy;
 #pragma link C++ class CbmTSUnpackNxyter;
 #pragma link C++ class CbmTSUnpackStsxyter;
