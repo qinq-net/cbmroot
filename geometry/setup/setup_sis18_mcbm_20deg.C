8737902768ff96f560777e19b4f7367b7d1dcd56
d.emschermann <d.emschermann@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2017-10-16 12:07:04 +0000

    add mCBM setup file for 20 degrees, refs #997

---
+++ geometry/setup/setup_sis18_mcbm_20deg.C
@@ -0,0 +1,65 @@
+/** ROOT macro to define the CBM setup sis18_mcbm
+ **
+ ** @author Volker Friese <v.friese@gsi.de>
+ ** @author David Emschermann <d.emschermann@gsi.de>
+ ** @date   5 February 2016
+ **
+ **/
+
+// 2017-10-16 - DE - use 20 degree beampipe
+// 2017-06-03 - DE - add RICH v18a_mcbm
+// 2017-05-02 - DE - switch back to 5x5 RPC TOF
+// 2017-05-02 - DE - skip MVD in the initial setup
+// 2016-02-05 - VF - Replaces former sis18_mcbm_setup.C,
+//                   now using the CbmSetup class.
+// 2015-06-18 - DE - mCBM @ SIS18 setup
+
+
+
+void setup_sis18_mcbm_20deg()
+{
+  
+  // -----  Geometry Tags  --------------------------------------------------
+  TString pipeGeoTag      = "v18f_mcbm";    // 20 degree beampipe
+  TString mvdGeoTag       = "v18b_mcbm";    // "v18a_mcbm";						  
+  TString stsGeoTag       = "v18f_mcbm";    // "v18e_mcbm"; // "v18d_mcbm";
+  TString muchGeoTag      = "v18a_mcbm";
+  TString trdGeoTag       = "v18i_1e_mcbm"; // "v18e_1e_mcbm";
+  TString tofGeoTag       = "v18f_mcbm";    // "v18g_mcbm";
+  TString richGeoTag      = "v18b_mcbm";
+  TString platGeoTag      = "v18a_mcbm";
+  // ------------------------------------------------------------------------
+
+
+  // -----  Magnetic field  -------------------------------------------------
+  TString fieldTag      = "v12b";
+  Double_t fieldZ       = 40.;            // field centre z position
+  Double_t fieldScale   =  0.;            // field scaling factor
+  // ------------------------------------------------------------------------
+
+
+  // -----  Create setup  ---------------------------------------------------
+  CbmSetup* setup = CbmSetup::Instance();
+  if ( ! setup->IsEmpty() ) {
+  	std::cout << "-W- setup_sis18_mcbm: overwriting existing setup"
+  			      << setup->GetTitle() << std::endl;
+  	setup->Clear();
+  }
+  setup->SetTitle("SIS18 - MCBM Setup");
+  setup->SetModule(kPipe, pipeGeoTag);
+  setup->SetModule(kPlatform, platGeoTag);
+  //  setup->SetModule(kMvd, mvdGeoTag);  // skip mvd in the initial setup
+  setup->SetModule(kSts, stsGeoTag);
+  setup->SetModule(kMuch, muchGeoTag);
+  setup->SetModule(kTrd, trdGeoTag);
+  setup->SetModule(kTof, tofGeoTag);
+  setup->SetModule(kRich, richGeoTag);
+  setup->SetField(fieldTag, fieldScale, 0., 0., fieldZ);
+  // ------------------------------------------------------------------------
+
+
+  // -----   Screen output   ------------------------------------------------
+  setup->Print();
+  // ------------------------------------------------------------------------
+
+}
