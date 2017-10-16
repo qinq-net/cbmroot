298d9fa061d1e611373e7ae7feea6552167f2920
d.emschermann <d.emschermann@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2017-10-16 12:01:35 +0000

    add mBUCH in mTRD_v18e to mCBM

--- geometry/setup/setup_sis18_mcbm.C
+++ geometry/setup/setup_sis18_mcbm.C
@@ -23,7 +23,7 @@ void setup_sis18_mcbm()
   TString mvdGeoTag       = "v18b_mcbm";    // "v18a_mcbm";						  
   TString stsGeoTag       = "v18f_mcbm";    // "v18e_mcbm"; // "v18d_mcbm";
   TString muchGeoTag      = "v18a_mcbm";
-  TString trdGeoTag       = "v18e_1e_mcbm"; // "v18g_1e_mcbm"; // "v18d_1e_mcbm"; // "v18c_1e_mcbm";  // "v18b_1e_mcbm";  // "v18a_1e_mcbm";
+  TString trdGeoTag       = "v18i_1e_mcbm"; // "v18e_1e_mcbm";
   TString tofGeoTag       = "v18f_mcbm";    // "v18g_mcbm";
   TString richGeoTag      = "v18b_mcbm";
   TString platGeoTag      = "v18a_mcbm";
