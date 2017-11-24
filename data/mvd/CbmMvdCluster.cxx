4f291104126d9bf8e45688feff30a64c0f068759
f.uhlig <f.uhlig@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2017-11-24 13:05:18 +0000

    Remove several data members and use memebers from the base class.

--- data/mvd/CbmMvdCluster.cxx
+++ data/mvd/CbmMvdCluster.cxx
@@ -12,62 +12,24 @@ using std::map;
 // -----   Default constructor   -------------------------------------------
 CbmMvdCluster::CbmMvdCluster() 
  : CbmCluster(),
-   fDigiArray(),
    fPixelMap(),
-   fNeighbourDown(-1),
-   fNeighbourUp(-1),
-   fDigisInThisObject(0),
-   fTotalDigisInCluster(-1),
-   fStation(-1),
    fRefId(-1),
-   fDetectorId(-1),
    fClusterCharge(0)
 {
-  for(Int_t i=0;i<fMaxDigisInObject; i++){fDigiArray[i]=-1;}
-}
-// -------------------------------------------------------------------------
 
-// -----   Standard constructor   ------------------------------------------
-CbmMvdCluster::CbmMvdCluster(Int_t* digiList, Short_t digisInThisObject, Short_t totalDigisInCluster, Int_t neighbourDown) 
- : CbmCluster(),
-   fDigiArray(),
-   fPixelMap(),
-   fNeighbourDown(neighbourDown),
-   fNeighbourUp(-1),
-   fDigisInThisObject(digisInThisObject),
-   fTotalDigisInCluster(totalDigisInCluster),
-   fStation(-1),
-   fRefId(-1),
-   fDetectorId(-1),
-   fClusterCharge(0)
-{
-    for(Int_t i=0;i<fMaxDigisInObject; i++){fDigiArray[i]=-1;}
-    for(Int_t i=0;i<digisInThisObject; i++){fDigiArray[i]=digiList[i];}   
 }
 // -------------------------------------------------------------------------
 
 // -------------------------------------------------------------------------
 CbmMvdCluster::CbmMvdCluster(const CbmMvdCluster& rhs)
  : CbmCluster(),
-   fDigiArray(),
    fPixelMap(),
-   fNeighbourDown(-1),
-   fNeighbourUp(-1),
-   fDigisInThisObject(0),
-   fTotalDigisInCluster(-1),
-   fStation(-1),
    fRefId(-1),
-   fDetectorId(-1),
    fClusterCharge(0) 
 { 
 	fPixelMap = rhs.fPixelMap;
-        fNeighbourDown = rhs.fNeighbourDown;
-        fNeighbourUp = rhs.fNeighbourUp;
-        fDigisInThisObject = rhs.fDigisInThisObject;
-        fTotalDigisInCluster = rhs.fTotalDigisInCluster;
-        fStation = rhs.fStation;
-        fRefId = rhs.fRefId;
-        fDetectorId = rhs.fDetectorId;
+	fRefId = rhs.fRefId;
+        fClusterCharge = rhs.fClusterCharge;
 }
 // -------------------------------------------------------------------------
 
--- data/mvd/CbmMvdCluster.h
+++ data/mvd/CbmMvdCluster.h
@@ -19,66 +19,45 @@
 #include "TObject.h"
 #include "CbmCluster.h"
 #include "FairLogger.h"
+#include "CbmMvdDetectorId.h"
 #include <map>
 
-class CbmMvdCluster : public CbmCluster
-{
 
+
+class CbmMvdCluster : public CbmCluster, public CbmMvdDetectorId
+{
  public:    
 
   /** Default constructor **/
   CbmMvdCluster();
 
-  CbmMvdCluster(Int_t* digiList, Short_t digisInThisObject, Short_t totalDigisInCluster, Int_t neighbourDown); 
+  CbmMvdCluster(const CbmMvdCluster&);
 
-
-CbmMvdCluster(const CbmMvdCluster&);
-
-CbmMvdCluster& operator=(const CbmMvdCluster&){return *this;};
+  CbmMvdCluster& operator=(const CbmMvdCluster&){return *this;};
 
   /** Destructor **/
   virtual ~CbmMvdCluster();
  
   /** Setters **/
-  void SetNeighbourUp(Int_t index){fNeighbourUp=index;};
-  void SetNeighbourDown(Int_t index){fNeighbourDown=index;};
   void SetPixelMap(std::map <std::pair<Int_t, Int_t>, Int_t > PixelMap); 
-  void SetStationNr(Int_t stationNr){fStation = stationNr;};
   void SetRefId(Int_t RefId){fRefId = RefId;}; //* stores the index to the global TClonesArray	
-  void SetDetectorId(Int_t detId)      { fDetectorId = detId;};
-  void SetSensorNr(Int_t SensorNr){fSensorNr = SensorNr;};
- 
 
  /** Accessors **/
-  Int_t   GetNeighbourDown(){return fNeighbourDown;};
-  Int_t   GetNeighbourUp(){return fNeighbourUp;};
-  Short_t GetDigisInThisObject(){return fDigisInThisObject;};
   Short_t GetTotalDigisInCluster(){return fPixelMap.size();};
-  Short_t GetMaxDigisInThisObject(){return fMaxDigisInObject;};
   std::map <std::pair<Int_t, Int_t>, Int_t > GetPixelMap(){return fPixelMap;};
-  Int_t   GetStationNr() {return fStation;};
-  Int_t*  GetDigiList(){return fDigiArray;};
+  Int_t   GetStationNr() {return (Int_t) GetAddress()/1000;};
   Int_t   GetRefId(){return fRefId;};
-  Int_t   GetDetectorId() {return fDetectorId;};
-  Int_t   GetSensorNr(){return fSensorNr;};
+  Int_t   GetDetectorId() {return DetectorId(GetStationNr());};
+  Int_t   GetSensorNr(){return GetAddress()- ( ( (Int_t) GetAddress()/1000) * 1000 ) ;};
 
   Float_t GetClusterCharge(){return fClusterCharge;};
    
  protected:
-     static const Short_t fMaxDigisInObject=8;
-     Int_t fDigiArray[fMaxDigisInObject];
-     std::map <std::pair<Int_t, Int_t>, Int_t > fPixelMap;
-     Int_t fNeighbourDown;
-     Int_t fNeighbourUp;
-     Short_t fDigisInThisObject;
-     Short_t fTotalDigisInCluster;
-     Int_t fStation;
-     Int_t fRefId;
-     Int_t fDetectorId;
-     Int_t fSensorNr;
-     Float_t fClusterCharge;
+    std::map <std::pair<Int_t, Int_t>, Int_t > fPixelMap;
+    Int_t fRefId;
+    Float_t fClusterCharge;
 
-  ClassDef(CbmMvdCluster,3);
+  ClassDef(CbmMvdCluster,4);
 
 };
 
