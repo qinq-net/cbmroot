7f365ae1c188c494578ce2b9e1788be06cf185c4
d.emschermann <d.emschermann@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2017-11-27 12:47:58 +0000

    Extend TRD basic functionality for TRD chamber with triangular pads, by Alexandru Bercuci, refs #1002

--- data/trd/CbmTrdAddress.h
+++ data/trd/CbmTrdAddress.h
@@ -5,6 +5,7 @@
  * \brief Helper class to convert unique channel ID back and forth.
  *
  * Modified 06/05/2013 by Andrey Lebedev <andrey.lebedev@gsi.de>
+ * Modified 18/04/2017 by Alex Bercuci <abercuci@niham.nipne.ro>
  *
  *                                 3         2         1          shift length
  * Current definition:            10987654321098765432109876543210
@@ -12,8 +13,9 @@
  * Layer ID on bits 4-7           00000000000000000000000011110000 <<4  15
  * Module ID on bits 8-14         00000000000000000111111100000000 <<8  127
  * Sector ID on bits 15-16        00000000000000011000000000000000 <<15 3
- * Row ID on bits 17-23           00000000111111100000000000000000 <<17 127
- * Column ID on bits 24-31        11111111000000000000000000000000 <<24 255
+ * Row ID on bits 17-22           00000000011111100000000000000000 <<17 63
+ * Column ID on bits 23-30        01111111100000000000000000000000 <<23 255
+ * Triangle up/down on bits 31    10000000000000000000000000000000 <<31 1
  *
  **/
 
@@ -34,18 +36,20 @@ public:
    * \param[in] sectorId Sector ID.
    * \param[in] rowId Row ID.
    * \param[in] columnId Column ID.
-   * \return Address from system ID, layer, module, sector, row and column IDs.
+   * \param[in] triangId Triangle ID.
+   * \return Address from system ID, layer, module, sector, row, column and triangle IDs.
    **/
    static UInt_t GetAddress(Int_t layerId,
                            Int_t moduleId,
                            Int_t sectorId,
                            Int_t rowId,
-                           Int_t columnId) {
+                           Int_t columnId,
+                           Int_t triangId=0) {
       assert(!(layerId < 0 || layerId > fgkLayerIdLength || moduleId < 0 || moduleId > fgkModuleIdLength
             || sectorId < 0 || sectorId > fgkSectorIdLength || rowId < 0 || rowId > fgkRowIdLength
-            || columnId < 0 || columnId > fgkColumnIdLength));
+            || columnId < 0 || columnId > fgkColumnIdLength || triangId < 0 || triangId > fgkTriangleIdLength));
       return (kTrd << fgkSystemIdShift) | (layerId << fgkLayerIdShift) | (moduleId << fgkModuleIdShift) |
-            (sectorId << fgkSectorIdShift) | (rowId << fgkRowIdShift) | (columnId << fgkColumnIdShift);
+            (sectorId << fgkSectorIdShift) | (rowId << fgkRowIdShift) | (columnId << fgkColumnIdShift) | (triangId << fgkTriangleIdShift);
     }
 
    /**
@@ -103,6 +107,15 @@ public:
    }
 
    /**
+   * \brief Return triangle ID from address.
+   * \param[in] address Unique channel address.
+   * \return Triangle ID from address.
+   **/
+   static UInt_t GetTriangleId(UInt_t address) {
+      return (address & (fgkTriangleIdLength << fgkTriangleIdShift)) >> fgkTriangleIdShift;
+   }
+
+   /**
    * \brief Return unique module ID from address.
    * \param[in] address Unique channel address.
    * \return Unique module ID from address.
@@ -128,7 +141,7 @@ public:
     */
    static UInt_t SetLayerId(UInt_t address, Int_t newLayerId) {
       assert(!(newLayerId < 0 || newLayerId > fgkLayerIdLength));
-      return GetAddress(newLayerId, GetModuleId(address), GetSectorId(address), GetRowId(address), GetColumnId(address));
+      return GetAddress(newLayerId, GetModuleId(address), GetSectorId(address), GetRowId(address), GetColumnId(address), GetTriangleId(address));
    }
 
    /**
@@ -139,7 +152,7 @@ public:
     */
    static UInt_t SetModuleId(UInt_t address, Int_t newModuleId) {
       assert(!(newModuleId < 0 || newModuleId > fgkModuleIdLength));
-      return GetAddress(GetLayerId(address), newModuleId, GetSectorId(address), GetRowId(address), GetColumnId(address));
+      return GetAddress(GetLayerId(address), newModuleId, GetSectorId(address), GetRowId(address), GetColumnId(address), GetTriangleId(address));
    }
 
    /**
@@ -150,7 +163,7 @@ public:
     */
    static UInt_t SetSectorId(UInt_t address, Int_t newSectorId) {
       assert(!(newSectorId < 0 || newSectorId > fgkSectorIdLength));
-      return GetAddress(GetLayerId(address), GetModuleId(address), newSectorId, GetRowId(address), GetColumnId(address));
+      return GetAddress(GetLayerId(address), GetModuleId(address), newSectorId, GetRowId(address), GetColumnId(address), GetTriangleId(address));
    }
 
    /**
@@ -161,7 +174,7 @@ public:
     */
    static UInt_t SetRowId(UInt_t address, Int_t newRowId) {
       assert(!(newRowId < 0 || newRowId > fgkRowIdLength));
-      return GetAddress(GetLayerId(address), GetModuleId(address), GetSectorId(address), newRowId, GetColumnId(address));
+      return GetAddress(GetLayerId(address), GetModuleId(address), GetSectorId(address), newRowId, GetColumnId(address), GetTriangleId(address));
    }
 
    /**
@@ -172,7 +185,18 @@ public:
     */
    static UInt_t SetColumnId(UInt_t address, Int_t newColumnId) {
       assert(!(newColumnId < 0 || newColumnId > fgkColumnIdLength));
-      return GetAddress(GetLayerId(address), GetModuleId(address), GetSectorId(address), GetRowId(address), newColumnId);
+      return GetAddress(GetLayerId(address), GetModuleId(address), GetSectorId(address), GetRowId(address), newColumnId, GetTriangleId(address));
+   }
+
+   /**
+    * \brief Set new triangle ID for address.
+    * \param address Current address.
+    * \param newTriangId New value for triangle ID.
+    * \return New address with modified triangle ID.
+    */
+   static UInt_t SetTriangleId(UInt_t address, Int_t newTriangId) {
+      assert(!(newTriangId < 0 || newTriangId > fgkTriangleIdLength));
+      return GetAddress(GetLayerId(address), GetModuleId(address), GetSectorId(address), GetRowId(address), GetColumnId(address), newTriangId);
    }
 
 private:
@@ -182,15 +206,17 @@ private:
    static const Int_t fgkLayerIdLength       =  15; // 2^4 - 1
    static const Int_t fgkModuleIdLength      = 127; // 2^7 - 1
    static const Int_t fgkSectorIdLength      =   3; // 2^2 - 1
-   static const Int_t fgkRowIdLength         = 127; // 2^7 - 1
+   static const Int_t fgkRowIdLength         =  63; // 2^6 - 1
    static const Int_t fgkColumnIdLength      = 255; // 2^8 - 1
+   static const Int_t fgkTriangleIdLength    =   1; // 2^1 - 1
    // Number of a start bit for each volume
    static const Int_t fgkSystemIdShift       =  0;
    static const Int_t fgkLayerIdShift        =  4;
    static const Int_t fgkModuleIdShift       =  8;
    static const Int_t fgkSectorIdShift       = 15;
    static const Int_t fgkRowIdShift          = 17;
-   static const Int_t fgkColumnIdShift       = 24;
+   static const Int_t fgkColumnIdShift       = 23;
+   static const Int_t fgkTriangleIdShift     = 31;
    // Start bit and length for unique module address.
    // For unique module address use all bits up to sector ID.
    static const Int_t fgkModuleAddressStart  = 0;
--- data/trd/CbmTrdCluster.h
+++ data/trd/CbmTrdCluster.h
@@ -17,7 +17,9 @@
 class CbmTrdCluster : public CbmCluster
 {
 public:
-
+  enum CbmTrdClusterDef{
+    kTriang = BIT(15)     ///< set type of pads on which the cluster is reconstructed
+  };
   /**
    * \brief Default constructor.
    */
@@ -30,13 +32,14 @@ public:
 
 
   /** Setters **/
-  void SetNCols(UShort_t ncols)  { fNCols=ncols; }
-  void SetNRows(UShort_t nrows)  { fNRows=nrows; }
+  void      SetNCols(UShort_t ncols)   { fNCols=ncols; }
+  void      SetNRows(UShort_t nrows)   { fNRows=nrows; }
+  void      SetTrianglePads(Bool_t set=kTRUE)  { SetBit(kTriang, set); }
 
   /** Accessors **/
-  UShort_t GetNCols() const { return fNCols; }
-  UShort_t GetNRows() const { return fNRows; }
-
+  UShort_t  GetNCols() const { return fNCols; }
+  UShort_t  GetNRows() const { return fNRows; }
+  Bool_t    HasTrianglePads() const     { return TestBit(kTriang); }
  protected:
   UShort_t fNCols; // number of columns with charge above threshold
   UShort_t fNRows; // number of rows with charge above threshold
