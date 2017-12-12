385f4126e3e52e474b3e05ab50f1941e761a8364
d.emschermann <d.emschermann@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2017-12-12 09:00:44 +0000

    fix output filename for mTRD geometry

--- macro/mcbm/geometry/trd/Create_TRD_Geometry_v18m.C
+++ macro/mcbm/geometry/trd/Create_TRD_Geometry_v18m.C
@@ -109,7 +109,7 @@ const Double_t zfront[5]  = { 260., 149., 360., 410., 550. };
 const TString setupVer[5] = { "_1h", "_1e", "_1m", "_3e", "_3m" };
 const TString subVersion = setupVer[setupid];
 
-const TString geoVersion   = "trd_" + tagVersion + subVersion;
+const TString geoVersion   = "trd_" + tagVersion; // + subVersion;
 const TString FileNameSim  = geoVersion + "_mcbm.geo.root";
 const TString FileNameGeo  = geoVersion + "_mcbm_geo.root";
 const TString FileNameInfo = geoVersion + "_mcbm.geo.info";
