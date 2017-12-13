bcaa6b9bae0038d98cfa8831ad4ab1700c036c6f
andrey.lebedev <andrey.lebedev@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2017-12-13 13:19:04 +0000

    Macroses forTBsimulation in RICH

---
+++ macro/rich/timebased/run_rich_reco_tb.C
@@ -0,0 +1,113 @@
+
+void run_rich_reco_tb()
+{
+   TTree::SetMaxTreeSize(90000000000);
+   TString script = TString(gSystem->Getenv("SCRIPT"));
+
+   TString myName = "run_reco_geotest";
+   TString srcDir = gSystem->Getenv("VMCWORKDIR");  // top source directory
+
+   TString geoSetupFile = srcDir + "/macro/rich/geosetup/rich_setup_sis100_tb.C";
+
+   TString outDir = "/Users/slebedev/Development/cbm/data/sim/rich/tb/";
+   TString mcFile = outDir + "mc.00000.root";
+   TString parFile = outDir + "param.00000.root";
+   TString digiFile = outDir + "digi.00000.root";
+   TString recoFile = outDir + "reco.00000.root";
+   std::string resultDir = "results_geotest/";
+
+
+   // Specify interaction rate in 1/s
+   Double_t eventRate = 1.e4;
+   // Specify duration of time slices in output [ns]
+   Double_t timeSliceSize = 1.e8;
+
+   remove(recoFile.Data());
+
+//   TString setupFunct = "do_setup()";
+//   std::cout << "-I- " << myName << ": Loading macro " << geoSetupFile << std::endl;
+//   gROOT->LoadMacro(geoSetupFile);
+//   gROOT->ProcessLine(setupFunct);
+
+   std::cout << std::endl<< "-I- " << myName << ": Defining parameter files " << std::endl;
+   TList *parFileList = new TList();
+
+
+   TStopwatch timer;
+   timer.Start();
+   gDebug = 0;
+
+
+   CbmRunAna *run = new CbmRunAna();
+   FairFileSource* inputSource = new FairFileSource(digiFile);
+   run->SetSource(inputSource);
+   run->SetAsync();
+   run->SetOutputFile(recoFile);
+   run->SetEventMeanTime(1.e9 / eventRate);
+   FairRootManager::Instance()->SetUseFairLinks(kTRUE);
+
+   FairLogger::GetLogger()->SetLogScreenLevel("INFO");
+   FairLogger::GetLogger()->SetLogVerbosityLevel("LOW");
+
+   run->AddTask(new CbmBuildEventsIdeal());
+
+   CbmRichHitProducer* richHitProd  = new CbmRichHitProducer();
+   run->AddTask(richHitProd);
+
+
+//
+//   CbmRichReconstruction* richReco = new CbmRichReconstruction();
+//   richReco->SetRunExtrapolation(false);
+//   richReco->SetRunProjection(false);
+//   richReco->SetRunTrackAssign(false);
+//   richReco->SetFinderName("ideal");
+//   run->AddTask(richReco);
+//
+//   CbmMatchRecoToMC* matchRecoToMc = new CbmMatchRecoToMC();
+//   run->AddTask(matchRecoToMc);
+
+//   CbmRichGeoTest* geoTest = new CbmRichGeoTest();
+//   geoTest->SetOutputDir(resultDir);
+//   run->AddTask(geoTest);
+
+
+
+   std::cout << std::endl << std::endl << "-I- " << myName << ": Set runtime DB" << std::endl;
+   FairRuntimeDb* rtdb = run->GetRuntimeDb();
+   FairParRootFileIo* parIo1 = new FairParRootFileIo();
+   FairParAsciiFileIo* parIo2 = new FairParAsciiFileIo();
+   parIo1->open(parFile.Data(),"UPDATE");
+   rtdb->setFirstInput(parIo1);
+   if ( ! parFileList->IsEmpty() ) {
+      parIo2->open(parFileList, "in");
+      rtdb->setSecondInput(parIo2);
+   }
+
+
+   std::cout << std::endl << "-I- " << myName << ": Initialise run" << std::endl;
+   run->Init();
+
+
+   rtdb->setOutput(parIo1);
+   rtdb->saveOutput();
+   rtdb->print();
+
+
+   std::cout << std::endl << std::endl;
+   std::cout << "-I- " << myName << ": Starting run" << std::endl;
+   run->Run();
+
+
+   timer.Stop();
+   Double_t rtime = timer.RealTime();
+   Double_t ctime = timer.CpuTime();
+   std::cout << std::endl << std::endl;
+   std::cout << "Macro finished succesfully." << std::endl;
+   std::cout << "Output file is " << recoFile << std::endl;
+   std::cout << "Parameter file is " << parFile << std::endl;
+   std::cout << "Real time " << rtime << " s, CPU time " << ctime << " s" << std::endl;
+   std::cout << std::endl;
+   std::cout << " Test passed" << std::endl;
+   std::cout << " All ok " << std::endl;
+
+}
---
+++ macro/rich/timebased/run_rich_sim_tb.C
@@ -0,0 +1,191 @@
+#include <stdio.h>
+
+void run_rich_sim_tb(Int_t nEvents = 10)
+{
+   TTree::SetMaxTreeSize(90000000000);
+
+   TString script = TString(gSystem->Getenv("SCRIPT"));
+
+   TString myName = "run_sim_tb";  // this macro's name for screen output
+   TString srcDir = gSystem->Getenv("VMCWORKDIR");  // top source directory
+
+   TString geoSetupFile = srcDir + "/macro/rich/geosetup/rich_setup_sis100_tb.C";
+
+   TString outDir = "/Users/slebedev/Development/cbm/data/sim/rich/tb/";
+   TString parFile =  outDir + "param.00000.root";
+   TString mcFile = outDir + "mc.00000.root";
+   TString geoFile = outDir + "geo_file.root";
+
+   remove(parFile.Data());
+   remove(mcFile.Data());
+   remove(geoFile.Data());
+
+
+   // Target geometry
+   TString  targetElement   = "Gold";
+   Double_t targetThickness = 0.025;  // full thickness in cm
+   Double_t targetDiameter  = 2.5;    // diameter in cm
+   Double_t targetPosX      = 0.;     // target x position in global c.s. [cm]
+   Double_t targetPosY      = 0.;     // target y position in global c.s. [cm]
+   Double_t targetPosZ      = 0.;     // target z position in global c.s. [cm]
+   Double_t targetRotY      = 0.;     // target rotation angle around the y axis [deg]
+
+
+   // primary vertex
+   Bool_t smearVertexXY = kTRUE;
+   Bool_t smearVertexZ  = kTRUE;
+   Double_t beamWidthX   = 0.1;  // Gaussian sigma of the beam profile in x [cm]
+   Double_t beamWidthY   = 0.1;  // Gaussian sigma of the beam profile in y [cm]
+
+
+   TStopwatch timer;
+   timer.Start();
+   gDebug = 0;
+
+   FairRunSim* run = new FairRunSim();
+   run->SetName("TGeant3");
+   run->SetOutputFile(mcFile);
+   run->SetGenerateRunInfo(kTRUE);
+
+
+   FairLogger::GetLogger()->SetLogScreenLevel("INFO");
+   FairLogger::GetLogger()->SetLogVerbosityLevel("LOW");
+
+   TString setupFunct = "do_setup()";
+   std::cout << "-I- " << myName << ": Loading macro " << geoSetupFile << std::endl;
+   gROOT->LoadMacro(geoSetupFile);
+   gROOT->ProcessLine(setupFunct);
+   // You can modify the pre-defined setup by using
+   // CbmSetup::Instance()->RemoveModule(ESystemId) or
+   // CbmSetup::Instance()->SetModule(ESystemId, const char*, Bool_t) or
+   // CbmSetup::Instance()->SetActive(ESystemId, Bool_t)
+   // See the class documentation of CbmSetup.
+
+
+   std::cout << std:: endl << "-I- " << myName << ": Setting media file" << std::endl;
+   run->SetMaterials("media.geo"); // Materials
+
+
+   TString macroName = gSystem->Getenv("VMCWORKDIR");
+   macroName += "/macro/run/modules/registerSetup.C";
+   std::cout << std::endl << "Loading macro " << macroName << std::endl;
+   gROOT->LoadMacro(macroName);
+   gROOT->ProcessLine("registerSetup()");
+
+
+   std::cout << std::endl << "-I- " << myName << ": Registering target" << std::endl;
+   CbmTarget* target = new CbmTarget(targetElement.Data(), targetThickness, targetDiameter);
+   target->SetPosition(targetPosX, targetPosY, targetPosZ);
+   target->SetRotation(targetRotY);
+   target->Print();
+   run->AddModule(target);
+
+
+   // Magnetic field
+   std::cout << std::endl << "-I- " << myName << ": Registering magnetic field" << std::endl;
+   CbmFieldMap* magField = CbmSetup::Instance()->CreateFieldMap();
+   if ( !magField ) {
+      std::cout << "-ERROR- : No valid field!";
+      return;
+   }
+   run->SetField(magField);
+
+
+   //  PrimaryGenerator
+   std::cout << std::endl << "-I- " << myName << ": Registering event generators" << std::endl;
+   FairPrimaryGenerator* primGen = new FairPrimaryGenerator();
+   // --- Uniform distribution of event plane angle
+   primGen->SetEventPlane(0., 2. * TMath::Pi());
+   // --- Get target parameters
+   Double_t tX = 0.;
+   Double_t tY = 0.;
+   Double_t tZ = 0.;
+   Double_t tDz = 0.;
+   if ( target ) {
+      target->GetPosition(tX, tY, tZ);
+      tDz = target->GetThickness();
+   }
+   primGen->SetTarget(tZ, tDz);
+   primGen->SetBeam(0., 0., beamWidthX, beamWidthY);
+   primGen->SmearGausVertexXY(smearVertexXY);
+   primGen->SmearVertexZ(smearVertexZ);
+
+
+   FairBoxGenerator* boxGen1 = new FairBoxGenerator(11, 1);
+   boxGen1->SetPtRange(0.,3.);
+   boxGen1->SetPhiRange(0.,360.);
+   boxGen1->SetThetaRange(2.5,25.);
+   boxGen1->SetCosTheta();
+   boxGen1->Init();
+   primGen->AddGenerator(boxGen1);
+
+   FairBoxGenerator* boxGen2 = new FairBoxGenerator(-11, 1);
+   boxGen2->SetPtRange(0.,3.);//4
+   boxGen2->SetPhiRange(0.,360.);
+   boxGen2->SetThetaRange(2.5,25.);//35
+   boxGen2->SetCosTheta();
+   boxGen2->Init();
+   primGen->AddGenerator(boxGen2);
+   run->SetGenerator(primGen);
+
+
+   std::cout << std::endl << "-I- " << myName << ": Initialise run" << std::endl;
+   //run->SetStoreTraj(kTRUE);
+   run->Init();
+
+
+   // -----   Runtime database   ---------------------------------------------
+   std::cout << std::endl << std::endl;
+   std::cout << "-I- " << myName << ": Set runtime DB" << std::endl;
+   FairRuntimeDb* rtdb = run->GetRuntimeDb();
+   CbmFieldPar* fieldPar = (CbmFieldPar*) rtdb->getContainer("CbmFieldPar");
+   fieldPar->SetParameters(magField);
+   fieldPar->setChanged();
+   fieldPar->setInputVersion(run->GetRunId(),1);
+   Bool_t kParameterMerged = kTRUE;
+   FairParRootFileIo* parOut = new FairParRootFileIo(kParameterMerged);
+   parOut->open(parFile.Data());
+   rtdb->setOutput(parOut);
+   rtdb->saveOutput();
+   rtdb->print();
+
+
+   std::cout << std::endl << std::endl << "-I- " << myName << ": Starting run" << std::endl;
+   run->Run(nEvents);
+
+
+   run->CreateGeometryFile(geoFile);
+
+   timer.Stop();
+   Double_t rtime = timer.RealTime();
+   Double_t ctime = timer.CpuTime();
+   std::cout << std::endl << std::endl;
+   std::cout << "Macro finished successfully." << std::endl;
+   std::cout << "Output file is "    << mcFile << std::endl;
+   std::cout << "Parameter file is " << parFile << std::endl;
+   std::cout << "Geometry file is "  << geoFile << std::endl;
+   std::cout << "Real time " << rtime << " s, CPU time " << ctime
+         << "s" << std::endl << std::endl;
+   // ------------------------------------------------------------------------
+
+
+   // -----   Resource monitoring   ------------------------------------------
+   //    if ( Has_Fair_Monitor() ) {      // FairRoot Version >= 15.11
+   //      // Extract the maximal used memory an add is as Dart measurement
+   //      // This line is filtered by CTest and the value send to CDash
+   //      FairSystemInfo sysInfo;
+   //      Float_t maxMemory=sysInfo.GetMaxMemory();
+   //      std::cout << "<DartMeasurement name=\"MaxMemory\" type=\"numeric/double\">";
+   //      std::cout << maxMemory;
+   //      std::cout << "</DartMeasurement>" << std::endl;
+   //
+   //      Float_t cpuUsage=ctime/rtime;
+   //      std::cout << "<DartMeasurement name=\"CpuLoad\" type=\"numeric/double\">";
+   //      std::cout << cpuUsage;
+   //      std::cout << "</DartMeasurement>" << std::endl;
+   //    }
+
+   std::cout << " Test passed" << std::endl;
+   std::cout << " All ok " << std::endl;
+
+}
