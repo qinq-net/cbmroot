bd0425a25e858bf444e86f85da786bfd2c826453
g.pitsch <g.pitsch@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2018-01-18 09:12:39 +0000

    work on mRICH geometry

---
+++ macro/rich/mcbm/eventDisplay.C
@@ -0,0 +1,49 @@
+//void eventDisplay(const char* setup = "sis18_mcbm_25deg")
+//void eventDisplay(const char* setup = "sis18_mcbm_20deg_short")
+void eventDisplay(const char* setup = "sis18_mcbm_20deg_long")
+{
+
+  TString dataDir   = "/home/aghoehne/Documents/CbmRoot/Gregor/";
+  TString InputFile = dataDir + setup + "_test.mc.root";
+  TString ParFile   = dataDir + setup + "_params.root";
+
+  // -----   Reconstruction run   -------------------------------------------
+  FairRunAna *fRun= new FairRunAna();
+
+  fRun->SetInputFile(InputFile.Data());
+  fRun->SetOutputFile(dataDir + setup + "_test.root");
+
+  FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
+  FairParRootFileIo* parInput1 = new FairParRootFileIo();
+  parInput1->open(ParFile.Data());
+  rtdb->setFirstInput(parInput1);
+
+  FairEventManager *fMan  = new FairEventManager();
+  FairMCTracks     *Track = new FairMCTracks ("Monte-Carlo Tracks");
+
+  FairMCPointDraw  *MvdPoint      = new FairMCPointDraw ("MvdPoint",      kBlack,  kFullSquare);
+  FairMCPointDraw  *StsPoint      = new FairMCPointDraw ("StsPoint",      kGreen,  kFullSquare);
+  FairMCPointDraw  *MuchPoint     = new FairMCPointDraw ("MuchPoint",     kOrange, kFullSquare);
+  FairMCPointDraw  *RichPoint     = new FairMCPointDraw ("RichPoint",     kGreen,  kFullSquare);
+  FairMCPointDraw  *TrdPoint      = new FairMCPointDraw ("TrdPoint",      kBlue,   kFullSquare);
+  FairMCPointDraw  *TofPoint      = new FairMCPointDraw ("TofPoint",      kRed,    kFullSquare);
+  FairMCPointDraw  *EcalPoint     = new FairMCPointDraw ("EcalPoint",     kYellow, kFullSquare);
+  FairMCPointDraw  *RefPlanePoint = new FairMCPointDraw ("RefPlanePoint", kPink,   kFullSquare);
+                                                               
+  fMan->AddTask(Track);
+  
+  fMan->AddTask(MvdPoint);
+  fMan->AddTask(StsPoint);
+  fMan->AddTask(MuchPoint);
+  fMan->AddTask(RichPoint);
+  fMan->AddTask(TrdPoint);
+  fMan->AddTask(TofPoint);   
+  fMan->AddTask(EcalPoint);   
+  fMan->AddTask(RefPlanePoint);
+ 
+    
+  //  fMan->Init(1,4,10000);
+  //  fMan->Init(1,5,10000);  // make STS visible by default
+  //  fMan->Init(1,6,10000);  // make MVD visible by default
+  fMan->Init(1,7,10000);  // make MVD visible by default
+}
