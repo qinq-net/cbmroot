4598effee285ae02d3947e53598f02dbdba49c0e
p.-a.loizeau <p.-a.loizeau@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2018-06-27 13:05:41 +0000

    Fix copy paths in script for mtof geometry and parameter creation

--- macro/mcbm/geometry/tof/create_tof_geometry.sh
+++ macro/mcbm/geometry/tof/create_tof_geometry.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 VGEO=$1
 
+echo ${VGEO}
+
 root -l -q Create_TOF_Geometry_${VGEO}_mcbm.C
-cp tof_${VGEO}_mcbm.geo.root ../../../geometry/tof/tof_${VGEO}_mcbm.geo.root
-cp tof_${VGEO}_mcbm.geo.info ../../../geometry/tof/tof_${VGEO}_mcbm.geo.info
+cp tof_${VGEO}_mcbm.geo.root ../../../../geometry/tof/tof_${VGEO}_mcbm.geo.root
+cp tof_${VGEO}_mcbm.geo.info ../../../../geometry/tof/tof_${VGEO}_mcbm.geo.info
 root -l 'create_tof_digipar.C("tof_'${VGEO}'_mcbm")'
-cp tof_${VGEO}_mcbm.digi.par ../../../parameters/tof/tof_${VGEO}_mcbm.digi.par
+cp tof_${VGEO}_mcbm.digi.par ../../../../parameters/tof/tof_${VGEO}_mcbm.digi.par
 rm tof_${VGEO}_mcbm* test.esd.tof_${VGEO}_mcbm.root
