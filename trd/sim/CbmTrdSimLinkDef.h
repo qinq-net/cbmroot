b17c216ff0dc567063d7906239216a06c4b44c90
f.uhlig <f.uhlig@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2018-07-04 12:10:27 +0000

    Create separate library for simulation related code. For the time being CbmGas and CbmRadiator have to stay in the base library. Otherwise there are circular library dependencies. This will be solved if the code from the attic sub directory will be removed. refs #1102 @30m.

---
+++ trd/sim/CbmTrdSimLinkDef.h
@@ -0,0 +1,18 @@
+// $Id: TrdSimLinkDef.h $
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class  CbmTrd+;
+//#pragma link C++ class  CbmTrdRadiator+;
+#pragma link C++ class  CbmTrdTrackFinderIdeal+;
+#pragma link C++ class  CbmTrdTrackFitterIdeal+;
+#pragma link C++ class  CbmTrdDigitizer+;
+//#pragma link C++ class  CbmTrdModuleSim+;
+//#pragma link C++ class  CbmTrdModuleSimR+;
+//#pragma link C++ class  CbmTrdModuleSimT+;
+//#pragma link C++ class  CbmTrdTrianglePRF+;
+#endif
