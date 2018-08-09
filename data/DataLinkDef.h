3e1ce4f60869cdeb43ea6e6e0f8ae571d8eb5608
f.uhlig <f.uhlig@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2018-08-09 13:48:07 +0000

    Remove obsolete scheme evolution rule.

--- data/DataLinkDef.h
+++ data/DataLinkDef.h
@@ -106,11 +106,4 @@
 #pragma link C++ class CbmStsxyterRawHitMessage;
 #pragma link C++ class CbmTrbRawMessage;
 
-#pragma read sourceClass="CbmTrdDigi" version="[6]" targetClass="CbmTrdDigi" version="[7]"\
-   source="int fNrSamples; float* fSamples" target="fSamples" \
-   code="{ if (onfile.fNrSamples > 0) { \
-           for (Int_t sample = 0; sample < onfile.fNrSamples; sample++) fSamples.push_back(onfile.fSamples[sample]); \
-           } \
-         }"
-
 #endif
