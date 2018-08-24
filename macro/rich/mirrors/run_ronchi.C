3bcba2be82b8aa440c5929c4ab2a46fd510ab787
g.pitsch <g.pitsch@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2018-08-24 11:52:53 +0000

    Ronchi Test

--- macro/rich/mirrors/run_ronchi.C
+++ macro/rich/mirrors/run_ronchi.C
@@ -4,9 +4,9 @@ void run_ronchi() {
     gSystem->Load("libtiff");
 
     std::string dir = "/dir/";
-    std::string fileName1 = "ronchi.tiff";
+    std::string fileName1 = "test_grid_1.tiff";
 
     CbmRichRonchiAna *ronchiAna = new CbmRichRonchiAna();
-    ronchiAna->SetTiffFileName("/Users/slebedev/Downloads/test1.tif");
+    ronchiAna->SetTiffFileName("test_grid_1.tif");
     ronchiAna->Run();
 }
