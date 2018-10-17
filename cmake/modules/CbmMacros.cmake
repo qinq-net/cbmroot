03dd929a03900a0c3fd548351353b1895ba35b5c
f.uhlig <f.uhlig@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2018-10-17 11:00:14 +0000

    Improve handling of external repositories
    Start the download procedure for external packages only
    if the package wasn't downloaded already or if the version
    was changed in our build system.

--- cmake/modules/CbmMacros.cmake
+++ cmake/modules/CbmMacros.cmake
@@ -117,3 +117,32 @@ Macro(Gen_Exe_Script _ExeName)
                  )
 
 EndMacro(Gen_Exe_Script)
+
+function(download_project_if_needed)
+  include(DownloadProject)
+  set(oneValueArgs PROJECT GIT_REPOSITORY GIT_TAG SOURCE_DIR TEST_FILE)
+  cmake_parse_arguments(MY "" "${oneValueArgs}"
+                       "" ${ARGN} )
+
+  If(NOT EXISTS ${MY_SOURCE_DIR}/${MY_TEST_FILE})
+    download_project(PROJ            ${MY_PROJECT}
+                     GIT_REPOSITORY  ${MY_GIT_REPOSITORY}
+                     GIT_TAG         ${MY_GIT_TAG}
+                     SOURCE_DIR      ${MY_SOURCE_DIR}
+                    )
+  Else()
+    Execute_process(COMMAND git rev-parse HEAD
+                    WORKING_DIRECTORY ${MY_SOURCE_DIR}
+                    OUTPUT_VARIABLE CURRENT_SPADIC_HASH
+                    OUTPUT_STRIP_TRAILING_WHITESPACE
+                   )
+    If(NOT ${MY_GIT_TAG} STREQUAL ${CURRENT_SPADIC_HASH})
+      download_project(PROJ            ${MY_PROJECT}
+                       GIT_REPOSITORY  ${MY_GIT_REPOSITORY}
+                       GIT_TAG         ${MY_GIT_TAG}
+                       SOURCE_DIR      ${MY_SOURCE_DIR}
+                      )
+    EndIf()
+  EndIf()
+EndFunction()
+
