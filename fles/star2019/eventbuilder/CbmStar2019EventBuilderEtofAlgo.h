84314e6c1fffe0e76bb33caaaec6e48698cbaa04
p.-a.loizeau <p.-a.loizeau@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2018-11-04 23:40:20 +0000

    eTOF 2019: Event builder algo skeletton

---
+++ fles/star2019/eventbuilder/CbmStar2019EventBuilderEtofAlgo.h
@@ -0,0 +1,102 @@
+// -----------------------------------------------------------------------------
+// -----                                                                   -----
+// -----                  CbmTofStarEventBuilderAlgo2019                   -----
+// -----               Created 03.11.2018 by P.-A. Loizeau                 -----
+// -----                                                                   -----
+// -----------------------------------------------------------------------------
+
+#ifndef CbmTofStarEventBuilderAlgo2019_H
+#define CbmTofStarEventBuilderAlgo2019_H
+
+#include "Timeslice.hpp"
+#include "gDpbMessv100.h"
+#include "CbmTofStarData2019.h"
+
+#include <vector>
+#include <map>
+#include <chrono>
+#include <chrono>
+
+class CbmTofStar2019Par;
+
+class TCanvas;
+class TH1;
+class TH2;
+class TProfile;
+
+#ifdef STAR_SUBEVT_BUILDER
+   /*
+    ** Function to send sub-event block to the STAR DAQ system
+    *       trg_word received is packed as:
+    *
+    *       trg_cmd|daq_cmd|tkn_hi|tkn_mid|tkn_lo
+   */
+   extern "C" int star_rhicf_write(unsigned int trg_word, void *dta, int bytes);
+#endif // STAR_SUBEVT_BUILDER
+
+class CbmTofStarEventBuilderAlgo2019 : // public CbmAlgo
+{
+   public:
+      CbmTofStarEventBuilderAlgo2019();
+      ~CbmTofStarEventBuilderAlgo2019();
+
+      Bool_t InitStsParameters();
+      Bool_t CreateHistograms();
+      Bool_t ProcessStsMs( const fles::Timeslice& ts, size_t uMsComp, UInt_t uMsIdx );
+      Bool_t BuildEvents();
+
+   private:
+      /// Control flags
+      Bool_t fbMonitorMode;
+      
+      /// FLES containers
+      std::vector< size_t > fvMsComponentsList; //!
+      size_t                fuNbCoreMsPerTs; //!
+      size_t                fuNbOverMsPerTs; //!
+      Bool_t                fbIgnoreOverlapMs; //! /** Ignore Overlap Ms: all fuOverlapMsNb MS at the end of timeslice **/
+
+      /// Settings from parameter file
+      CbmTofStar2019Par* fUnpackPar;      //!
+      UInt_t fuNrOfGdpbs;           // Total number of GDPBs in the system
+      UInt_t fuNrOfFeePerGdpb;      // Number of FEBs per GDPB
+      UInt_t fuNrOfGet4PerFee;      // Number of GET4s per FEE
+      UInt_t fuNrOfChannelsPerGet4; // Number of channels in each GET4
+
+      UInt_t fuNrOfChannelsPerFee;  // Number of channels in each FEE
+      UInt_t fuNrOfGet4;            // Total number of Get4 chips in the system
+      UInt_t fuNrOfGet4PerGdpb;     // Number of GET4s per GDPB
+      UInt_t fuNrOfChannelsPerGdpb; // Number of channels per GDPB
+
+      UInt_t fuNrOfGbtx;
+      UInt_t fuNrOfModules;
+      std::vector< Int_t > fviNrOfRpc;
+      std::vector< Int_t > fviRpcType;
+      std::vector< Int_t > fviRpcSide;
+      std::vector< Int_t > fviModuleId;
+
+      const UInt_t kuNbFeePerGbtx  = 5; /// TODO => In parameter!
+      const UInt_t kuNbGbtxPerGdpb = 6; /// TODO => In parameter!
+      
+      /// Running indices 
+      uint64_t fulCurrentTsIndex;  // Idx of the current TS
+      size_t   fuCurrentMs; // Idx of the current MS in TS (0 to fuTotalMsNb)
+      Double_t fdMsIndex;   // Time in ns of current MS from its index
+      UInt_t   fuGdpbId;    // Id (hex number) of the GDPB for current message
+      UInt_t   fuGdpbNr;    // running number (0 to fuNrOfGdpbs) of the GDPB for current message
+      UInt_t   fuGet4Id;    // running number (0 to fuNrOfGet4PerGdpb) of the Get4 chip of a unique GDPB for current message
+      UInt_t   fuGet4Nr;    // running number (0 to fuNrOfGet4) of the Get4 chip in the system for current message
+      Int_t    fiEquipmentId;
+
+      /// Buffers
+      std::vector< gdpbv100::Message >     fvBufferMessages;        //! [sector]
+      std::vector< gdpbv100::Message >     fvBufferMessagesOverlap; //! [sector]
+      std::vector< CbmTofStarTrigger2019 > fvBufferTriggers;        //! [sector]
+      std::vector< CbmTofStarTrigger2019 > fvBufferTriggersOverlap; //! [sector]
+      
+      CbmTofStarEventBuilderAlgo2019(const CbmTofStarEventBuilderAlgo2019&);
+      CbmTofStarEventBuilderAlgo2019 operator=(const CbmTofStarEventBuilderAlgo2019&);
+
+      ClassDef(CbmTofStarEventBuilder2018, 1)
+};
+
+#endif
