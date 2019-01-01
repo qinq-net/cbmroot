322ac51fa047257ff8f6d6d8189da811644a8f8a
n.herrmann <n.herrmann@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2019-01-01 08:18:19 +0000

    add TShift parameter for diamond

--- MQ/unpacker/runUnpackTofMcbm2018.cxx
+++ MQ/unpacker/runUnpackTofMcbm2018.cxx
@@ -13,6 +13,7 @@ void addCustomOptions(bpo::options_description& options)
   options.add_options() ("ReqTint", bpo::value<uint64_t>()->default_value(100), "Time intervall length in ns");
   options.add_options() ("PulserMode",  bpo::value<int64_t>()->default_value(0),"Choose pulser configuration");
   options.add_options() ("PulMulMin",   bpo::value<uint64_t>()->default_value(0), "Min number of pulsed detectors");
+  options.add_options() ("TShiftRef", bpo::value<double_t>()->default_value(0.), "Time shift of reference counter to match digis");
    
  int iNDet=36;
   for (int i=0; i<iNDet; i++)
