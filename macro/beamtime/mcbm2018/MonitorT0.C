16bb7cc7e548bc2b81a3414158a63fe2fd962d26
p.-a.loizeau <p.-a.loizeau@5a1b234a-d7ce-0410-9a93-fd649a8fa65c>
2019-03-04 17:04:41 +0000

    mCBM 2018: add macro and parameter file for T0 monitor

---
+++ macro/beamtime/mcbm2018/MonitorT0.C
@@ -0,0 +1,129 @@
+/** @file MCBM DATA unpacking
+ ** @author Florian Uhlig <f.uhlig@gsi.de>
+ ** @date 20.06.2016
+ ** Modified by P.-A. Loizeau
+ ** @date 30.01.2019
+ ** ROOT macro to read tsa files which have been produced with the new data transport
+ ** Convert data into cbmroot format.
+ ** Uses CbmMcbm2018Source as source task.
+ */
+// In order to call later Finish, we make this global
+FairRunOnline *run = NULL;
+
+void MonitorT0( TString inFile = "", TString sHostname = "localhost",
+                Int_t iServerHttpPort = 8080, Int_t iServerRefreshRate = 100,
+                UInt_t uRunId = 0, UInt_t nrEvents=0)
+{
+  TString srcDir = gSystem->Getenv("VMCWORKDIR");
+
+  // --- Specify number of events to be produced.
+  // --- -1 means run until the end of the input file.
+  Int_t nEvents=-1;
+  // --- Specify output file name (this is just an example)
+  TString runId = TString::Format("%u", uRunId);
+  TString outFile = "data/moni_t0_" + runId + ".root";
+  TString parFile = "data/moni_t0_params_" + runId + ".root";
+
+  // --- Set log output levels
+  FairLogger::GetLogger();
+  gLogger->SetLogScreenLevel("INFO");
+  //gLogger->SetLogScreenLevel("DEBUG");
+  gLogger->SetLogVerbosityLevel("MEDIUM");
+
+  // --- Define parameter files
+  TList *parFileList = new TList();
+  TString paramDir = "./";
+
+  TString paramFileTof = paramDir + "mT0Par.par";
+  TObjString* parTofFileName = new TObjString(paramFileTof);
+  parFileList->Add(parTofFileName);
+
+  // --- Set debug level
+  gDebug = 0;
+
+  std::cout << std::endl;
+  std::cout << ">>> MonitorT0: output file is " << outFile << std::endl;
+
+  // ========================================================================
+  // ========================================================================
+  std::cout << std::endl;
+  std::cout << ">>> MonitorT0: Initialising..." << std::endl;
+  CbmMcbm2018MonitorTaskT0  * monitor_t0  = new CbmMcbm2018MonitorTaskT0();
+
+  monitor_t0->SetIgnoreOverlapMs();
+  monitor_t0->SetHistoryHistoSize( 1800 );
+
+  // --- Source task
+  CbmMcbm2018Source* source = new CbmMcbm2018Source();
+
+  if( "" != inFile )
+  {
+    source->SetFileName(inFile);
+  } // if( "" != inFile )
+      else
+      {
+         source->SetHostName( sHostname );
+         source->SetPortNumber( 5556 );
+      } // else of if( "" != inFile )
+
+  source->AddUnpacker(monitor_t0,  0x60, 9  );//gDPB TOF
+  source->AddUnpacker(monitor_t0,  0x90, 9  );//gDPB T0
+
+  // --- Event header
+  FairEventHeader* event = new CbmTbEvent();
+  event->SetRunId(uRunId);
+
+  // --- RootFileSink
+  // --- Open next outputfile after 4GB
+  FairRootFileSink* sink = new FairRootFileSink(outFile);
+//  sink->GetOutTree()->SetMaxTreeSize(4294967295LL);
+
+  // --- Run
+  run = new FairRunOnline(source);
+  run->SetSink(sink);
+  run->SetEventHeader(event);
+  run->ActivateHttpServer( iServerRefreshRate, iServerHttpPort ); // refresh each 100 events
+  run->SetAutoFinish(kFALSE);
+
+
+  // -----   Runtime database   ---------------------------------------------
+  FairRuntimeDb* rtdb = run->GetRuntimeDb();
+  Bool_t kParameterMerged = kTRUE;
+  FairParRootFileIo* parOut = new FairParRootFileIo(kParameterMerged);
+  FairParAsciiFileIo* parIn = new FairParAsciiFileIo();
+  parOut->open(parFile.Data());
+  parIn->open(parFileList, "in");
+  rtdb->setFirstInput(parIn);
+  rtdb->setOutput(parOut);
+
+  run->Init();
+
+  // --- Start run
+  TStopwatch timer;
+  timer.Start();
+  std::cout << ">>> MonitorT0: Starting run..." << std::endl;
+  if ( 0 == nrEvents) {
+    run->Run(nEvents, 0); // run until end of input file
+  } else {
+    run->Run(0, nrEvents); // process  2000 Events
+  }
+  run->Finish();
+
+  timer.Stop();
+
+  std::cout << "Processed " << std::dec << source->GetTsCount() << " timeslices" << std::endl;
+
+  // --- End-of-run info
+  Double_t rtime = timer.RealTime();
+  Double_t ctime = timer.CpuTime();
+  std::cout << std::endl << std::endl;
+  std::cout << ">>> MonitorT0: Macro finished successfully." << std::endl;
+  std::cout << ">>> MonitorT0: Output file is " << outFile << std::endl;
+  std::cout << ">>> MonitorT0: Real time " << rtime << " s, CPU time "
+	    << ctime << " s" << std::endl;
+  std::cout << std::endl;
+
+  /// --- Screen output for automatic tests
+  std::cout << " Test passed" << std::endl;
+  std::cout << " All ok " << std::endl;
+}
